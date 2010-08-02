703e3da1775ac96023e919393853ab0487cce7ac
twinh <twinhuang@qq.com>
2010-08-02 02:05:51 +0000

    git-svn-id: https://miku.googlecode.com/svn/trunk@14 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

---
+++ Qwin/Panda/Model.php
@@ -0,0 +1,30 @@
+<?php
+ /**
+ * Model
+ *
+ * Copyright (c) 2008-2010 Twin Huang. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package     Qwin
+ * @subpackage  Miku
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @copyright   Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @version     $Id$
+ * @since       2010-4-17 15:49:35
+ */
+
+class Qwin_Miku_Model
+{
+}
