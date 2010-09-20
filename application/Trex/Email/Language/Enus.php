c19018c8f175ec41f761cf7960451ae711e635c1
twinh <twinhuang@qq.com>
2010-09-20 07:09:13 +0000

    [应用]更新邮件模块
    git-svn-id: https://miku.googlecode.com/svn/trunk@158 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

---
+++ application/Trex/Email/Language/Enus.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Enus
+ *
+ * Copyright (c) 2008-2010 Twin Huang. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package     Trex
+ * @subpackage  Email
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @copyright   Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @version     $Id$
+ * @since       2010-09-20 10:37:51
+ */
+
+class Trex_Email_Language_Enus extends Trex_Language_Enus
+{
+    public function  __construct()
+    {
+        parent::__construct();
+        $this->_data += array(
+            'LBL_FIELD_FROM' => 'From',
+            'LBL_FIELD_FROM_NAME' => 'From Name',
+            'LBL_FIELD_TO' => 'To',
+            'LBL_FIELD_TO_NAME' => 'To Name',
+            'LBL_FIELD_SUBJECT' => 'Subject',
+            'LBL_FIELD_RESULT' => 'Result',
+
+            'LBL_ACTION_POST' => 'Post',
+
+            'LBL_MODULE_EMAIL' => 'Email',
+        );
+    }
+}
