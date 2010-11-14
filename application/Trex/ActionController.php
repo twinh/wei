ab1906c62f46155a419c91ebdd1785be614f92b3
twinh <twinhuang@qq.com>
2010-11-14 06:30:21 +0000

    [应用]修复细节问题
    git-svn-id: https://miku.googlecode.com/svn/trunk@207 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- application/Trex/ActionController.php
+++ application/Trex/ActionController.php
@@ -256,7 +256,7 @@ class Trex_ActionController extends Trex_Controller
     public function createCustomLink()
     {
         return Qwin_Helper_Html::jQueryLink($this->url->createUrl($this->_set, array('action' => 'Add')), $this->_lang->t('LBL_ACTION_ADD'), 'ui-icon-plus')
-             . Qwin_Helper_Html::jQueryLink('javascript:;', $this->_lang->t('LBL_ACTION_DELETE'), 'ui-icon-trash')
+             . Qwin_Helper_Html::jQueryLink('javascript:;', $this->_lang->t('LBL_ACTION_DELETE'), 'ui-icon-trash', 'action-delete')
              . Qwin_Helper_Html::jQueryLink($this->url->createUrl($this->_set, array('action' => 'Index')), $this->_lang->t('LBL_ACTION_LIST'), 'ui-icon-note');
     }
 
