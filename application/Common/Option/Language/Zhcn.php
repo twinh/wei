ec5d3840104dd49efe988eb6a70a840fa40955c6
twinh <twinhuang@qq.com>
2011-01-20 06:34:58 +0000

    [应用]调整通用分类模块为选项(CommonClass=>Option)
    git-svn-id: https://miku.googlecode.com/svn/trunk@304 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

---
+++ application/Common/Option/Language/Zhcn.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Zhcn
+ *
+ * Copyright (c) 2008-2010 Twin Huang. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package     Common
+ * @subpackage  Option
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @copyright   Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @version     $Id$
+ * @since       2010-08-26 17:20:56
+ */
+
+class Common_Option_Language_Zhcn extends Common_Language_Zhcn
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->_data += array(
+            'LBL_FIELD_CODE' => '代码',
+            'LBL_FIELD_VALUE' => '值',
+            'LBL_FIELD_VAR_NAME' => '变量名称',
+            'LBL_FIELD_TYPE' => '类型',
+
+            'LBL_ACTION_ADD_NEXT' => '添加下一个分类',
+
+            'LBL_MODULE_OPTION' => '选项',
+            'LBL_FIELD_LANGUAGE' => '语言',
+            'LBL_FIELD_SIGN' => '标识',
+
+        );
+    }
+}
