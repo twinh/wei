17cf7a200637b5b80cac3a6b62ca27057b096852
twinh <twinhuang@qq.com>
2011-01-20 16:57:37 +0000

    [应用]更新完善选项模块 [资源]更新jQuery至1.4.4,UI至1.8.8
    git-svn-id: https://miku.googlecode.com/svn/trunk@307 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- js/jquery/plugins/mapeditor/jquery.mapeditor.js
+++ js/jquery/plugins/mapeditor/jquery.mapeditor.js
@@ -1,149 +1,78 @@
-(function($) {
-	/**
-	 * map 数据编辑器
-	 *
-	 * @depend json2.js http://www.JSON.org/json2.js
-	 * @depend qui.js Resource/js/jquery/plugin/qui.js
-	 * @todo 隐藏 json 数据,以更好的数据形式显示给用户
-	 */
-	$.fn.mapEditor = function(options) {
-		var $this = this;
-		$this.opts = $.extend({}, $.fn.mapEditor.defaults, options);
-		$this.bind('click', $this, function(){
-			
-			// textarea 框中数据作为主要来源,其次使用配置的数据
-			if('' != $this.val())
-			{
-				$this.opts.data = JSON.parse($this.val());
-			}
-			// 初始化html
-			init($this);
-			
+jQuery(function($){
+    $('td.ui-mapeditor-oper a').qui();
 
-			// 加载已有数据
-			for(var key in $this.opts.data)
-			{
-				addRow($this, key, $this.opts.data[key]);
-			}
-			// 在已有的基础上增加三行
-			$.each( [0,1,2], function(i, n){
-				addRow($this);
-			}); 
-			
-			// 增加新行
-			$this._html.find('.ui-mapeditor-add').click(function(){
-				addRow($this);
-			});
-			
-			// 保存数据
-			$this._html.find('.ui-mapeditor-submit').click(function(){
-				submit($this);
-			});
-			
-			// 关闭对话框时,移除html内容
-			$this.opts.dialog.close = function(event, ui){
-				remove($this);
-			};
-			// 显示对话框
-			$this._html.dialog($this.opts.dialog);
-		});
-		return this;
-	};
-	
-	function init($this)
-	{
-		// 加载基本的界面
-		$this._html = $(['<div class="ui-mapeditor"><form class="ui-mapeditor-form">',
-				'<table cellpadding="0" cellspacing="5" width="96%" border="0">',
-				'<thead><tr><td>' + $this.opts.language.key  + '</td><td>' + $this.opts.language.value,
-				'</td><td>' + $this.opts.language.operate + '</td></tr></thead>',
-				'<tbody></tbody><tfoot><tr><td colspan="3">',
-				'<input type="button" class="ui-widget-content ui-corner-all ui-mapeditor-add" value="' + $this.opts.language.add +'" />',
-				'<input type="button" class="ui-widget-content ui-corner-all ui-mapeditor-submit" value="' + $this.opts.language.submit + '" />',
-				'</td></tr></tfoot></table></form></div>'].join(''));
-		hoverAndFocus($this._html);
-		$('body').append($this._html);
-		$this._table = $this._html.find('table > tbody');
-	}
-	
-	function hoverAndFocus(obj)
-	{
-		if('undefined' != typeof $.fn.qui)
-		{
-			obj.find('input').qui({focus: true});
-		}
-	}
-	
-	/**
-	 * 提交处理数据
-	 */
-	function submit($this)
-	{
-		var json = {};
-		// 在文本框中取值,构建 map 对象
-		$('form.ui-mapeditor-form input[type=text]').each(function(i){
-			var val = $.trim($(this).val());
-			if(0 == i%2)
-			{
-				key = val;
-				// 如果是空,则不加入
-				if('' != val)
-				{
-					json[key] = '';
-				}
-			} else {
-				// 如果键名为空,该行数据无效
-				if('undefined' != typeof json[key])
-				{
-					json[key] = val;
-				}
-			}
-		});
-		$this.val(JSON.stringify(json));
-		$this._html.dialog('close');
-	}
-	
-	/**
-	 * 移除html内容
-	 */
-	function remove($this)
-	{
-		$this._html.remove();
-	}
+    $('.ui-mapeditor .ui-mapeditor-oper span.ui-icon-circle-close').live('click', function(){
+        deleteItem($(this));
+    });
 
-	/**
-	 * 添加新的一行数据
-	 *
-	 * @todo td onclick
-	 */
-	function addRow($this, key, val)
-	{
-		if('undefined' == typeof key) key = '';
-		if('undefined' == typeof val) val = '';
-		var html = $(['<tr><td><input type="text" name="key" value="' + key + '" class="ui-widget-content ui-corner-all" /></td>',
-					'<td><input type="text" name="value" value="' + val + '" class="ui-widget-content ui-corner-all" /></td>',
-					'<td><a onclick="jQuery(this).parent().parent().remove();" class="ui-state-default"><span class="ui-icon ui-icon-circle-close">X</span></a></td></tr>'].join(''));
-		hoverAndFocus(html)
-		$this._table.append(html);
-	}
+    $('.ui-mapeditor-button').click(function(){
+        var number = parseInt($('#ui-mapeditor-number').val());
+        if (0 >= number ) {
+            number = 1;
+        }
+        for (var i = 0; i < number; i++) {
+            addItem();
+        }
+    });
 
-	$.fn.mapEditor.defaults = {
-		dialog: {
-			title: 'MapEditor',
-			bgiframe: true,
-			modal: true,
-			width: 300,
-			height: 'auto',
-			maxHeight: 250
-		},
-		language: {
-			key: 'Key',
-			value: 'Value',
-			operate: 'Operate',
-			add:　'Add a new Row',
-			submit: 'Submit'
-		},
-		data: {
-		}
-	}
-})(jQuery);
+    // 删除一项
+    function deleteItem(object)
+    {
+        var liList = $('.ui-mapeditor li');
+        // 如果是最后一个,则清空数据,不删除
+        if (1 == liList.length) {
+            clearItemForm(liList[0]);
+        } else {
+            object.parents('li').slideUp('normal', function(){
+                $(this).remove();
+            })
+        }
+    }
+
+    // 清空表单数据
+    function clearItemForm(element)
+    {
+        element = $(element);
+        element.find('input').val('');
+        element.find('option:first').attr('selected','selected');
+        return element;
+    }
+
+    // 添加一项
+    function addItem()
+    {
+        clearItemForm($('.ui-mapeditor li:first').clone())
+            .hide()
+            .appendTo($('#ui-mapeditor-sortable'))
+            .slideDown()
+            .find('input:first').val($('.ui-mapeditor li').length);
+    }
+
+    $('#<?php echo $meta['id'] ?>').hide();
+    $( "#ui-mapeditor-sortable" ).sortable({
+        placeholder: 'ui-state-highlight',
+        forcePlaceholderSize: true,
+        opacity: 0.9,
+        scroll: false,
+        create: function () {
+            $(this).find('input')
+                .bind('mousedown.ui-disableSelection selectstart.ui-disableSelection', function(e) {
+                    e.stopImmediatePropagation();
+            });
+        },
+        stop: function () {
+            $(this).find('input')
+                .bind('mousedown.ui-disableSelection selectstart.ui-disableSelection', function(e) {
+                    e.stopImmediatePropagation();
+            });
+        }
+    }).disableSelection();
+
+    // 防止表单提交,转变为添加选项
+    $('#ui-mapeditor-number').keypress(function(e){
+        if(e.which == 13){
+            $('.ui-mapeditor-button').click();
+            return false;
+        }
+    });
+});
---
+++ js/jquery/plugins/optioneditor/jquery.optioneditor.js
@@ -0,0 +1,124 @@
+/**
+ * optioneditor
+ *
+ * Copyright (c) 2008-2010 Twin Huang. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @copyright   Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @version     $Id$
+ * @since       2011-01-20 21:12:58
+ */
+jQuery(function($){
+    // 绑定删除按钮
+    $('#ui-optioneditor-sortable .ui-optioneditor-oper a')
+        .qui()
+        .find('span.ui-icon-circle-close')
+        .live('click', function(){
+            deleteItem($(this));
+        });
+
+    // 点击添加按钮
+    $('.ui-optioneditor-button').click(function(){
+        var number = parseInt($('#ui-optioneditor-number').val());
+        if (0 >= number ) {
+            number = 1;
+        }
+        for (var i = 0; i < number; i++) {
+            addItem();
+        }
+    });
+
+    // 删除一项
+    function deleteItem(object)
+    {
+        var liList = $('.ui-optioneditor li');
+        // 如果是最后一个,则清空数据,不删除
+        if (1 == liList.length) {
+            clearItemForm(liList[0]);
+        } else {
+            object.parents('li').slideUp('normal', function(){
+                $(this).remove();
+            })
+        }
+    }
+
+    // 清空表单数据
+    function clearItemForm(element)
+    {
+        element = $(element);
+        element.find('input').val('');
+        element.find('option:first').attr('selected','selected');
+        return element;
+    }
+
+    // 添加一项
+    function addItem()
+    {
+        // 替换所有表单的名称
+        var html = $('.ui-optioneditor li:first').clone().html();
+        var rand = Math.floor(Math.random() * 10000);
+        html = html.replace(/name=\"(\w+)+\[(.+?)\]\[(.+?)\]\"/g, 'name="$1[' + rand + '][$3]"');
+        clearItemForm($(html))
+            .hide()
+            .appendTo($('#ui-optioneditor-sortable'))
+            .slideDown()
+            .find('input')
+            .bind('mousedown.ui-disableSelection selectstart.ui-disableSelection', function(e){
+                e.stopImmediatePropagation();
+            });        
+    }
+
+    // 排列
+    $('#ui-optioneditor-sortable').sortable({
+        placeholder: 'ui-state-highlight',
+        forcePlaceholderSize: true,
+        opacity: 0.9,
+        scroll: false,
+        create: function () {
+            $(this).find('input')
+                .bind('mousedown.ui-disableSelection selectstart.ui-disableSelection', function(e) {
+                    e.stopImmediatePropagation();
+            });
+        },
+        stop: function () {
+            $(this).find('input')
+                .bind('mousedown.ui-disableSelection selectstart.ui-disableSelection', function(e) {
+                    e.stopImmediatePropagation();
+            });
+        }
+    }).disableSelection();
+
+    // 重置顺序
+    /*$('.ui-optioneditor-reset').click(function(){
+        $('#ui-optioneditor-sortable').sortable();
+    });*/
+
+    // 防止表单提交,转变为添加选项
+    $('#ui-optioneditor-number').keypress(function(e){
+        if(e.which == 13){
+            $('.ui-optioneditor-button').click();
+            return false;
+        }
+        return true;
+    });
+
+    // 至少显示3个选项,如果超过3个,则再显示一个做新增用
+    var i = 3 - $('.ui-optioneditor li').length;
+    do {
+        addItem();
+        i--;
+    } while(i > 0);
+});
