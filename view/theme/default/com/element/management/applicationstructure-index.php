7c28cc0908947b9021234f8dcae4a3bf8ef34b26
itwinh@gmail.com <itwinh@gmail.com@a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5>
2011-03-19 09:15:01 +0000

    [视图]调整目录错误
    git-svn-id: https://miku.googlecode.com/svn/trunk@466 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

---
+++ view/theme/default/com/element/management/applicationstructure-index.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * mangementapplicationstructure
+ *
+ * Copyright (c) 2008-2010 Twin Huang. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package     Trex
+ * @subpackage  View
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @copyright   Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @version     $Id$
+ * @since       2010-09-12 11:11:34
+ */
+// 防止直接访问导致错误
+!defined('QWIN_PATH') && exit('Forbidden');
+?>
+<div class="ui-form ui-box ui-widget ui-widget-content ui-corner-all" id="ui-form">
+    <div class="ui-box-header">
+    	<?php $this->loadWidget('Common_Widget_Header') ?>
+    </div>
+    <form action="" method="post">
+    <div class="ui-form-content ui-box-content ui-widget-content">
+    	<div class="ui-space-10px"></div>
+        <div class="ui-operation-field">
+            <?php echo qw_jQuery_link(qw_url(array('module' => 'Management', 'controller' => 'ApplicationStructure', 'action' => 'Update')), qw_lang('ACT_UPDATE_APPLICATION_STRUCTURE'), 'ui-icon-refresh') ?>
+            <!--<?php echo qw_jQuery_link(qw_url(array('module' => 'Management', 'controller' => 'Package')), qw_lang('ACT_NAMESPACE_LIST'), 'ui-icon-script') ?>
+            <?php echo qw_jQuery_link(qw_url(array('module' => 'Management', 'controller' => 'Package', 'action' => 'Add')), qw_lang('ACT_ADD_NAMESPACE'), 'ui-icon-plus') ?>-->
+            <?php echo qw_jQuery_link('javascript:history.go(-1);', qw_lang('ACT_RETURN'), 'ui-icon-arrowthickstop-1-w') ?>
+        </div>
+        <div class="ui-space-10px"></div>
+        <hr class="ui-line ui-widget-content" />
+    </div>
+    </form>
+</div>
---
+++ view/theme/default/com/element/member/setting-index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * 用户设置中心
+ *
+ * Copyright (c) 2008-2010 Twin Huang. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package     Trex
+ * @subpackage  Member
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @copyright   Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @version     $Id$
+ * @since       2010-08-28 19:29:07
+ */
+// 防止直接访问导致错误
+!defined('QWIN_PATH') && exit('Forbidden');
+?>
+<div class="ui-form ui-box ui-widget ui-widget-content ui-corner-all" id="ui-form">
+	<div class="ui-box-header">
+    	<?php $this->loadWidget('Common_Widget_Header') ?>
+    </div>
+    <form action="" method="post">
+    <div class="ui-form-content ui-box-content ui-widget-content">
+    	<div class="ui-space-10px"></div>
+        <div class="ui-operation-field">
+        <?php 
+        echo qw_jQuery_link(qw_url(array('module' => 'Member', 'controller' => 'Member', 'action' => 'View', 'id' => $member['id'])), qw_lang('LBL_VIEW_DATA'), 'ui-icon-lightbulb'),
+             qw_jQuery_link(qw_url(array('module' => 'Member', 'controller' => 'Member', 'action' => 'Edit', 'id' => $member['id'])), qw_lang('LBL_EDIT_DATA'), 'ui-icon-tag'),
+             qw_jQuery_link(qw_url(array('module' => 'Member', 'controller' => 'Member', 'action' => 'EditPassword', 'id' => $member['id'])), qw_lang('LBL_EDIT_PASSWORD'), 'ui-icon-key'),
+             qw_jQuery_link(qw_url(array('module' => 'Member', 'controller' => 'LoginLog', 'searchField' => 'member_id', 'searchValue' => $member['id'])), qw_lang('LBL_MODULE_MEMBER_LOGINLOG'), 'ui-icon-script'),
+             qw_jQuery_link(qw_url(array('module' => 'Member', 'controller' => 'Setting', 'action' => 'SwitchStyle')), qw_lang('LBL_SWITCH_STYLE'), 'ui-icon-calculator'),
+             qw_jQuery_link(qw_url(array('module' => 'Member', 'controller' => 'Setting', 'action' => 'SwitchLanguage')), qw_lang('LBL_SWITCH_LANGUAGE'), 'ui-icon-script'),
+             qw_jQuery_link('javascript:history.go(-1);', qw_lang('ACT_RETURN'), 'ui-icon-arrowthickstop-1-w')
+        ?>
+        </div>
+        <div class="ui-space-10px"></div>
+    </div>
+    </form>
+</div>
