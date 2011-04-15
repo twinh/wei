ca471041a4496aa8e3b1cf7f2c78ee239b7fa667
itwinh@gmail.com <itwinh@gmail.com@a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5>
2011-04-15 14:16:10 +0000

    [微件]增加Smarty微件
    git-svn-id: https://miku.googlecode.com/svn/trunk@539 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

---
+++ widget/Smarty/Widget.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Widget
+ *
+ * Copyright (c) 2008-2011 Twin Huang. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @copyright   Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @version     $Id$
+ * @since       2011-04-15 22:00:11 v0.7.9
+ */
+
+class Smarty_Widget extends Qwin_Widget_Abstract
+{
+    /**
+     * Smarty对象
+     * @var Smarty
+     */
+    protected $_smarty;
+
+    public function  __construct(array $options = array()) {
+        parent::__construct($options);
+        require_once $this->_rootPath . 'libs/Smarty.class.php';
+        $this->_smarty = Qwin::call('Smarty');
+    }
+
+    /**
+     * 通过魔术方法将微件的方法映射到Smarty对象的方法上.
+     *
+     * @param string $name 调用的方法名称
+     * @param array $arguments 参数数组
+     * @return mixed
+     */
+    public function  __call($name, $arguments) {
+        return call_user_func_array(array($this->_smarty, $name), $arguments);
+    }
+
+    /**
+     * 获取Smarty对象
+     * @return Smarty
+     */
+    public function getObject()
+    {
+        return $this->_smarty;
+    }
+}
