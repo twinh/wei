6276896439194947eec1e11234ef9af50041bfa9
twinh <twinhuang@qq.com>
2011-07-25 10:04:40 +0000

    [视图]调整视图路径
    git-svn-id: https://miku.googlecode.com/svn/trunk@746 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- widgets/ListAction/view/default.php
+++ widgets/ListAction/view/default.php
@@ -29,7 +29,90 @@ echo $this->getPackerSign();
 <div class="qw-middle">
     <div class="qw-middle-content">
         <div class="qw-p5">
-        <?php require $this->decodePath('<root>com/basic/jqgird<suffix>') ?>
+        <div class="ui-jqgrid-top">
+    <?php Qwin::hook('viewListTop', array('view' => $this)) ?>
+</div>
+<div class="qw-c"></div>
+<?php $jqGridWidget->render($jqGridOptions) ?>
+<script type="text/javascript">
+jQuery(function($){
+    var primaryKey = '<?php echo $id?>';
+    var jqGridObj = qwin.jqGrid = $('#<?php echo $jqGridWidget->getId() ?>');
+
+    jqGridObj.jqGrid('setGridParam',{
+        ondblClickRow: function(rowId, iRow, iCol, e){
+            var rowData = jqGridObj.jqGrid('getRowData', rowId),
+                addition = {};
+            addition['action'] = 'view';
+            addition[primaryKey] = rowData[primaryKey];
+            window.location.href = qwin.url.createUrl(qwin.get, addition);
+            return false;
+    }});
+
+    if (document.getElementById('ui-box-tab')) {
+        jqGridObj.jqGrid('setGridWidth', $('#ui-box-tab').width() - 30);
+    }
+
+    // 点击删除按钮
+    $('#action-<?php echo $module->getId() ?>-delete').click(function(){
+        var keyList = new Array(),
+            rowList = jqGridObj.jqGrid('getGridParam','selarrrow');
+        if (rowList.length == 0) {
+            alert(qwin.lang.MSG_CHOOSE_AT_LEASE_ONE_ROW);
+            return false;
+        }
+        for (var i in rowList) {
+            var rowData = jqGridObj.jqGrid('getRowData', rowList[i]);
+            keyList[i] = rowData[primaryKey];
+        }
+        var addition = {};
+        addition['action'] = 'delete';
+        addition[primaryKey] = keyList.join(',');
+
+        var message = '';
+        if ($(this).find('span').hasClass('ui-icon-trash')) {
+            message = qwin.lang.MSG_CONFIRM_TO_DELETE_TO_TRASH;
+        } else {
+            message = qwin.lang.MSG_CONFIRM_TO_DELETE;
+        }
+        if (confirm(message)) {
+            window.location.href = qwin.url.createUrl(qwin.get, addition);
+        }
+        return false;
+    });
+
+    // 点击复制按钮
+    $('#action-<?php echo $module->getId() ?>-copy').click(function(){
+        var rowList = jqGridObj.jqGrid('getGridParam','selarrrow');
+        if (rowList.length != 1) {
+            alert(qwin.lang.MSG_CHOOSE_ONLY_ONE_ROW);
+            return false;
+        }
+        var rowData = jqGridObj.jqGrid('getRowData', rowList[0]);
+        var url = $(this).attr('href') + '&' + primaryKey + '=' + rowData[primaryKey];
+        window.location.href = url;
+        return false;
+    });
+
+    // 根据窗口变化设置jqGrid宽度等
+    // 修复360极速浏览器(6.0Chrome内核)宽度不正确的问题
+    $(window).load(function() {
+        setJqGridWidth();
+    }).resize(function(){
+        setJqGridWidth();
+    });
+    qwin.page.splitter.bind('toggle', function(){
+        setJqGridWidth();
+    });
+    function setJqGridWidth() {
+        // 窗口宽度 - 左栏宽度 - 分割栏宽度 - 左栏宽度 - 分割栏宽度
+        var leftWidth = 'none' == qwin.page.left.css('display') ? 0 : qwin.page.left.width();
+        var rightWidth = 'none' == qwin.page.right.css('display') ? 0 : qwin.page.right.width();
+        var width = $(window).width() - leftWidth - rightWidth - (2 * qwin.page.splitter.width()) - 16;
+        jqGridObj.jqGrid('setGridWidth', width);
+    }
+});
+</script>
         </div>
     </div>
 </div>
