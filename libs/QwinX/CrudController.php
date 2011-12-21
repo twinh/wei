af58b39d9e233363a09767fba54054ea253c45ea
itwinh@gmail.com <itwinh@gmail.com@a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5>
2011-12-21 03:21:23 +0000

    [应用]移动视图文件
    git-svn-id: https://miku.googlecode.com/svn/trunk@913 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

---
+++ libs/QwinX/CrudController.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Widget
+ *
+ * Copyright (c) 2008-2011 Twin Huang. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @copyright   Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @version     $Id$
+ * @since       2011-06-03 14:45:19
+ */
+
+class Qwin_CrudController extends Qwin_Controller
+{
+    /**
+     * 首页,默认显示列表记录
+     */
+    public function indexAction()
+    {
+        return $this->listAction();
+    }
+
+    /**
+     * 显示列表记录
+     */
+    public function listAction()
+    {
+        if ($this->request->isJson()) {
+            return $this->widget->jsonListAction(array(
+                'jqGrid'    => $this->options['jqGrid'],
+                'record'    => $this->record(),
+                'layout'    => $this->get('layout'),
+                'search'    => $this->get('search'),
+                'page'      => $this->get('page'),
+                'row'       => $this->get('rows'),
+                'order'     => array(
+                    $this->get('sidx'),
+                    $this->get('sord')
+                ),
+            ));
+        } else {
+            return $this->widget->listAction(array(
+                'jqGrid'    => $this->options['jqGrid'],
+                'layout'    => $this->get('layout'),
+                'row'       => $this->get('row'),
+            ));
+        }
+    }
+
+    /**
+     * 查看一条记录    
+     */
+    public function viewAction()
+    {
+        return $this->widget->viewAction(array(
+            'form'      => $this->options['form'],
+            'record'    => $this->record(),
+            'id'        => $this->get('id'),
+        ));
+    }
+
+    /**
+     * 添加记录
+     */
+    public function addAction()
+    {
+        if (!$this->request->isPost()) {
+            return $this->widget->addFormAction(array(
+                'form'      => $this->options['form'],
+                'record'    => $this->record(),
+                'id'        => $this->get('id'),
+            ));
+        } else {
+            return $this->widget->addAction(array(
+                'record'    => $this->record(),
+                'data'      => $_POST,
+                'url'       => urldecode($this->post('_page')),
+            ));
+        }
+    }
+
+    /**
+     * 编辑记录
+     */
+    public function editAction()
+    {
+        if (!$this->request->isPost()) {
+            return $this->widget->editFormAction(array(
+                'form'      => $this->options['form'],
+                'record'    => $this->record(),
+                'id'        => $this->get('id'),
+            ));
+        } else {
+            return $this->widget->editAction(array(
+                'record'    => $this->record(),
+                'data'      => $_POST,
+                'url'       => urldecode($this->post('_page')),
+            ));
+        }
+    }
+
+    /**
+     * 删除记录
+     */
+    public function deleteAction()
+    {
+        return $this->widget->deleteAction(array(
+            'record'    => $this->record(),
+            'id'        => $this->get('id'),
+        ));
+    }
+}
