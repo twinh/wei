3bd3f0d791c9e9079953364574e79a5392e1c945
itwinh@gmail.com <itwinh@gmail.com@a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5>
2012-01-01 03:49:59 +0000

    [应用]调整类注释日期
    git-svn-id: https://miku.googlecode.com/svn/trunk@952 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- apps/Index/views/updatelog-content.php
+++ apps/Index/views/updatelog-content.php
@@ -2,7 +2,7 @@
 /**
  * updatelog
  *
- * Copyright (c) 2008-2011 Twin Huang. All rights reserved.
+ * Copyright (c) 2008-2012 Twin Huang. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
