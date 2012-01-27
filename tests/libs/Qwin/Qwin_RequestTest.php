1693a8241aa507ec73b78c45955e1ff62fcda367
twinh <twinhuang@qq.com>
2012-01-27 02:47:39 +0000

    [Widget]Rewrited request and make is as a widget [Test]Added request widget test
    git-svn-id: https://miku.googlecode.com/svn/trunk@1062 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- tests/libs/Qwin/Qwin_RequestTest.php
+++ tests/libs/Qwin/Qwin_RequestTest.php
@@ -27,218 +27,23 @@ class Qwin_RequestTest extends PHPUnit_Framework_TestCase {
      * This method is called after a test is executed.
      */
     protected function tearDown() {
-        
+
     }
 
     /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testGet().
+     * @covers Qwin_Request::call
      */
-    public function testGet() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+    public function testCall()
+    {
+        $name = $this->object->call('name');
+        $source = isset($_REQUEST['name']) ? $_REQUEST['name'] : null;
 
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testAddGet().
-     */
-    public function testAddGet() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+        $this->assertEquals($name->source, $source);
 
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testGetGet().
-     */
-    public function testGetGet() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+        $default = 'default';
+        $name2 = $this->object->call('name', $default);
+        $source = isset($_REQUEST['name']) ? $_REQUEST['name'] : $default;
 
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testGetPost().
-     */
-    public function testGetPost() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $this->assertEquals($name2->source, $default);
     }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testPost().
-     */
-    public function testPost() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testAddPost().
-     */
-    public function testAddPost() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testCookie().
-     */
-    public function testCookie() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testAddCookie().
-     */
-    public function testAddCookie() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testRequest().
-     */
-    public function testRequest() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testAddRequest().
-     */
-    public function testAddRequest() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testServer().
-     */
-    public function testServer() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testIsPost().
-     */
-    public function testIsPost() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testIsGet().
-     */
-    public function testIsGet() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testOffsetExists().
-     */
-    public function testOffsetExists() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testOffsetGet().
-     */
-    public function testOffsetGet() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testOffsetSet().
-     */
-    public function testOffsetSet() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testOffsetUnset().
-     */
-    public function testOffsetUnset() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testIsAjax().
-     */
-    public function testIsAjax() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers {className}::{origMethodName}
-     * @todo Implement testIsJson().
-     */
-    public function testIsJson() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
 }
-
-?>
