446d7a89f91ecf962c68e616070ae518f255a418
itwinh@gmail.com <itwinh@gmail.com@a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5>
2012-02-15 02:50:28 +0000

    [Library]Deleted useless file
    git-svn-id: https://miku.googlecode.com/svn/trunk@1144 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- libs/Qwin/Util/File.php
+++ libs/Qwin/Util/File.php
@@ -28,18 +28,12 @@
 class Qwin_Util_File
 {
     /**
-     * 系统是否为Windows
-     * @var boolen|null
-     */
-    protected static $_inWin = null;
-
-    /**
      * 将内容以数组的形式写入文件中
      *
      * @param mixed $array 内容
      * @param string $path 路径,如果不存在,会自动创建
      * @param string|null $name 如果存在,则以此作为数组名字
-     * @return <type> 
+     * @return <type>
      */
     public static function writeArray($path, $array, $name = null)
     {
@@ -52,11 +46,11 @@ class Qwin_Util_File
         } else {
             $array = "<?php\nreturn $array;";
         }
-        
+
         if (!file_put_contents($path, $array)) {
             throw new Qwin_Util_Exception('Can not write into the file "' . $path . '"');
         }
-        
+
         return true;
     }
 
@@ -86,25 +80,6 @@ class Qwin_Util_File
         fwrite($fp, $code);
         fclose($fp);
         return true;
-    }
-
-    /**
-     * 创建路径
-     *
-     * @param string $path 路径
-     */
-    public static function makePath($path)
-    {
-        $path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
-        $pathArray = explode(DIRECTORY_SEPARATOR, $path);
-        unset($pathArray[count($pathArray) - 1]);
-        $pathTemp = '';
-        foreach ($pathArray as $path) {
-            $pathTemp .= $path . DIRECTORY_SEPARATOR;
-            if (!is_dir($pathTemp)) {
-                mkdir($pathTemp);
-            }
-        }
     }
 
     /**
@@ -141,76 +116,6 @@ class Qwin_Util_File
         }
         return implode('/', $relPath);
     }
-    
-    /*
-    * 从指定的路径中读取文件,或文件夹, 使用回调函数,对文件进行处理
-    * @param $path string 起始的路径
-    * @param $callback mixed 回调函数,包括一般函数,类,静态类, 包含2个参数,依次是 文件(夹)名称, 文件路径
-    * @param $level int 查找路径的文件夹层数, 0 表示所有, 1 表示当前层, 2 表示到第二层,依次类推
-    * @param $type string 回调函数作用的范围,包括 all, dir, file
-    * @todo 限定数目等
-    */
-    /*function scanPath($path, $callback, $level = 0, $type = 'all')
-    {
-        $file_arr = scandir($path);
-        foreach ($file_arr as $val) {
-            if ($val == '.' || $val == '..') {
-                continue;
-            // 目录
-            } elseif (true == is_dir($path . DS . $val)) {
-                self::_evalScanPathCallBack($callback, $val, $path);
-                self::scanPath($path . $val . DS, $callback);
-            // 文件
-            } else {
-                self::_evalScanPathCallBack($callback, $val, $path);
-            }
-        }        
-    }*/
-    
-    // 执行 scanPath 方法的回调函数
-    // TODO 动态类 array($class, $method) 静态类 array('class', 'method');
-    /*private function _evalScanPathCallBack($callback, $file, $path)
-    {
-        if (true == is_array($callback)) {
-            if (true == is_object($callback[0])) {
-                $callback[0] = get_class($callback[0]);
-            }
-            return eval($callback[0] . '::' . $callback[1] . '("' . $file . '", "' . qw('-str')->decodeEvalVarCode($path) . '");');
-        } else {
-            return $callback($file, $path);
-        }
-    }*/
-    
-    /**
-     * 判断文件是否存在,区分名称大小写
-     * 
-     * @param string $file
-     */
-    public static function isExist($file)
-    {
-        if(!is_file($file)) {
-            return false;
-        }
-        // 如果是前面的路径不一致?
-        if (self::_inWin() && basename(realpath($file)) != basename($file)) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * 检测系统是否为Windows
-     *
-     * @return boolen
-     */
-    protected static function _inWin()
-    {
-        if (null !== self::$_inWin) {
-            return self::$_inWin;
-        }
-        self::$_inWin = 'WIN' == substr(PHP_OS, 0, 3) ? true : false;
-        return self::$_inWin;
-    }
 
     /**
      * 过滤文件名中的非法字符
@@ -225,7 +130,7 @@ class Qwin_Util_File
 
     /**
      * 转换路径分隔符为网址路径
-     * 
+     *
      * @param string $path 路径
      * @return string 路径
      */
