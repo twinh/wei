39582c7f1ccefd8ae08e1f74c76224d361ba9fa5
itwinh@gmail.com <itwinh@gmail.com@a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5>
2012-02-19 09:47:45 +0000

    [Widget]Added sort widget
    git-svn-id: https://miku.googlecode.com/svn/trunk@1176 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

---
+++ libs/Qwin/Sort.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Qwin Framework
+ *
+ * Copyright (c) 2008-2012 Twin Huang. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @copyright   Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @version     $Id$
+ */
+
+/**
+ * Sort
+ *
+ * @package     Qwin
+ * @subpackage  Widget
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @since       2012-02-17 23:48:41
+ */
+class Qwin_Sort extends Qwin_Widget
+{
+    /**
+     * Sort two-dimensional array like SQL ORDER BY clause
+     *
+     * @param array $array the two-dimensional array
+     * @param string $key the array to be sort
+     * @param int $type sort in ascending or descending order
+     * @return array
+     */
+    public function call(array $array, $key = 'order', $type = SORT_ASC)
+    {
+        if (!$array) {
+            return $array;
+        }
+        $array2 = array();
+        foreach ($array as $k => $v) {
+            $array2[$k] = $v[$key];
+        }
+        array_multisort($array2, $type, $array);
+        return $array;
+    }
+}
