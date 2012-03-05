c9db090add9e7423fc131af59cd730700f2c0201
twinh <twinhuang@qq.com>
2012-03-05 15:36:09 +0000

    [Application]Added user module index view
    git-svn-id: https://miku.googlecode.com/svn/trunk@1216 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- apps/User/views/index.php
+++ apps/User/views/index.php
@@ -10,11 +10,64 @@ $minify->add(array(
     $jQuery->getDir() . '/jqGrid/i18n/grid.locale-cn.js',
     $this->getFile('views/style.js'),
     $this->getFile('views/style.css'),
-    $this->getFile('views/form/default.css'),
 ));
 ?>
 </head>
 <body>
+<script type="text/javascript">
+jQuery(function($){
+    $('#user-grid').jqGrid({
+        sortorder: 'id',
+        datatype: 'json',
+        width: '100%',
+        height: 'auto',
+        forceFit: true,
+        autowidth: true,
+        rownumbers: true,
+        multiselect: true,
+        viewrecords: true,
+        pager: '#user-grid-pager',
+        rowNum: 15,
+        rowList: [15, 30, 50, 100, 500],
+        colNames: [
+            '编号', '分组', '用户名', '邮箱', '性别', '修改时间', '操作'
+        ],
+        colModel: [{
+            name: 'id',
+            hidden: true
+        }, {
+            name: 'group_id',
+            formatter: function(value) {
+                return value;
+            }
+        }, {
+            name: 'username'
+        }, {
+            name: 'email'
+        }, {
+            name: 'sex',
+            align: 'center',
+            formatter: function(value) {
+                return 1 == value ? '男' : '女';
+            }
+        }, {
+            name: 'date_modified',
+            align: 'center'
+        }, {
+            name: 'operation',
+            align: 'center',
+            formatter: function(cellvalue, options, rowObject) {
+                return '<a href="?module=user&action=edit&id=' + rowObject[0] + '">编辑</a> | <a href="#">删除</a>';
+            }
+        }]
+    }).jqGrid('navGrid', '#user-grid-pager', {
+        add : false,
+        edit : false,
+        del : false,
+        search : false
+    }).jqGrid('fullContainer');
+});
+</script>
 <table id="user-grid"></table>
 <div id="user-grid-pager"></div>
 </body>
