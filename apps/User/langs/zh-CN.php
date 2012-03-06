be54aff4a5236270854c35d51c3137e998658bd1
twinh <twinhuang@qq.com>
2012-03-06 16:26:55 +0000

    [Application]Deleted useless files
    git-svn-id: https://miku.googlecode.com/svn/trunk@1225 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- apps/User/langs/zh-CN.php
+++ apps/User/langs/zh-CN.php
@@ -1,32 +1,6 @@
 <?php
-/**
- * Setting
- *
- * Copyright (c) 2008-2012 Twin Huang. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @package     Com
- * @subpackage  Member
- * @author      Twin Huang <twinh@yahoo.cn>
- * @copyright   Twin Huang
- * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
- * @version     $Id$
- * @since       2010-05-19 13:49:27
- */
-
 return array(
-    'Member' => '用户',
+    'User' => '用户',
     'Group id' => '分组编号',
     'Username' => '用户名',
     'Email' => '邮箱',
@@ -43,56 +17,4 @@ return array(
     'Mobile' => '手机',
     'Homepage' => '主页',
     'Address' => '地址',
-
-    'FLD_LANG' => '语言代号',
-    'FLD_GROUP_ID' => '分组',
-    'FLD_NEW_PASSWORD' => '新密码',
-    'FLD_OLD_PASSWORD' => '旧密码',
-    'FLD_PASSWORD2' => '再次输入密码',
-    'FLD_CONFIRM_PASSWORD' => '确认密码',
-    'FLD_MONEY' => '金额',
-
-    
-    'GRP_CUSTOMER' => '界面配置',
-
-    'LBL_MODULE_MEMBER_DETAIL' => '用户详细',
-    'LBL_MODULE_MEMBER_SETTING' => '用户配置',
-
-    'ACT_LOGIN' => '登陆',
-    'ACT_LOGOUT' => '注销',
-
-    'LBL_THEME' => '主题',
-    'LBL_LANGUAGE' => '语言',
-
-    'LBL_APPLY_TO_SETTING' => '应用当前风格到个人配置中',
-    'LBL_LOGIN_TITLE' => '欢迎您使用本系统',
-    'VLD_VALIDATEPASSWORD' => '用户名或密码错误',
-    'VLD_VALIDATEUSERNAME' => '用户名已经存在',
-    'VLD_VALIDATEEMAIL' => '邮箱已经存在',
-    'MSG_LOGINED' => '您已经登陆,要重新登录请先注销。',
-    'MSG_NOT_LOGIN' => '您还未登陆',
-    'MSG_USERNAME_EXISTS' => '用户名已存在.',
-    'MSG_MEMBER_LOCKED' => '帐号已锁定，无法执行该操作。',
-
-
-    'VLD_VALIDATEOLDPASSWORD' => '旧密码不正确',
-    'MSG_MEMBER_NOT_ALLOW_DELETE' => '不允许删除系统用户',
-    'MSG_GUEST_NOT_ALLOW_EDIT_PASSWORD' => '不允许修改游客密码',
-
-    'FLD_IMAGE_PATH' => '图像路径',
-    'FLD_QQ' => 'QQ',
-    'FLD_POSTCODE' => '邮编',
-    'FLD_MEMBER_ID' => '用户编号',
-    'FLD_THEME' => '主题',
-    'FLD_LANGUAGE' => '语言',
-    'FLD_IP' => 'Ip',
-    'LBL_MODULE_MEMBER_GROUP' => '用户组',
-    
-    
-    'ACT_ALLOCATEPERMISSION' => '分配权限',
-    'ACT_EDIT_PASSWORD' => '修改密码',
-    'ACT_SWITCHLANGUAGE' => '切换语言',
-    'ACT_SWITCHSTYLE' => '切换风格',
-    'MSG_LOGOUTED' => '您已登出.',
-    'LBL_LOGIN' => '登录',
 );
