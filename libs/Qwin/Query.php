a6edfbaeca66caba520d2ec6b69e83e841939c3b
itwinh@gmail.com <itwinh@gmail.com@a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5>
2012-03-06 16:04:47 +0000

    [Widget]Simplify query and record widget
    git-svn-id: https://miku.googlecode.com/svn/trunk@1223 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- libs/Qwin/Query.php
+++ libs/Qwin/Query.php
@@ -24,7 +24,7 @@
 
 /**
  * Query
- * 
+ *
  * @package     Qwin
  * @subpackage  Application
  * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
@@ -40,11 +40,11 @@ class Qwin_Query extends Doctrine_Query
     protected static $_orderTypes = array(
         'DESC', 'ASC'
     );
-    
+
     /**
      * 数据查询类型
      * @var array
-     * @todo 是否使用%s替换 
+     * @todo 是否使用%s替换
      */
     protected static $_searchTypes = array(
         'eq' => '=',
@@ -62,72 +62,28 @@ class Qwin_Query extends Doctrine_Query
         'cn' => 'LIKE',
         'nc' => 'NOT LIKE',
     );
-    
+
     /**
      * 查询对应的记录对象
      * @var Doctrine_Record
      */
     protected $_record;
-    
+
     public function __construct(Doctrine_Connection $connection = null, Doctrine_Hydrator_Abstract $hydrator = null)
     {
         Qwin_Record::connect();
         parent::__construct($connection, $hydrator);
     }
-    
+
     public function __invoke($name = null, $module = null)
     {
         $widget = Qwin::getInstance();
         $record = $widget->record($name, $module);
         $class = get_class($record);
-        
+
         $query = Doctrine_Query::create(null, __CLASS__)->from($class);
         $query->_record = $record;
         return $query;
-
-        // 根据别名和索引构建DQL的from语句
-        $from = $recordClass;
-        if ($meta['alias']) {
-            $from .= ' ' . $meta['alias'];
-        }
-        if ($meta['indexBy']) {
-            $from .= ' INDEXBY ' . $meta['indexBy'];
-        }
-
-        // 初始化查询
-        // TODO 更多选项,如缓存
-        $query = Doctrine_Query::create(null, __CLASS__)->from($from);
-        
-        // 将元数据存储在查询对象中,方便获取其关联关系
-        $query->_record = $record;
-
-        // 增加默认查询
-        if (!empty($this['where'])) {
-            $query->addRawWhere($this['where']);
-        }
-    }
-
-    public function leftJoinByType($type)
-    {
-        $types = is_array($type) ? $type : array($type);
-        foreach ($this->_record->options['relations'] as $alias => $relation) {
-//            if (in_array($relation['type'], $types)) {
-//                $this->leftJoin($this->getRootAlias() . '.' . $alias . ' ' . $alias);
-//            }
-        }
-        return $this;
-    }
-    
-    public function leftJoinByAlias($name)
-    {
-        !is_array($name) && $name = array($name);
-        foreach ($name as $alias) {
-            if (!isset($this->_meta['relations'][$alias])) {
-                throw new Qwin_Widget_Exception('Undefined relation "' . $alias . '"');
-            }
-            $this->leftJoin($this->getRootAlias() . '.' . $alias . ' ' . $alias);
-        }
-        return $this; 
     }
 
     /**
@@ -144,21 +100,18 @@ class Qwin_Query extends Doctrine_Query
             return $this;
         }
 
-        // 排序为字符串形式,进行分割
-        if (is_string($order)) {
-            $order = Qwin_Util_String::splitQuery($order);
-        }
-
         // 排序只有一项,补全数组
         if (isset($order[0]) && (is_string($order[0])) || is_object($order[0])) {
             $order = array($order);
         }
-        
+
         $alias = $this->getRootAlias();
         $alias && $alias .= '.';
 
+        $columns = $this->_record->getTable()->getColumns();
+
         foreach ($order as $field) {
-            if (!isset($field[0]) || !isset($this->_record->options['fields'][(string)$field[0]])) {
+            if (!isset($field[0]) || !isset($columns[(string)$field[0]])) {
                 continue;
             }
             $field[1] = strtoupper($field[1]);
@@ -259,76 +212,5 @@ class Qwin_Query extends Doctrine_Query
     protected function _escapeWildcard($value)
     {
         return strtr($value, array('%' => '\%', '_' => '\_'));
-    }
-
-    /**
-     * 为Doctrine查询对象增加偏移语句
-     *
-     * @param Doctrine_Query $query
-     * @param int|null $addition 附加的偏移配置
-     * @return Com_Meta 当前对象
-     */
-    public function addRawOffset(Qwin_Meta_Db $db, $offset)
-    {
-        $offset = intval($offset);
-        $offset = $offset < 0 ? 0 : $offset;
-        $this->offset($offset);
-        return $this;
-    }
-
-    /**
-     * 为Doctrine查询对象增加限制语句
-     *
-     * @param Doctrine_Query $query
-     * @param int|null $addition 附加的限制配置
-     * @return Com_Meta 当前对象
-     */
-    public function addRawLimit(Qwin_Meta_Db $db, $addition = null)
-    {
-        $limit = 0;
-        if (null != $addition) {
-            $addition = intval($addition);
-            if (0 < $addition) {
-                $limit = $addition;
-            }
-        }
-        $this->limit($limit);
-        return $this;
-    }
-
-    /**
-     * 添加字段到SELECT查询中
-     * 
-     * @return Query_Widget 当前对象
-     */
-    public function addRawSelect()
-    {
-        /**
-         * 设置主类的查询语句
-         */
-        foreach ($this->_meta['fields'] as $name => $field) {
-            if ($field['dbQuery'] && $field['dbField']) {
-                $this->addSelect($name);
-            }
-        }
-        
-        /**
-         * 设置关联类的查询语句
-         */
-        //foreach ((array)$this->offsetLoad('meta') as $meta2) {
-//            $linkedMetaObj = self::getByModule($meta2['module']);
-//
-//            // 调整主键的属性,因为查询时至少需要选择一列
-//            $id = $linkedMetaObj['db']['id'];
-//            $linkedMetaObj->fields
-//                          ->setAttr($id, 'isDbField', true)
-//                          ->setAttr($id, 'isDbQuery', true);
-//
-//            $queryField = $linkedMetaObj->fields->getAttrList(array('isDbQuery', 'isDbField'));
-//            foreach ($queryField as $field) {
-//                $query->addSelect($meta2['alias'] . '.' . $field);
-//            }
-        //}
-        return $this;
     }
 }
--- libs/Qwin/Record.php
+++ libs/Qwin/Record.php
@@ -39,9 +39,7 @@ class Qwin_Record extends Doctrine_Record
      */
     protected static $_connected = false;
 
-    public $invoker;
-
-    public $source;
+    public $__invoker;
 
     public static $dbOptions = array(
         'type'      => 'mysql',
@@ -55,114 +53,12 @@ class Qwin_Record extends Doctrine_Record
         'collate'   => 'utf8_general_ci',
     );
 
-    /**
-     * @var array $_defaults        默认选项
-     *
-     *      -- name                 名称
-     *
-     *      -- title                标题
-     *
-     *      -- description          域描述
-     *
-     *      -- order                排序
-     *
-     *      -- dbField              是否为数据库字段
-     *
-     *      -- dbQuery              是否允许数据库查询
-     *
-     *      -- urlQuery             是否允许Url查询
-     *
-     *      -- readonly             是否只读
-     */
-    protected $_fieldDefaults = array(
-        'name'          => null,
-        'title'         => null,
-        'description'   => array(),
-        'dbField'       => true,
-        'dbQuery'       => true,
-        'urlQuery'      => true,
-        'readonly'      => false,
-        // TODO 扩展
-        //'type'          => 'string',
-        //'length'        => null,
-        //'default'       => null,
-        //'unsigned'      => false,
-        //'notnull'       => false,
-        //'primary'       => false,
-        //'fixed'         => false,
-    );
-
-    protected $_relationDefaults = array(
-        'module'    => null,
-        'alias'     => null,
-        'meta'      => 'db',
-        'relation'  => 'hasOne',
-        'local'     => 'id',
-        'foreign'   => 'id',
-        'type'      => 'db',
-        'fieldMap'  => array(), // ?是否仍需要
-        'enabled'   => true,
-    );
-
-    /**
-     * 默认选项
-     * @var array
-     */
-    public $options = array(
-        'fields'    => array(),
-        //'type'      => 'sql',
-        'uid'       => 'db',
-        'table'     => null,
-        'id'        => 'id',
-        'alias'     => null,
-        'indexBy'   => null,
-        'offset'    => 0,
-        'limit'     => 10,
-        'order'     => array(),
-        'where'     => array(),
-        'relations' => array(),
-    );
-
     public function __construct($table = null, $isNewEntry = false)
     {
         // 保证连接数据库链接上,否则出现异常
         self::connect((array)$table + self::$dbOptions);
 
-        $this->getRecordData();
-
         parent::__construct($table, $isNewEntry);
-    }
-
-    /**
-     * 将数据格式化并加入
-     *
-     * @param array $data 数据
-     * @param array $option 选项
-     * @return Qwin_Meta_Field 当前对象
-     */
-    public function merge2($data, array $options = array())
-    {
-        $data = (array)$data + $this->_defaults;
-        !is_array($data['fields']) && (array)$data['fields'];
-
-        // 处理通配选项
-        if (array_key_exists('*', $data['fields'])) {
-            $this->_fieldDefaults = $this->_fieldDefaults + (array)$data['fields']['*'];
-            unset($data['fields']['*']);
-        }
-
-        foreach ($data['fields'] as $name => &$field) {
-            !isset($field['name']) && $field['name'] = $name;
-            //!isset($field['title']) && $field['title'] = 'FLD_' . strtoupper($name);
-            $field = (array)$field + $this->_fieldDefaults;
-        }
-
-        foreach ($data['relations'] as &$relation) {
-            $relation += $this->_relationDefaults;
-        }
-
-        $this->exchangeArray($data);
-        return $this;
     }
 
     /**
@@ -179,53 +75,7 @@ class Qwin_Record extends Doctrine_Record
             $module = $widget->module();
         }
         $class = ucfirst($module) . '_' . ucfirst($name) . 'Record';
-        return $widget->call($class);
-    }
-
-    /**
-     * 设置字段属性和关联关系
-     *
-     * @return void
-     */
-    public function setTableDefinition()
-    {
-        $data = $this->options;
-        if (empty($data)) {
-            return $this;
-        }
-
-        // 设置数据表
-        $this->setTableName($data['table']);
-
-        // 设置字段
-        foreach ($data['fields'] as $name => $field) {
-            //if ($field['dbField'] && $field['dbQuery']) {
-                $this->hasColumn($name);
-            //}
-        }
-
-        // 设置关联
-        /*foreach ($data['relations'] as  $relation) {
-            $class = self::getByModule($relation['module'], $relation['meta'], false);
-            call_user_func(
-                array($this, $relation['relation']),
-                $class . ' as ' . $relation['alias'],
-                array(
-                    'local' => $relation['local'],
-                    'foreign' => $relation['foreign']
-                )
-            );
-        }*/
-    }
-
-    /**
-     * 获取记录配置
-     *
-     * @return array
-     */
-    public function getRecordData()
-    {
-        return array();
+        return $widget->__invoke($class);
     }
 
     /**
