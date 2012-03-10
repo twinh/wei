67a1779025b7cc860b546ebb38c69380c1c29471
twinh <twinhuang@qq.com>
2012-03-10 03:22:46 +0000

    [Application]Fixed menu module
    git-svn-id: https://miku.googlecode.com/svn/trunk@1232 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- apps/Menu/Controller.php
+++ apps/Menu/Controller.php
@@ -25,10 +25,103 @@
  * @since       2010-05-25 07:59:33
  */
 
-class User_Menu_Controller extends Qwin_Controller
+class Menu_Controller extends Qwin_Controller
 {
     public function indexAction()
     {
-        
+        if ($this->isAjax()) {
+            $rows = $this->getInt('rows', 1, 500);
+
+            $page = $this->getInt('page', 1);
+
+            $query = $this->query()
+                ->leftJoin('Menu_Record.category')
+                ->addRawOrder(array($this->get('sidx'), $this->get('sord')))
+                ->offset(($page - 1) * $rows)
+                ->limit($rows);
+
+            $data = $query->fetchArray();
+            foreach ($data as &$row) {
+                if ($row['category']) {
+                    $row['category_id'] = $row['category']['title'];
+                } else {
+                    $row['category_id'] = '-';
+                }
+            }
+
+            $total = $query->count();
+
+            return $this->jQGridJson(array(
+                'columns' => array('id', 'category_id', 'title', 'url', 'target', 'order', 'operation'),
+                'data' => $data,
+                'page' => $page,
+                'rows' => $rows,
+                'total' => $total,
+            ));
+        }
+    }
+
+    public function editAction()
+    {
+        if ($this->isPost()) {
+            $id = $this->post('id');
+
+            $data = $this->query()
+                ->where('id = ?', $id)
+                ->fetchOne();
+
+            if (!$data) {
+                $this->error('Menu is not exists');
+            }
+
+            $data->fromArray($this->post->toArray());
+
+            return json_encode(array(
+                'code' => 0,
+                'message' => '编辑成功',
+            ));
+        } else {
+            $id = $this->get('id');
+
+            $data = $this->query()
+                ->where('id = ?', $id)
+                ->fetchOne();
+
+            if (!$data) {
+                $this->error('Menu is not exists');
+            }
+
+            $data = json_encode($data->toArray());
+
+            $categorys = json_encode($this->record()->getCagetoryOptions());
+
+            $this->view->assign(get_defined_vars());
+        }
+    }
+
+    public function addAction()
+    {
+        if ($this->isPost()) {
+            $id = $this->post('id');
+
+            $data = $this->query()
+                ->where('id = ?', $id)
+                ->fetchOne();
+
+            if (!$data) {
+                $this->error('Menu is not exists');
+            }
+
+            $data->fromArray($this->post->toArray());
+
+            return json_encode(array(
+                'code' => 0,
+                'message' => '编辑成功',
+            ));
+        } else {
+            $categorys = json_encode($this->record()->getCagetoryOptions());
+
+            $this->view->assign(get_defined_vars());
+        }
     }
 }
