e43fee4d6289c919f12c16106e77968e902ebad8
itwinh@gmail.com <itwinh@gmail.com@a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5>
2012-03-12 03:10:26 +0000

    [Application]Added javascript function: formatUrl
    git-svn-id: https://miku.googlecode.com/svn/trunk@1240 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- apps/views/style.js
+++ apps/views/style.js
@@ -60,7 +60,16 @@ jQuery(function($){
     qwin.format = function() {
         var s = arguments[0];
         for (var i = 0; i < arguments.length - 1; i++) {
-            var reg = new RegExp("\\{" + i + "\\}", "gm");
+            var reg = new RegExp('\\{' + i + '\\}', 'gm');
+            s = s.replace(reg, arguments[i + 1]);
+        }
+        return s;
+    }
+
+    qwin.formatUrl = function() {
+        var s = arguments[0];
+        for (var i = 0; i < arguments.length - 1; i++) {
+            var reg = new RegExp('%7B' + i + '%7D', 'gm');
             s = s.replace(reg, arguments[i + 1]);
         }
         return s;
