ada194a9e76fc221f65bf916a72651844dd2356b
itwinh@gmail.com <itwinh@gmail.com@a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5>
2012-06-30 09:03:59 +0000

    [Widget]Simply minify widget
    git-svn-id: https://miku.googlecode.com/svn/trunk@1270 a5e82c4f-c0ae-4e74-87f7-1291ebdd7ca5

--- libs/Qwin/Minify.php
+++ libs/Qwin/Minify.php
@@ -23,7 +23,7 @@
  */
 
 /**
- * CrudController
+ * Minify
  *
  * @package     Qwin
  * @subpackage  Application
@@ -34,73 +34,97 @@
 class Qwin_Minify extends Qwin_Widget
 {
     /**
+     * 存储文件的二维数组,键名为文件类型,如js,css,值为文件路径数组
      *
-     * @var array       存储文件路径的数组
-     *
-     *      -- js       js文件路径
-     *
-     *      -- css      css文件路径
-     *
-     *      -- ...      其他文件类型,也许是php等等
+     * @var array
      */
-    protected $_data = array(
-        'js'    => array(),
-        'css'   => array(),
+    protected $_files = array();
+
+    /**
+     * 缓存对象
+     *
+     * @var Qwin_Storable
+     */
+    protected $_cache;
+
+    /**
+     * 测试输出的模板
+     *
+     * @var array
+     */
+    protected $_tpls = array(
+        'js' => '<script type="text/javascript" src="%s"></script>',
+        'css' => '<link rel="stylesheet" type="text/css" media="all" href="%s" />',
     );
 
     /**
-     * 缓存目录
-     * @var string
+     * 初始化
      */
-    protected $_cacheDir;
-
-    /**
-     * 初始化系统
-     *
-     * @param string $cachePath 缓存文件的路径
-     */
-    public function  __construct(array $options = null)
+    public function  __construct(array $options = array())
     {
         parent::__construct($options);
-        $this->_cacheDir = $this->cache->options['dir'] . '/minify';
-        if (!is_dir($this->_cacheDir)) {
-            mkdir($this->_cacheDir, 0777, true);
-        }
+
+        // todo 缓存类型,参数
+        $this->_cache = $this->fcache;
     }
 
-    public function __invoke($options = null)
+    public function __invoke($file)
     {
-        return $this->add($options);
+        return $this->add($file);
     }
 
     /**
-     * 添加一个文件
+     * 添加一个或多个文件
      *
      * @param string $file 文件路径
-     * @param string $extension 后缀名,如果为设置,则自动获取
-     * @return Widget_Minify 当前对象
-     * @todo 未加入时,是否应该返回false
+     * @return Qwin_Minify
      */
-    public function add($file, $extension = null)
+    public function add($file)
     {
         if (is_array($file)) {
             foreach ($file as $item) {
                 $this->add($item);
             }
-        } elseif (file_exists($file)) {
-            if (null == $extension) {
-                $extension = pathinfo($file, PATHINFO_EXTENSION);
-            }
-//            if ('js' == $extension) {
-//                echo '<script src="' . $file . '"></script>';
-//            } else {
-//                echo '<link rel="stylesheet" type="text/css" media="all" href="' . $file . '" />';
-//            }
-            if (isset($this->_data[$extension])) {
-                $this->_data[$extension][] = $file;
-            }
+        } elseif (is_file($file)) {
+            $ext = pathinfo($file, PATHINFO_EXTENSION);
+
+            $this->_files[$ext][] = $file;
         }
         return $this;
+    }
+
+    /**
+     * 打包某一类后缀的文件
+     *
+     * @param string $ext 后缀名
+     * @return string 打包后的名称
+     */
+    public function pack($ext)
+    {
+        if (!isset($this->_files[$ext]) || empty($this->_files[$ext])) {
+            return false;
+        }
+
+        $this->_files[$ext] = array_unique($this->_files[$ext]);
+
+        $name = md5(implode('|', $this->_files[$ext]));
+
+        if (!$this->_cache->get('minify_' . $name)) {
+            $this->_cache->set('minify_' . $name, $this->_files[$ext]);
+        }
+
+        return $name;
+    }
+
+    /**
+     * 根据名称获取文件数组
+     *
+     * @param string $name 名称
+     * @return array|false
+     */
+    public function getFiles($name)
+    {
+        return $this->_cache->get('minify_' . $name);
     }
 
     /**
@@ -109,34 +133,18 @@ class Qwin_Minify extends Qwin_Widget
      * @param string $extension 后缀名
      * @return string 打包的名称
      */
-    public function pack($extension)
+    public function output($ext)
     {
-        if (!isset($this->_data[$extension]) || empty($this->_data[$extension])) {
-            return false;
+        if (isset($tpl[$ext])) {
+            if (isset($tpl[$ext])) {
+                foreach ($this->_files[$ext] as $file) {
+                    echo sprintf($tpl[$ext], $file);
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return $this->exception('Unsupport output type "' . $ext . '"');
         }
-        $this->_data[$extension] = array_unique($this->_data[$extension]);
-        $name = md5(implode('|', $this->_data[$extension]));
-        $fileName = $this->_cacheDir . '/' . $name . '.php';
-        if (file_exists($fileName)) {
-            return $name;
-        }
-        file_put_contents($fileName, '<?php return ' . var_export($this->_data[$extension], true) . ';' );
-
-        return $name;
-    }
-
-    /**
-     * 获取缓存文件
-     *
-     * @param string $name 名称
-     * @return string|false
-     */
-    public function getCacheFile($name)
-    {
-        $file = $this->_cacheDir . '/' . $name . '.php';
-        if (file_exists($file)) {
-            return $file;
-        }
-        return false;
     }
 }
