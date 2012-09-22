6843388b93bcfd2bbdaa0bbcc3a674954d8509f3
twinh <twinh@yahoo.cn>
2012-09-21 19:12:15 -0700

    added DateTime dicrect for jqGrid json response

--- lib/Qwin/JqGridJson.php
+++ lib/Qwin/JqGridJson.php
@@ -2,36 +2,19 @@
 /**
  * Qwin Framework
  *
- * Copyright (c) 2008-2012 Twin Huang. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author      Twin Huang <twinh@yahoo.cn>
- * @copyright   Twin Huang
+ * @copyright   Copyright (c) 2008-2012 Twin Huang
  * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
- * @version     $Id$
  */
 
+namespace Qwin;
+
 /**
- * JQGridJson
+ * JqGridJson
  *
  * @package     Qwin
- * @subpackage  Widget
- * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
  * @author      Twin Huang <twinh@yahoo.cn>
- * @since       2012-02-26 00:17:06
  */
-class Qwin_JqGridJson extends Qwin_Widget
+class JqGridJson extends Widget
 {
     public $options = array(
         'page' => 1,
@@ -51,7 +34,16 @@ class Qwin_JqGridJson extends Qwin_Widget
         foreach ($data as $row) {
             $cell = array();
             foreach ($columns as $column) {
-                $cell[] = isset($row[$column]) ? $row[$column] : null;
+                if (isset($row[$column])) {
+                    if ($row[$column] instanceof \DateTime) {
+                        $date = (array)$row[$column];
+                        $cell[] = $date['date'];
+                    } else {
+                        $cell[] = $row[$column];
+                    }
+                } else {
+                    $cell[] = null;
+                }
             }
             $json[] = array(
                 'id' => $row['id'],
