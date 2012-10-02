9f9788096824462f5d6bcc3146b0ee3c2390d281
twinh <twinh@yahoo.cn>
2012-10-02 01:46:05 -0700

    refacted app widget

--- lib/Qwin/App.php
+++ lib/Qwin/App.php
@@ -58,10 +58,10 @@ class App extends Widget
         // merge options
         $options = $this->option($options);
         
-        $this->controller = $options['controller'] ?: $this->request('controller');
-        $this->action = $options['action'] ?: $this->request('action');
-        
-        return $this->dispatch($this->controller, $this->action);
+        $controller = $this->getControllerName();
+        $action = $this->getActionName();
+                
+        return $this->dispatch($controller, $action);
     }
     
     public function dispatch($controller, $action = 'index')
@@ -69,31 +69,88 @@ class App extends Widget
         $controllerObject = $this->getController($controller);
         
         if ($controllerObject) {
-                      
-            $result = $controllerObject($action);
-            
-            if ($result) {
-                $this->response($result);
+            $method = $action . 'Action';
+            if (method_exists($controllerObject, $method)) {
+                
+                $this->trigger('before.action');
+                
+                $result = $controllerObject->$method();
+                
+                $this->trigger('after.action');
+                
+                return $this->handleActionResult($result);
+            } else {
+                $this->log(sprintf('Action "%s" not found in controller "%s".', $action, get_class($controllerObject)));
+                throw new Exception('The page you requested was not found.', 404);
             }
-
-            return $this;
+        } else {
+            $this->log(sprintf('Controller "%s" not found', $controller));
+            throw new Exception('The page you requested was not found.', 404);
         }
-          
-        throw new Exception('The page you requested was not found.', 404);
     }
     
+    /**
+     * Get the of name controller
+     * 
+     * @return string the name of controller
+     */
     public function getControllerName()
     {
+        if (!$this->controller) {
+            $this->controller = $this->options['controller'] ?: $this->request('controller');
+        }
         return $this->controller;
     }
     
+    /**
+     * Set the name of controller
+     * 
+     * @param string $controller the name of controller
+     * @return \Qwin\App
+     */
+    public function setControllerName($controller)
+    {
+        $this->controller = $controller;
+        return $this;
+    }
+    
+    /**
+     * Get the name of action
+     * 
+     * @return string the name of controller
+     */
     public function getActionName()
     {
+        if (!$this->action) {
+            $this->action = $this->options['action'] ?: $this->request('action');
+        }
         return $this->action;
     }
     
+    /**
+     * Set the name of action 
+     * 
+     * @param string $action the name of action
+     * @return \Qwin\App
+     */
+    public function setActionName($action)
+    {
+        $this->action = $action;
+        return $this;
+    }
+    
+    /**
+     * Get the controller instance, if not found, return false instead
+     * 
+     * @param string $name the name of controller
+     * @return boolean|\Qwin\Controller
+     */
     public function getController($name)
     {
+        if (!preg_match('/^([_a-z0-9]+)$/i', $name)) {
+            return false;
+        }
+        
         foreach ($this->options['dirs'] as $namespace => $dir) {
             $file = $dir . '/' . $namespace . '/Controller/' . ucfirst($name) . 'Controller.php';
 
@@ -109,22 +166,36 @@ class App extends Widget
                 'widget' => $this->widgetManager
             ));
         }
-        
-        $this->log(sprintf('Controller "%s" not found', $name));
-            
+         
         return false;
     }
     
-    public function getAction()
+    public function handleActionResult($result)
     {
-        
+        switch (true) {
+            case is_null($result):
+            case is_string($result):
+                return $this->response($result);
+                
+            case is_array($result):
+                return $this->render(null, $result);
+                
+            case $result instanceof \Qwin\Response:
+                // todo
+                
+            case is_object($result) && method_exists($result, '__toString') :
+                
+                
+            default:
+                throw new \UnexpectedValueException();
+        }
     }
     
     /**
      * Set application directories
      *
      * @param array $dirs
-     * @return Qwin_App
+     * @return \Qwin\App
      */
     public function setDirsOption($dirs)
     {
