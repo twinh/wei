f34383930dee4c9d5c3584337360e6b97deba9ca
twinh <twinh@yahoo.cn>
2012-10-06 01:29:31 -0700

    refacted excetion class

--- lib/Qwin/Exception.php
+++ lib/Qwin/Exception.php
@@ -16,37 +16,11 @@ namespace Qwin;
  */
 class Exception extends \Exception
 {
-    public $options = array(
-        'code' => 500,
-        'message' => null,
-    );
-
-    public function __construct($message, $code = 500)
-    {
-        if (is_array($message)) {
-            $options = $message + $this->options;
-            $this->message = $options['message'];
-            $this->code = $options['code'];
-        } else {
-            parent::__construct($message, $code);
-        }
-    }
-
-    public function __invoke($message, $code = 500)
+    public function __construct($message, $code = 500, $file = null, $line = null)
     {
         $this->message = $message;
         $this->code = $code;
-        throw $this;
-    }
-
-    /**
-     * Get the class name of invoker
-     * 
-     * @return string
-     * @todo getInvokeWidget ?
-     */
-    public function getInvokerClass()
-    {
-        return $this->options['invoker'] ? get_class($this->options['invoker']) : null;
+        $file && $this->file = $file;
+        $line && $this->line = $line;
     }
 }
