73de83d90016950ccbcf3d9868e0ab575dd9aa34
twinh <twinh@yahoo.cn>
2012-10-17 23:10:05 -0700

    updated test config

---
+++ tests/QwinTest/ConfigTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Qwin\Test;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+    public function test__invoke()
+    {
+        $this->assertEquals(true, true);
+    }
+}
