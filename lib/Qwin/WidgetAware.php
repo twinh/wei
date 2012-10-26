136076bf3277f86d0ea6cebfb5a00ac7f16b320f
twinh <twinh@yahoo.cn>
2012-10-26 12:05:51 -0700

    added WidgetAware class for third-party library using

---
+++ lib/Qwin/WidgetAware.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Qwin Framework
+ *
+ * @copyright   Copyright (c) 2008-2012 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Qwin;
+
+/**
+ * WidgetAware
+ *
+ * @package     Qwin
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+abstract class WidgetAware
+{
+    /**
+     * Invoke widget by the given name
+     *
+     * @param  string $name The name of widget
+     * @param  array  $args The arguments for widget's __invoke method
+     * @return mixed
+     */
+    public function __call($name, $args)
+    {
+        return WidgetManager::create()->invoke($name, $args);
+    }
+
+    /**
+     * Get widget instance by the given name
+     *
+     * @param  string       $name The name of widget
+     * @return \Qwin\Widget
+     */
+    public function __get($name)
+    {
+        return $this->$name = WidgetManager::create()->get($name);
+    }
+}
