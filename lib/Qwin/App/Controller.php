c3b03c07f00a7b95c5a6370dbf3d225355864738
twinh <twinh@yahoo.cn>
2012-11-04 08:20:25 -0800

    moved Controller to App directory, removed useless methods

---
+++ lib/Qwin/App/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Qwin Library
+ *
+ * @copyright   Copyright (c) 2008-2012 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Qwin\App;
+
+/**
+ * Controller
+ *
+ * @package     Qwin
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+abstract class Controller extends WidgetProvider
+{
+}
