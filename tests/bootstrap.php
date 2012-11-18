234902226bc5524e9f3a4602f6fb165f7eac1ae4
twinh <twinh@yahoo.cn>
2012-11-18 05:59:01 -0800

    fixed only variables should be passed by reference

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,9 @@ require dirname(__DIR__) . '/lib/Widget/Widget.php';
 
 Widget::create(array(
     'widget' => array(
+        'inis' => array(
+            'error_reporting' => -1,
+        ),
         // Set up autoload for WidgetTest namespace
         'autoloadMap' => array(
             'WidgetTest' => __DIR__
