9cadf7d7776ab510b9bf171132e90a668a88aeab
twinh <twinh@yahoo.cn>
2012-12-01 04:35:45 -0800

    refactored escape widget, now it's use to escape html and javascript data

--- lib/Widget/Escape.php
+++ lib/Widget/Escape.php
@@ -1,55 +1,69 @@
 <?php
+
 /**
  * Widget Framework
- *
- * Copyright (c) 2008-2011 Twin Huang. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author      Twin Huang <twinh@yahoo.cn>
+ * 
  * @copyright   Twin Huang
  * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
- * @version     $Id$
  */
 
 namespace Widget;
-
-use Widget\Widget;
 
 /**
  * Escape
  *
  * @package     Widget
- * @subpackage  Widget
- * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
  * @author      Twin Huang <twinh@yahoo.cn>
- * @since       2012-01-12 17:53:49
- * @todo        html, js ...
  */
 class Escape extends WidgetProvider
 {
     /**
-     * Escapes special characters for sql query
-     *
-     * @see www.php.net/manual/en/function.mysql-real-escape-string.php
+     * Escapes special characters for safe ouput
+     * 
+     * @param string $data
+     * @param string $type
+     * @param string $charset
+     * @return string
      */
-    public function __invoke($data)
+    public function __invoke($data, $type = 'html', $charset = 'UTF-8')
     {
-        // \0 === \x00
-        return str_replace(array(
-            '\\', "\0", "\n", "\r", "'", '"', "\x1a"
-        ), array(
-            '\\\\', '\0', '\n', '\r', "\\'", '\\"', '\Z'
-        ), $data);
+        switch ($type) {
+            case 'js':
+                return $this->js($data);
+                
+            case 'html':
+            default:
+                return $this->html($data, $charset);
+        }
+    }
+
+    /**
+     * Escapes html data
+     * 
+     * @param string $data
+     * @param string $charset
+     * @return string
+     */
+    public function html($data, $charset = 'UTF-8')
+    {
+        return htmlspecialchars($data, ENT_QUOTES, $charset);
+    }
+
+    /**
+     * Escapes javascript string
+     * 
+     * @param string $data
+     * @return string
+     */
+    public function js($data)
+    {
+        return strtr($data, array(
+            '\\' => '\\\\',
+            "'" => "\\'",
+            '"' => '\\"',
+            "\r" => '\\r',
+            "\n" => '\\n',
+        ));
     }
 }
+
