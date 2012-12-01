35760f9c8ce4d6bbcaf074e885ba2d3c831a1194
twinh <twinh@yahoo.cn>
2012-12-01 05:59:11 -0800

    added callback validator rule

---
+++ lib/Widget/Is/Rule/Callback.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2012 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Is\Rule;
+
+use Widget\WidgetProvider;
+
+/**
+ * Callback
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class Callback extends WidgetProvider
+{
+    public function __invoke($data, $fn)
+    {
+        return (bool) call_user_func($fn, $data);
+    }
+}
