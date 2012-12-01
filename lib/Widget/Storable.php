4b13a4f7a3641d381f8e26c599440cd37b064297
twinh <twinh@yahoo.cn>
2012-12-01 08:10:59 -0800

    added comment for storable interface

--- lib/Widget/Storable.php
+++ lib/Widget/Storable.php
@@ -16,19 +16,84 @@ namespace Widget;
  */
 interface Storable
 {
+    /**
+     * Get or set cache
+     *
+     * @param  string      $key    The name of cache
+     * @param  mixed       $value  The value of cache
+     * @param  int         $expire The expire time for set cache
+     * @return mixed
+     */
+    public function __invoke($key, $value = null, $expire = 0);
+    
+    /**
+     * Get cache
+     *
+     * @param  string      $key The name of cache
+     * @return mixed|false
+     */
     public function get($key, $options = null);
 
+    /**
+     * Set cache
+     *
+     * @param  string $key    The name of cache
+     * @param  value  $value  The value of cache
+     * @param  int    $expire The expire time, 0 means never expired
+     * @return bool
+     */
     public function set($key, $value, $expire = 0, array $options = array());
 
+    /**
+     * Remove cache by key
+     *
+     * @param  string $key the name of cache
+     * @return bool
+     */
     public function remove($key);
 
+    /**
+     * Add cache, if cache is exists, return false
+     *
+     * @param  string $key    The name of cache
+     * @param  mixed  $value  The value of cache
+     * @param  int    $expire The expire time (seconds)
+     * @return bool
+     */
     public function add($key, $value, $expire = 0, array $options = array());
 
+    /**
+     * Replace cache, if cache not exists, return false
+     *
+     * @param  string $key    The name of cache
+     * @param  mixed  $value  The value of cache
+     * @param  int    $expire The expire time
+     * @return bool
+     */
     public function replace($key, $value, $expire = 0, array $options = array());
 
+    /**
+     * Increase a numerical cache
+     *
+     * @param  string    $key    the name of cache
+     * @param  int       $offset the value to decrease
+     * @return int|false
+     */
     public function increment($key, $step = 1);
 
+    /**
+     * Decrease a numerical cache
+     *
+     * @param  string    $key    the name of cache
+     * @param  int       $offset the value to decrease
+     * @return int|false
+     */
     public function decrement($key, $step = 1);
 
+    /**
+     * Clear all cache
+     *
+     * @return boolean
+     */
     public function clear();
 }
