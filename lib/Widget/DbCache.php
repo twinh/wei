985ce3697bafc6c2e5fb5388b1eeaa0bb15320e1
twinh <twinh@yahoo.cn>
2012-12-01 22:52:31 -0800

    updated dbCache widget to new version

--- lib/Widget/DbCache.php
+++ lib/Widget/DbCache.php
@@ -1,109 +1,132 @@
 <?php
 
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2011 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
 namespace Widget;
+
+use PDO;
+use PDOException;
 
 /**
  * DbCache
  *
  * @package     Widget
  * @author      Twin Huang <twinh@yahoo.cn>
- * @copyright   2008-2012 Twin Huang
- * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
- * @version     $Id$
- * @since       2012-7-24
  */
 class DbCache extends WidgetProvider implements Storable
 {
-    public $options = array(
-        'dsn' => 'sqlite:cache.sqlite',
-        'user' => null,
-        'password' => null,
-        'dbh' => null,
-        'table' => 'cache',
-    );
-
     /**
-     * PDO object
-     *
-     * @var PDO
+     * The cache table name
+     * 
+     * @var string
      */
-    protected $_dbh;
-
+    protected $table = 'cache';
+    
     /**
-     * PDOStatement object
-     *
-     * @var PDOStatement
+     * The database username
+     * 
+     * @var string
      */
-    protected $_stmt;
-
+    protected $user;
+    
     /**
-     * Sql driver object
-     *
-     * @var Widget_DbCache_Driver
+     * The database password
+     * 
+     * @var string
      */
-    protected $_driver;
+    protected $password;
+    
+    /**
+     * The dsn parameter for PDO constructor
+     * 
+     * @var string
+     */
+    protected $dsn = 'sqlite:cache.sqlite';
 
     /**
-     * Sql queries
+     * The PDO object
+     *
+     * @var \PDO
+     */
+    protected $dbh;
+
+    /**
+     * The PDOStatement object
+     *
+     * @var \PDOStatement
+     */
+    protected $stmt;
+
+    /**
+     * The sql driver object
+     *
+     * @var \Widget\DbCache\Driver
+     */
+    protected $driver;
+
+    /**
+     * The sql queries
      *
      * @var array
      */
-    protected $_sqls = array();
+    protected $sqls = array();
 
     /**
-     * Connect database
-     *
+     * Constructor
+     * 
      * @param array $options
      */
     public function __construct(array $options = array())
     {
-        $this->options = $options += $this->options;
-
-        // connect
-        if ($options['dbh'] && $options['dbh'] instanceof PDO) {
-            $this->_dbh = $options['dbh'];
-        } else {
-            try {
-                $this->_dbh = new PDO($options['dsn'], $options['user'], $options['password']);
-                $this->_dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $this->_dbh->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, true);
-            } catch (PDOException $e) {
-                $this->exception($e->getMessage());
-            }
+        parent::__construct($options);
+        
+        $this->connect();
+    }
+    
+    /**
+     * Connect the database
+     * 
+     * @throws \Widget\Exception When driver not support
+     */
+    public function connect()
+    {
+        if (!$this->dbh) {
+            $this->dbh = new PDO($this->dsn, $this->user, $this->password);
+            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->dbh->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, true);
         }
-
-        // get driver and load sql queries
-        $driver = $this->_dbh->getAttribute(PDO::ATTR_DRIVER_NAME);
-        $class = 'Widget_DbCache_' . ucfirst($driver);
+        
+        // Get driver and load sql queries
+        $driver = $this->dbh->getAttribute(PDO::ATTR_DRIVER_NAME);
+        $class = 'Widget\DbCache\\' . ucfirst($driver);
         if (class_exists($class)) {
-            $this->_driver = new $class;
-            $this->_sqls = $this->_driver->getSqls();
+            $this->driver = new $class;
+            $this->sqls = $this->driver->getSqls();
         } else {
-            $this->exception('Unsupport driver "' . $driver . '"');
+            throw new Exception(sprintf('Unsupport driver "%s"', $driver));
         }
 
-        // execute prepare sql query
-        if ($this->_sqls['prepare']) {
-            $this->query($this->_sqls['prepare']);
+        // Execute prepare sql query
+        if ($this->sqls['prepare']) {
+            $this->query($this->sqls['prepare']);
         }
 
-        // check if table exists, if not, create table
+        // Check if the table exists, if not, create the table
         try {
-            if (!$this->query($this->_sqls['checkTable'])) {
-                $this->query($this->_sqls['create']);
+            if (!$this->query($this->sqls['checkTable'])) {
+                $this->query($this->sqls['create']);
             }
-        } catch (Widget_Exception $e) {
-            $this->query($this->_sqls['create']);
+        } catch (PDOException $e) {
+            $this->query($this->sqls['create']);
         }
     }
 
     /**
-     * Get or set cache
-     *
-     * @param  string $key    the name of cache
-     * @param  mixed  $value
-     * @param  int    $expire expire time for set cache
-     * @return mixed
+     * {@inheritdoc}
      */
     public function __invoke($key, $value = null, $expire = 0)
     {
@@ -115,20 +138,17 @@ class DbCache extends WidgetProvider implements Storable
     }
 
     /**
-     * Get cache
-     *
-     * @param  string      $key the name of cache
-     * @return mixed|false
+     * {@inheritdoc}
      */
     public function get($key, $options = null)
     {
-        $result = $this->query($this->_sqls['get'], array(
+        $result = $this->query($this->sqls['get'], array(
             ':id' => $key,
             ':expire' => time(),
         ));
 
         if ($result) {
-            $row = $this->_stmt->fetch(PDO::FETCH_ASSOC);
+            $row = $this->stmt->fetch(PDO::FETCH_ASSOC);
 
             return $row ? $row['value'] : false;
         }
@@ -137,18 +157,13 @@ class DbCache extends WidgetProvider implements Storable
     }
 
     /**
-     * Set cache
-     *
-     * @param  string $key    the name of cache
-     * @param  value  $value  the value of cache
-     * @param  int    $expire expire time, 0 means never expired
-     * @return bool
+     * {@inheritdoc}
      */
     public function set($key, $value, $expire = 0, array $options = array())
     {
         $this->remove($key);
 
-        $result = $this->query($this->_sqls['set'], array(
+        $result = $this->query($this->sqls['set'], array(
             ':id' => $key,
             ':value' => $value,
             ':lastModified' => time(),
@@ -159,14 +174,11 @@ class DbCache extends WidgetProvider implements Storable
     }
 
     /**
-     * Remove cache by key
-     *
-     * @param  string $key the name of cache
-     * @return bool
+     * {@inheritdoc}
      */
     public function remove($key)
     {
-        $result = $this->query($this->_sqls['remove'], array(
+        $result = $this->query($this->sqls['remove'], array(
             ':id' => $key,
         ));
 
@@ -174,17 +186,12 @@ class DbCache extends WidgetProvider implements Storable
     }
 
     /**
-     * Add cache, if cache is exists, return false
-     *
-     * @param  string $key    the name of cache
-     * @param  mixed  $value  the value of cache
-     * @param  int    $expire expire time
-     * @return bool
+     * {@inheritdoc}
      */
     public function add($key, $value, $expire = 0, array $options = array())
     {
         try {
-            $result = $this->query($this->_sqls['set'], array(
+            $result = $this->query($this->sqls['set'], array(
                 ':id' => $key,
                 ':value' => $value,
                 ':lastModified' => time(),
@@ -192,37 +199,28 @@ class DbCache extends WidgetProvider implements Storable
             ));
 
             return $result;
-        } catch (Widget_Exception $e) {
+        } catch (PDOException $e) {
             return false;
         }
     }
 
     /**
-     * Replace cache, if cache not exists, return false
-     *
-     * @param  string $key    the name of cache
-     * @param  mixed  $value  the value of cache
-     * @param  int    $expire expire time
-     * @return bool
+     * {@inheritdoc}
      */
     public function replace($key, $value, $expire = 0, array $options = array())
     {
-        $this->query($this->_sqls['replace'], array(
+        $this->query($this->sqls['replace'], array(
             ':id' => $key,
             ':value' => $value,
             ':lastModified' => time(),
             ':expire' => $expire ? time() + $expire : 2147483647
         ));
 
-        return (bool) $this->_stmt->rowCount();
+        return (bool) $this->stmt->rowCount();
     }
 
     /**
-     * Increase a numerical cache
-     *
-     * @param  string    $key    the name of cache
-     * @param  int       $offset the value to decrease
-     * @return int|false
+     * {@inheritdoc}
      */
     public function increment($key, $offset = 1)
     {
@@ -232,28 +230,24 @@ class DbCache extends WidgetProvider implements Storable
         }
 
         try {
-            $this->query($this->_sqls['increment'], array(
+            $this->query($this->sqls['increment'], array(
                 ':id' => $key,
                 ':offset' => $offset,
                 ':lastModified' => time(),
             ));
 
-            if (0 === $this->_stmt->rowCount()) {
+            if (0 === $this->stmt->rowCount()) {
                 return false;
             } else {
                 return $value + $offset;
             }
-        } catch (Widget_Exception $e) {
+        } catch (PDOException $e) {
             return false;
         }
     }
 
     /**
-     * Decrease a numerical cache
-     *
-     * @param  string    $key    the name of cache
-     * @param  int       $offset the value to decrease
-     * @return int|false
+     * {@inheritdoc}
      */
     public function decrement($key, $offset = 1)
     {
@@ -261,13 +255,11 @@ class DbCache extends WidgetProvider implements Storable
     }
 
     /**
-     * Clear all cache
-     *
-     * @return boolean
+     * {@inheritdoc}
      */
     public function clear()
     {
-        return $this->query($this->_sqls['clear']);
+        return $this->query($this->sqls['clear']);
     }
 
     /**
@@ -279,25 +271,21 @@ class DbCache extends WidgetProvider implements Storable
      */
     public function query($sql, $args = array())
     {
-        try {
-            $sql = sprintf($sql, $this->options['table']);
+        $sql = sprintf($sql, $this->table);
 
-            $this->_stmt = $this->_dbh->prepare($sql);
+        $this->stmt = $this->dbh->prepare($sql);
 
-            return $this->_stmt->execute($args);
-        } catch (PDOException $e) {
-            $this->exception($e->getMessage(), $e->getCode());
-        }
+        return $this->stmt->execute($args);
     }
 
     /**
-     * Get PDO object
+     * Get the PDO object
      *
      * @return PDO
      */
     public function getDbh()
     {
-        return $this->_dbh;
+        return $this->dbh;
     }
 
     /**
@@ -307,7 +295,7 @@ class DbCache extends WidgetProvider implements Storable
      */
     public function getStmt()
     {
-        return $this->_stmt;
+        return $this->stmt;
     }
 
     /**
@@ -317,6 +305,6 @@ class DbCache extends WidgetProvider implements Storable
      */
     public function getDriver()
     {
-        return $this->_driver;
+        return $this->driver;
     }
 }
--- lib/Widget/DbCache/Driver.php
+++ lib/Widget/DbCache/Driver.php
@@ -1,54 +1,38 @@
 <?php
+
 /**
  * Widget Framework
  *
- * Copyright (c) 2008-2011 Twin Huang. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author      Twin Huang <twinh@yahoo.cn>
- * @copyright   Twin Huang
+ * @copyright   Copyright (c) 2008-2012 Twin Huang
  * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
- * @version     $Id$
  */
+
+namespace Widget\DbCache;
 
 /**
  * Driver
  *
  * @package     Widget
- * @subpackage  Widget
- * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
  * @author      Twin Huang <twinh@yahoo.cn>
- * @since       2012-07-26
  */
-abstract class Widget_DbCache_Driver
+abstract class Driver
 {
     /**
      * Sql queries
      *
      * @var array
      */
-    protected $_sqls = array();
+    protected $sqls = array();
 
     /**
      *  Get one sql query
      *
-     * @param  string $type sql type, the key of the $this->_sqls array
+     * @param  string $type sql type, the key of the $this->sqls array
      * @return string
      */
     public function getSql($type)
     {
-        return isset($this->_sqls[$type]) ? $this->_sqls[$type] : false;
+        return isset($this->sqls[$type]) ? $this->sqls[$type] : false;
     }
 
     /**
@@ -58,6 +42,6 @@ abstract class Widget_DbCache_Driver
      */
     public function getSqls()
     {
-        return $this->_sqls;
+        return $this->sqls;
     }
 }
