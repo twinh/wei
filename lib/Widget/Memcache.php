7d7e8e1607d1931fe98f6d88257f4f0561e15d39
twinh <twinh@yahoo.cn>
2012-12-01 19:13:43 -0800

    refactored memcache widget

--- lib/Widget/Memcache.php
+++ lib/Widget/Memcache.php
@@ -1,25 +1,10 @@
 <?php
+
 /**
  * Widget Framework
  *
- * Copyright (c) 2008-2012 Twin Huang. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author      Twin Huang <twinh@yahoo.cn>
  * @copyright   Twin Huang
  * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
- * @version     $Id: Memcache.php 1282 2012-07-06 07:38:44Z itwinh@gmail.com $
  */
 
 namespace Widget;
@@ -28,108 +13,152 @@ namespace Widget;
  * Memcache
  *
  * @package     Widget
- * @subpackage  Widget
- * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
  * @author      Twin Huang <twinh@yahoo.cn>
- * @since       2012-05-30
  */
 class Memcache extends WidgetProvider implements Storable
 {
     /**
-     * Options
-     *
+     * The memcache server configurations
+     * 
      * @var array
+     * @see \Memcache::addServer
      */
-    public $options = array(
-        'servers' => array(),
-        'object' => null,
-        'flag' => MEMCACHE_COMPRESSED,
+    protected $servers = array(
+        array(
+            'host'          => '127.0.0.1',
+            'port'          => 11211,
+            'persistent'    => true
+        )
     );
-
-    protected $_serverOptions = array(
-        'host' => '127.0.0.1',
-        'port' => 11211,
-        'persistent' => true,
-        'weight' => 0,
-        'timeout' => 0,
-        'retryInterval' => 0,
-        'failureCallback' => null,
-    );
+    
+    /**
+     * MEMCACHE_COMPRESSED
+     * 
+     * @var int
+     */
+    protected $flag = 2;
 
     /**
-     * Memcache object
+     * The memcache object
      *
-     * @var Memcache
+     * @var \Memcache
      */
-    protected $_object;
-
+    protected $object;
+    
     /**
-     * Init
+     * Constructor
      *
      * @param array $options
      */
     public function __construct(array $options = array())
     {
-        $options = $options + $this->options;
-        parent::__construct($options);
+        $this->object = new \Memcache;
 
-        $this->_object = new Memcache;
-        $this->_object->addserver($options['host'], $options['port']);
+        parent::__construct($options + array(
+            'servers' => $this->servers
+        ));
     }
-
-    public function get($key, $options = null)
+    
+    /**
+     * Set servers
+     * 
+     * @param array $servers
+     * @return \Widget\Memcache
+     */
+    public function setServers($servers)
     {
-        return $this->_object->get($key, $options);
-    }
-
-    public function set($key, $value, $expire = 0, array $options = array())
-    {
-        $o = $options + $this->_objectOptions;
-
-        return $this->_object->set($key, $value, $o['flag'], $expire);
-    }
-
-    public function remove($key)
-    {
-        return $this->_object->delete($key);
-    }
-
-    public function add($key, $value, $expire = 0, array $options = array())
-    {
-        $o = $options + $this->_objectOptions;
-
-        return $this->_object->add($key, $value, $o['flag'], $expire);
-    }
-
-    public function replace($key, $value, $expire = 0, array $options = array())
-    {
-        $o = $options + $this->_objectOptions;
-
-        return $this->_object->replace($key, $value, $o['flag'], $expire);
-    }
-
-    public function increment($key, $offset = 1)
-    {
-        return $this->_object->increment($key, $offset);
-    }
-
-    public function decrement($key, $offset = 1)
-    {
-        return $this->_object->decrement($key, $offset);
-    }
-
-    public function clear()
-    {
-        return $this->_object->flush();
+        foreach ((array)$servers as $server) {
+            call_user_func_array(array($this->object, 'addServer'), $server);
+        }
+        
+        $this->servers = $servers;
+        
+        return $this;
     }
 
     /**
+     * {@inheritdoc}
+     */
+    public function get($key, $options = null)
+    {
+        return $this->object->get($key, $options);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($key, $value, $expire = 0, array $options = array())
+    {
+        return $this->object->set($key, $value, $this->flag, $expire);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove($key)
+    {
+        return $this->object->delete($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function add($key, $value, $expire = 0, array $options = array())
+    {
+        return $this->object->add($key, $value, $this->flag, $expire);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function replace($key, $value, $expire = 0, array $options = array())
+    {
+        return $this->object->replace($key, $value, $this->flag, $expire);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function increment($key, $offset = 1)
+    {
+        return $this->object->increment($key, $offset);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function decrement($key, $offset = 1)
+    {
+        return $this->object->decrement($key, $offset);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear()
+    {
+        return $this->object->flush();
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke($key, $value = null, $expire = 0)
+    {
+        if (1 == func_num_args()) {
+            return $this->get($key);
+        } else {
+            return $this->set($key, $value, $expire);
+        }
+    }
+    
+    /**
      * Get memcache object
      *
-     * @return Memcache
+     * @return \Memcache
      */
     public function getObject()
     {
-        return $this->_object;
+        return $this->object;
     }
 }
