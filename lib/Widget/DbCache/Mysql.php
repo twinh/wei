734719f0c4fdf49602c16705db6d2fdfd0bdfcb0
twinh <twinh@yahoo.cn>
2012-12-01 22:58:56 -0800

    updated dbCache drivers

--- lib/Widget/DbCache/Mysql.php
+++ lib/Widget/DbCache/Mysql.php
@@ -1,44 +1,28 @@
- <?php
+<?php
+
 /**
  * Widget Framework
  *
- * Copyright (c) 2008-2011 Twin Huang. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author      Twin Huang <twinh@yahoo.cn>
- * @copyright   Twin Huang
+ * @copyright   Copyright (c) 2008-2012 Twin Huang
  * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
- * @version     $Id$
  */
+
+namespace Widget\DbCache;
 
 /**
  * Mysql
  *
  * @package     Widget
- * @subpackage  Widget
- * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
  * @author      Twin Huang <twinh@yahoo.cn>
- * @since       2012-07-26
  */
-class Widget_DbCache_Mysql extends Widget_DbCache_Driver
+class Mysql extends Driver
 {
     /**
-     * Sql queries
+     * The sql queries
      *
      * @var array
      */
-    protected $_sqls = array(
+    protected $sqls = array(
         'prepare' => 'SET NAMES UTF8',
         'checkTable' => 'SELECT 1 from %s',
         'create' => 'CREATE TABLE %s (id varchar(255) PRIMARY KEY, value longtext, lastModified int, expire int) ENGINE=InnoDB DEFAULT CHARSET=utf8',
--- lib/Widget/DbCache/PgSql.php
+++ lib/Widget/DbCache/PgSql.php
@@ -3,43 +3,26 @@
 /**
  * Widget Framework
  *
- * Copyright (c) 2008-2011 Twin Huang. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author      Twin Huang <twinh@yahoo.cn>
- * @copyright   Twin Huang
+ * @copyright   Copyright (c) 2008-2012 Twin Huang
  * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
- * @version     $Id$
  */
+
+namespace Widget\DbCache;
 
 /**
  * PgSql
  *
  * @package     Widget
- * @subpackage  Widget
- * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
  * @author      Twin Huang <twinh@yahoo.cn>
- * @since       2012-07-26
  */
-class Widget_DbCache_PgSql extends Widget_DbCache_Driver
+class PgSql extends Driver
 {
     /**
-     * Sql queries
+     * The sql queries
      *
      * @var array
      */
-    protected $_sqls = array(
+    protected $sqls = array(
         'prepare' => null,
         'checkTable' => 'SELECT 1 from %s',
         'create' => 'CREATE TABLE %s (id varchar(255) PRIMARY KEY, value text, "lastModified" int4, expire int4)',
