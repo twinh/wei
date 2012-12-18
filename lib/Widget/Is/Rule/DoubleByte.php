b90169535aea1c45a2c0b99aa16c0fa6fb88f067
twinh <twinh@yahoo.cn>
2012-12-18 05:57:36 -0800

    added doubleByte validate widget

---
+++ lib/Widget/Is/Rule/DoubleByte.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Widget Framework
+ * 
+ * @copyright   Copyright (c) 2008-2012 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Is\Rule;
+
+/**
+ * DoubleByte
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+
+class DoubleByte
+{
+    public function __invoke($value)
+    {
+        return (bool) preg_match('/^[^\x00-xff]+$/', $value);
+    }
+}
