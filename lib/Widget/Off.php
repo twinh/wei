3eedab2d71894778974f9768b38fb78e900c70b7
twinh <twinh@yahoo.cn>
2012-12-22 06:00:31 -0800

    added off widget, close #10

---
+++ lib/Widget/Off.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2012 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget;
+
+/**
+ * Off
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @property \Widget\EventManager $eventManager The event manager widget
+ */
+class Off extends WidgetProvider
+{    
+    /**
+     * Remove one or all handlers
+     *
+     * param string|null $type The type of event
+     * @return \Widget\EventManager
+     */
+    public function __invoke($type)
+    {
+        return $this->eventManager->remove($type);
+    }
+}
