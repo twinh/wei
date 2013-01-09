b5a44ba921707674f46aa7b228f487be71afddd3
twinh <twinh@yahoo.cn>
2013-01-09 07:42:53 -0800

    added @codeCoverageIgnore for internel events

--- lib/Widget/EventManager.php
+++ lib/Widget/EventManager.php
@@ -181,6 +181,28 @@ class EventManager extends WidgetProvider
     {
         list($type, $namespaces) = $this->splitNamespace($type);
         
+        /*if ($type && isset($this->handlers[$type])) {
+            if (!$namespaces) {
+                return isset($this->handlers[$type]);
+            } else {
+                foreach ($this->handlers[$type] as $i => $handlers) {
+                    foreach ($handlers as $j => $handler) {
+                        if ($namespaces == array_intersect($namespaces, $handler[2])) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        } else {
+            foreach ($this->handlers as $type => $handlers) {
+                if (true === $this->has($type . '.' . implode('.', $namespaces))) {
+                    return true;
+                }
+            }
+            return false;
+        }*/
+        
         if (!$namespaces) {
             return isset($this->handlers[$type]);
         } elseif (!$type) {
@@ -208,6 +230,8 @@ class EventManager extends WidgetProvider
      
     /**
      * Register the internal event
+     * 
+     * @codeCoverageIgnore
      */
     protected function registerInternalEvent()
     {
