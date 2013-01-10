f077af24a33c24f579d6d77b181aa97b83f532f9
twinh <twinhuang@qq.com>
2013-01-10 05:19:16 -0800

    addedisTime widget

---
+++ lib/Widget/Is/Rule/Time.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Widget Framework
+ * 
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Is\Rule;
+
+/**
+ * Time
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class Time
+{
+    public function __invoke($value)
+    {
+        return (bool) preg_match('/^([01]\d|2[0-3])(:[0-5]\d){0,2}$/', $value);
+    }
+}
