5ad0d820770c919919bbf4409015b935888ebf87
twinh <twinhuang@qq.com>
2013-01-13 01:11:29 -0800

    add isDateTime widget

---
+++ lib/Widget/Is/Rule/DateTime.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Is\Rule;
+
+use Widget\WidgetProvider;
+use DateTime as Dt;
+
+
+/**
+ * The date validate rule
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class DateTime extends WidgetProvider
+{
+    protected $format = 'Y-m-d H:i:s';
+    
+    public function __invoke($value)
+    {
+        $date = Dt::createFromFormat($this->format, $value);
+        
+        if (false === $date) {
+            return false;
+        }
+
+        return $value === $date->format($this->format);
+    }
+}
