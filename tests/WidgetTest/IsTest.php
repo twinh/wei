c195b504b1a332d488be3ca8e43140fe1b971e12
twinh <twinhuang@qq.com>
2013-01-12 21:53:19 -0800

    added test for isFile widget

--- tests/WidgetTest/IsTest.php
+++ tests/WidgetTest/IsTest.php
@@ -141,4 +141,26 @@ class IsTest extends TestCase
         
         $this->assertFalse($this->isPostcode('0234567'));
     }
+    
+    public function testIsFile()
+    {
+        $this->assertFalse(false, $this->isFile(array()), 'Not File path');
+
+        $this->assertEquals($this->isFile(__FILE__), __FILE__, 'File found');
+
+        $this->assertFalse($this->isFile('.file not found'), 'File not found');
+
+        $paths = explode(PATH_SEPARATOR, ini_get('include_path'));
+        $path = array_pop($paths);
+        $files = scandir($path);
+        foreach ($files as $file) {
+            if ('.' == $file || '..' == $file) {
+                continue;
+            }
+            if (is_file($path . DIRECTORY_SEPARATOR . $file)) {
+                $this->assertNotEquals(false, $this->isFile($file), 'File in include path found');
+                break;
+            }
+        }
+    }
 }
