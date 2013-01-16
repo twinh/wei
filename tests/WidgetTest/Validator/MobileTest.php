05cb5b45fa140da33bd385ba8f4e768d868b8f0f
twinh <twinhuang@qq.com>
2013-01-16 02:02:49 -0800

    moved isMobile test to new file

---
+++ tests/WidgetTest/Validator/MobileTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WidgetTest\Validator;
+
+use WidgetTest\TestCase;
+
+class MobileTest extends TestCase
+{
+    /**
+     * @dataProvider providerForMobile
+     */
+    public function testMobile($input)
+    {
+        $this->assertTrue($this->isMobile($input));
+    }
+    
+    /**
+     * @dataProvider providerForNotMobile
+     */
+    public function testNotMobile($input)
+    {
+        $this->assertFalse($this->isMobile($input));
+    }
+    
+    public function providerForMobile()
+    {
+        return array(
+            array('13112345678'),
+            array('13612345678'),
+            array('13800138000'),
+            array('15012345678'),
+            array('15812345678'),
+            array('18812345678'),
+        );
+    }
+    
+    public function providerForNotMobile()
+    {
+        return array(
+            array('12000000000'),
+            array('88888888'),
+            array('not digit'),
+            array('0754-8888888')
+        );
+    }
+}
