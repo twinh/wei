bcd3df1b05f837d8433dfb703dcfd77bb5530438
twinh <twinhuang@qq.com>
2013-01-16 06:57:40 -0800

    added isType widget

---
+++ lib/Widget/Is/Rule/Type.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Widget Framework
+ * 
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Is\Rule;
+
+use Widget\Exception;
+
+/**
+ * Time
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class Type extends AbstractRule
+{
+    public function __invoke($value, $type)
+    {
+        $type = strtolower($type);
+        
+        if (function_exists($fn = 'is_' . $type)) {
+            return $fn($value);
+        } elseif (function_exists($fn = 'ctype_' . $type)) {
+            return $fn($value);
+        } else {
+            throw new Exception(sprintf('Unknow type %s', $type));
+        }
+    }
+}
