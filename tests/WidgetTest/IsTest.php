a8547584bcf5dd6ae8a03d7b50e54d2730e1cd50
twinh <twinhuang@qq.com>
2013-01-19 05:50:48 -0800

    moved isAlnum test to new file

--- tests/WidgetTest/IsTest.php
+++ tests/WidgetTest/IsTest.php
@@ -4,19 +4,6 @@ namespace WidgetTest;
 
 class IsTest extends TestCase
 {
-    public function testIsAlnum()
-    {
-        $this->assertTrue($this->isAlnum('abcedfg'));
-
-        $this->assertTrue($this->isAlnum('a2BcD3eFg4'));
-
-        $this->assertTrue($this->isAlnum('045fewwefds'));
-
-        $this->assertFalse($this->isAlnum('a bcdefg'));
-
-        $this->assertFalse($this->isAlnum('-213a bcdefg'));
-    }
-
     public function testIsAlpha()
     {
         $this->assertTrue($this->isAlpha('abcedfg'));
---
+++ tests/WidgetTest/Validator/AlnumTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace WidgetTest\Validator;
+
+use WidgetTest\TestCase;
+
+class AlnumTest extends TestCase
+{
+    /**
+     * @dataProvider providerForAlnum
+     */
+    public function testAlnum($input)
+    {
+        $this->assertTrue($this->isAlnum($input));
+    }
+
+    /**
+     * @dataProvider providerForNotAlnum
+     */
+    public function testNotAlnum($input)
+    {
+        $this->assertFalse($this->isAlnum($input));
+    }
+
+    public function providerForAlnum()
+    {
+        return array(
+            array('abcedfg'),
+            array('a2BcD3eFg4'),
+            array('045fewwefds'),
+        );
+    }
+
+    public function providerForNotAlnum()
+    {
+        return array(
+            array('a bcdefg'),
+            array('-213a bcdefg'),
+        );
+    }
+}
