108b69d2cc54b75994d23e18314aba339aecf41c
twinh <twinhuang@qq.com>
2013-01-20 08:09:27 -0800

    added isOneof widget

---
+++ lib/Widget/Validator/Rule/OneOf.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Validator\Rule;
+
+/**
+ * The default logger for widget, which is base on the Monolog!
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class OneOf extends AbstractRule
+{
+    public function __invoke($data, $rules)
+    {
+        foreach ($rules as $rule => $options) {
+            if ($this->is->validateOne($rule, $data, $options)) {
+                return true;
+            }
+        }
+        return false;
+        
+
+//        
+//        // NoneOf 所有的都不通过才true
+//        foreach ($rules as $rule => $options) {
+//            if ($this->is->validateOne($rule, $data, $options)) {
+//                return false;
+//            }
+//        }
+//        return true;
+    }
+}
