d01c13901456153216c7e0cd347ef81b195aa6ca
twinh <twinhuang@qq.com>
2013-01-20 08:12:07 -0800

    added test for isOneOf widget

---
+++ tests/WidgetTest/Validator/OneOfTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace WidgetTest\Validator;
+
+use WidgetTest\TestCase;
+
+class OneOffTest extends TestCase
+{
+    public function testOneOf()
+    {
+        $this->assertTrue($this->isOneOf('13', array(
+            'type' => 'int',
+            'alnum' => true
+        )));
+    }
+    
+    public function testNotOneOf()
+    {
+        $this->assertFalse($this->isOneOf('13', array(
+            'email' => true,
+            'length' => 3
+        )));
+    }
+}
