50f85417be40076f0b27931aef0d9a5dfbdc2c71
twinh <twinhuang@qq.com>
2013-01-19 19:52:15 -0800

    use filter_var instead of regex to validate email

--- lib/Widget/Validator/Rule/Email.php
+++ lib/Widget/Validator/Rule/Email.php
@@ -20,6 +20,6 @@ class Email extends AbstractRule
 
     public function __invoke($value)
     {
-        return (bool) preg_match('/^[-a-zA-Z0-9_\.]+\@([0-9A-Za-z][0-9A-Za-z-]+\.)+[A-Za-z]{2,5}$/i', $value);
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
     }
 }
