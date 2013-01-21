1b734f03cda05500cb6da9b13490a6acffaea832
twinh <twinhuang@qq.com>
2013-01-20 17:43:51 -0800

    added isNoneOf widget

---
+++ lib/Widget/Validator/Rule/NoneOf.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Validator\Rule;
+
+/**
+ * The default logger for widget, which is base on the Monolog!
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class NoneOf extends AbstractRule
+{
+    public function __invoke($data, array $rules)
+    {
+        foreach ($rules as $rule => $options) {
+            if ($this->is->validateOne($rule, $data, $options)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
