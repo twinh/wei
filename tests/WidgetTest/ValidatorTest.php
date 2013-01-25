7ba32fa9132a2f604c5f2e64eebcdac6a1ba9c2a
twinh <twinhuang@qq.com>
2013-01-24 21:27:13 -0800

    added test for isExists widget

--- tests/WidgetTest/ValidatorTest.php
+++ tests/WidgetTest/ValidatorTest.php
@@ -6,28 +6,6 @@ use Widget\Validator;
 
 class ValidatorTest extends TestCase
 {
-    public function testIsExists()
-    {
-        $this->assertEquals(false, $this->isExists(array()), 'Not File path');
-
-        $this->assertEquals($this->isExists(__FILE__), __FILE__, 'File found');
-
-        $this->assertFalse($this->isExists('.file not found'), 'File not found');
-
-        $paths = explode(PATH_SEPARATOR, ini_get('include_path'));
-        $path = array_pop($paths);
-        $files = scandir($path);
-        foreach ($files as $file) {
-            if ('.' == $file || '..' == $file) {
-                continue;
-            }
-            if (file_exists($path . DIRECTORY_SEPARATOR . $file)) {
-                $this->assertNotEquals(false, $this->isExists($file), 'File in include path found');
-                break;
-            }
-        }
-    }
-
     public function testIsDate()
     {
         $this->assertTrue($this->isDate('2013-01-13'));
