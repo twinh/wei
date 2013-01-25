a53c6b3e74678eb5dfe68372339bd2447646ef8d
twinh <twinhuang@qq.com>
2013-01-24 21:25:53 -0800

    added test for isDir widget

--- tests/WidgetTest/ValidatorTest.php
+++ tests/WidgetTest/ValidatorTest.php
@@ -6,29 +6,6 @@ use Widget\Validator;
 
 class ValidatorTest extends TestCase
 {
-    
-    public function testIsDir()
-    {
-        $this->assertEquals(false, $this->isDir(array()), 'Not File path');
-
-        $this->assertEquals($this->isDir(__DIR__), __DIR__, 'File found');
-
-        $this->assertFalse($this->isDir('.file not found'), 'File not found');
-
-        $paths = explode(PATH_SEPARATOR, ini_get('include_path'));
-        $path = array_pop($paths);
-        $files = scandir($path);
-        foreach ($files as $file) {
-            if ('.' == $file || '..' == $file) {
-                continue;
-            }
-            if (is_dir($path . DIRECTORY_SEPARATOR . $file)) {
-                $this->assertNotEquals(false, $this->isDir($file), 'File in include path found');
-                break;
-            }
-        }
-    }
-
     public function testIsExists()
     {
         $this->assertEquals(false, $this->isExists(array()), 'Not File path');
