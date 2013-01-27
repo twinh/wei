c8e8cd8bbb9f2878b5d0a25b6a4403a625db442a
twinh <twinhuang@qq.com>
2013-01-26 20:34:28 -0800

    added isAll validator widget

---
+++ lib/Widget/Validator/All.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Validator;
+
+/**
+ * The default logger for widget, which is base on the Monolog!
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class All extends AbstractRule
+{
+    public function __invoke($data, array $rules)
+    {
+        if (!is_array($data) && !$data instanceof \Traversable) {
+            throw new \InvalidArgumentException(sprintf('Excepted array or Traversable, %s given', gettype($data)));
+        }
+        
+        foreach ($data as $element) {
+            foreach ($rules as $rule => $options) {
+                if (!$this->is->validateOne($rule, $element, $options)) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
