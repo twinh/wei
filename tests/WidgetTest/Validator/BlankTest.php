7ed69460ea3ad24d44b447b083fd58dcef20ce3c
twinh <twinhuang@qq.com>
2013-01-28 00:19:46 -0800

    added test for isBlank validator widget

---
+++ tests/WidgetTest/Validator/BlankTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace WidgetTest\Validator;
+
+use WidgetTest\TestCase;
+
+/**
+ * @todo more space test
+ * @link http://en.wikipedia.org/wiki/Space_(punctuation)
+ */
+class BlankTest extends TestCase
+{
+    /**
+     * @dataProvider providerForBlank
+     */
+    public function testBlank($input)
+    {
+        $this->assertTrue($this->isBlank($input));
+    }
+
+    /**
+     * @dataProvider providerForNotBlank
+     */
+    public function testNotBlank($input)
+    {
+        $this->assertFalse($this->isBlank($input));
+    }
+
+    public function providerForBlank()
+    {
+        return array(
+            array('   '),
+            array(" \r\n"),
+        );
+    }
+
+    public function providerForNotBlank()
+    {
+        return array(
+            array(' abc '),
+            array('a b'),
+            array('　'), // Chinse fullwith space
+        );
+    }
+}
