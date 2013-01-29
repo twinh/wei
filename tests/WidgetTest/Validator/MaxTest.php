3d3a32782dc4023dfe205f0608c343d40e1f7dd9
twinh <twinhuang@qq.com>
2013-01-29 00:04:37 -0800

    added test for isMax validator widget

---
+++ tests/WidgetTest/Validator/MaxTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WidgetTest\Validator;
+
+use WidgetTest\TestCase;
+
+class MaxTest extends TestCase
+{
+    /**
+     * @dataProvider providerForMax
+     */
+    public function testMax($input, $options)
+    {
+        $this->assertTrue($this->isMax($input, $options));
+    }
+
+    /**
+     * @dataProvider providerForNotMax
+     */
+    public function testNotMax($input, $options)
+    {
+        $this->assertFalse($this->isMax($input, $options));
+    }
+    
+    public function providerForMax()
+    {
+        return array(
+            array(7, 7),
+            array(7, 8),
+            array(0.1, 0.2),
+            array('2000-01-01', '2001-01-01'),
+            array('10:00', '11:00'),
+            array('10:03', '9:24'),
+        );
+    }
+
+    public function providerForNotMax()
+    {
+        return array(
+            array(7, 6),
+            array(0.1, 0.01),
+            array('2000-01-01', '1999-01-01'),
+            array('10:03', '09:24'),
+        );
+    }
+}
