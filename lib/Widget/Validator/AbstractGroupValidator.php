7282c27bb13c3e5d5b903aae93ed75f73517f31c
twinh <twinhuang@qq.com>
2013-02-10 18:11:21 -0800

    added abstract group validator

---
+++ lib/Widget/Validator/AbstractGroupValidator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Validator;
+
+class AbstractGroupValidator extends AbstractValidator
+{
+
+}
