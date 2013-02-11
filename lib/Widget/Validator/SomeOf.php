b0db04a56a9b1d449ff1067635ae8cf24e631fca
twinh <twinhuang@qq.com>
2013-02-10 21:36:25 -0800

    added someOf validator

---
+++ lib/Widget/Validator/SomeOf.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Validator;
+
+/**
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @property    \Widget\Is $is The validator manager
+ */
+class SomeOf extends AbstractGroupValidator
+{
+    protected $atLeastMessage = '%name% must be passed by at least %left% of %count% rules';
+    
+    protected $rules = array();
+    
+    protected $atLeast;
+    
+    public function __invoke($input, array $rules = array(), $atLeast = null)
+    {
+        $atLeast && $this->atLeast = $atLeast;
+        $rules && $this->rules = $rules;
+        
+        // Adds "atLeast" error at first, make sure this error at the top of 
+        // strack, if any rule is passed, the error will be removed
+        $this->addError('atLeast');
+
+        $passed = 0;
+        $validator = null;
+        foreach ($this->rules as $rule => $options) {
+            if ($this->is->validateOne($rule, $input, $options, $validator)) {
+                $passed++;
+                if ($passed >= $this->atLeast) {
+                    // Removes all error messages
+                    $this->errors = array();
+                    return true;
+                }
+            } else {
+                foreach ($validator->getErrors() as $name => $error) {
+                    $this->addError($rule . '.' . $name, $error[1], $error[0]);
+                }
+            }
+        }
+        
+        // Updates error parameters
+        $this->addError('atLeast', array(
+            'count' => count($rules) - $passed,
+            'left' => $this->atLeast - $passed
+        ));
+        
+        return false;
+    }
+}
