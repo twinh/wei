6d586c4bb4e43f36e7448af96c722da73a908c2f
twinh <twinhuang@qq.com>
2013-02-10 21:47:28 -0800

    used SomeOf as the parent class of oneOf validator

--- lib/Widget/Validator/OneOf.php
+++ lib/Widget/Validator/OneOf.php
@@ -13,34 +13,9 @@ namespace Widget\Validator;
  * @author      Twin Huang <twinh@yahoo.cn>
  * @property \Widget\Is $is The validator manager
  */
-class OneOf extends AbstractGroupValidator
+class OneOf extends SomeOf
 {
-    protected $noRulePassedMessage = '%name% must be passed by at least one rule';
+    protected $atLeastMessage = '%name% must be passed by at least one rule';
     
-    protected $rules = array();
-    
-    public function __invoke($input, array $rules = array())
-    {
-        $rules && $this->rules = $rules;
-
-        // Adds no rule passed error at first, if any rule is passed, the error will be removed
-        $this->addError('noRulePassed', array(
-            'count' => 1
-        ));
-
-        $validator = null;
-        foreach ($this->rules as $rule => $options) {
-            if ($this->is->validateOne($rule, $input, $options, $validator)) {
-                // Removes all error messages
-                $this->errors = array();
-                return true;
-            } else {
-                foreach ($validator->getErrors() as $name => $error) {
-                    $this->addError($rule . '.' . $name, $error[1], $error[0]);
-                }
-            }
-        }
-
-        return false;
-    }
+    protected $atLeast = 1;
 }
