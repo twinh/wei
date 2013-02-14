1e372676d6b27af35723d0980317d2ebe98a1e05
twinh <twinhuang@qq.com>
2013-02-13 19:07:17 -0800

    added en validator language

---
+++ lib/Widget/Resource/i18n/en/validator.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+return array(
+    // Type
+    // is_xxx
+    'array'     => 'array',
+    'bool'      => 'boolen',
+    'float'     => 'float',
+    'int'       => 'integer',
+    'integer'   => 'integer',
+    'null'      => 'NULL',
+    'numeric'   => 'number',
+    'object'    => 'object',
+    'resource'  => 'resource',
+    'scalar'    => 'scalar',
+    'string'    => 'string',
+    // ctype_xxx
+    'alnum'     => 'alphanumeric characters',
+    'alpha'     => 'alphabetic characters',
+    'cntrl'     => 'control characters',
+    'digit'     => 'numeric characters',
+    'graph'     => 'printable characters except space',
+    'lower'     => 'lowercase characters',
+    'print'     => 'printable characters',
+    'punct'     => 'punctuation characters',
+    'space'     => 'whitespace characters',
+    'upper'     => 'uppercase characters',
+    'xdigit'    => 'hexadecimal digit',
+);
