675cdd6444545fda937ede2b6887746a0ae4b7f1
twinh <twinhuang@qq.com>
2013-02-19 06:35:43 -0800

    removed extra char

--- lib/Widget/Resource/i18n/zh-CN/validator.php
+++ lib/Widget/Resource/i18n/zh-CN/validator.php
@@ -51,7 +51,7 @@ return array(
     '%name% must be an existing directory' => '%name%必须是存在的目录',
     '%name% must be a non-existing directory' => '%name%必须是不存在的目录',
     
-    // 'doubleByte'
+    // doubleByte
     '%name% must contain only double byte characters' => '%name%只能由双字节字符组成',
     '%name% must not contain only double byte characters' => '%name%不能只由双字节字符组成',
     
