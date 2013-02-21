7a814b31972fbe1d1b1963815fac844c60101836
twinh <twinhuang@qq.com>
2013-02-21 04:48:09 -0800

    updatd uuid widget's class annotation

--- lib/Widget/Uuid.php
+++ lib/Widget/Uuid.php
@@ -2,44 +2,25 @@
 /**
  * Widget Framework
  *
- * Copyright (c) 2008-2013 Twin Huang. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author      Twin Huang <twinh@yahoo.cn>
- * @copyright   Twin Huang
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
  * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
- * @version     $Id$
  */
 
 namespace Widget;
 
 /**
- * Uuid
+ * UUid
  *
- * @namespace   Widget
- * @subpackage  Widget
- * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ * @package     Widget
  * @author      Twin Huang <twinh@yahoo.cn>
- * @since       2011-12-20 22:05:35
  */
 class Uuid extends WidgetProvider
 {
     /**
      * Generate a FAKE UUID(universally unique identifier)
      *
-     * @see http://stackoverflow.com/questions/2040240/php-function-to-generate-v4-uuid
-     * @see http://php.net/manual/en/function.uniqid.php
+     * @link http://stackoverflow.com/questions/2040240/php-function-to-generate-v4-uuid
+     * @link http://php.net/manual/en/function.uniqid.php
      * @return string
      */
     public function __invoke()
