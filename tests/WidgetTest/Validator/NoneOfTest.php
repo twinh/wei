e7fe6cdc2e3a5b93810300ac14a45a60c6c5bad9
twinh <twinhuang@qq.com>
2013-02-21 08:44:59 -0800

    added getMessages test for noneOf validator

--- tests/WidgetTest/Validator/NoneOfTest.php
+++ tests/WidgetTest/Validator/NoneOfTest.php
@@ -23,4 +23,17 @@ class NoneOfTest extends TestCase
             )
         )));
     }
+    
+    public function testGetMessages()
+    {
+        $noneOf = $this->is->createRuleValidator('noneOf');
+        
+        $noneOf('abc', array(
+            'equals' => 'abc',
+            'alnum' => true
+        ));
+        
+        // Returns multi messages as default
+        $this->assertCount(2, $noneOf->getMessages());
+    }
 }
