9027a6c9d42c6847ff9b004439bdf8f1d36ca7dd
twinh <twinhuang@qq.com>
2013-02-21 04:24:12 -0800

    used internal fromatBytes insteand file validator's fromBytes method

--- lib/Widget/Marker.php
+++ lib/Widget/Marker.php
@@ -100,7 +100,7 @@ class Marker extends WidgetProvider
                 $data['fullName']           = $prevData['name'] . '~' . $name;
                 $data['elapsedTime']        = bcsub($data['time'], $prevData['time'], 4);
                 $data['timePercentage']     = $totalTime ? bcmul(bcdiv($data['elapsedTime'], $totalTime, 4), 100, 2) . '%' : 0;
-                $data['increasedMemory']    = $this->isFile->fromBytes($data['memory'] - $prevData['memory']);
+                $data['increasedMemory']    = $this->fromBytes($data['memory'] - $prevData['memory']);
                 $data['memoryPercentage']   = bcmul(bcdiv(bcsub($data['memory'], $prevData['memory']), $totalMemory, 4), 100, 2) . '%';
             } else {
                 $data['fullName']           = $name;
@@ -111,16 +111,32 @@ class Marker extends WidgetProvider
             }
             $data['name']                   = $name;
             $data['formatedTime']           = date('i:s', $data['time']) . '.' . substr($data['time'], 11) . 's';
-            $data['formatedMemory']         = $this->isFile->fromBytes($data['memory']);
+            $data['formatedMemory']         = $this->fromBytes($data['memory']);
             $result[] = $prevData = $data;
         }
         return $result;
     }
+    
+    /**
+     * Formats bytes to human readable file size (e.g. 1.2MB, 10KB)
+     * 
+     * @param int $bytes
+     * @return string
+     */
+    protected function fromBytes($bytes)
+    {
+        $units = array('B', 'KB', 'MB', 'GB');
+        for ($i=0; $bytes >= 1024 && $i < 8; $i++) {
+            $bytes /= 1024;
+        }
+        return round($bytes, 2) . $units[$i];
+    }
+    
 
     /**
-     * Display profiling data
+     * Display profiling data in html code
      *
-     * @param  boolean|string $print
+     * @param  boolean|string $print Whether output or return the result
      * @return \Widget\Marker|string
      */
     public function display($print = true)
