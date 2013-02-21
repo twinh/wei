11bd5faf201d55dea52b0239baf878964a8a2f8a
twinh <twinhuang@qq.com>
2013-02-21 04:41:46 -0800

    added test for cookie widget

---
+++ tests/WidgetTest/CookieTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use WidgetTest\TestCase;
+
+class CookieTest extends TestCase
+{
+    public function testInvoker() 
+    {
+        $cookie = $this->object;
+
+        $cookie('test', __METHOD__);
+
+        $this->assertEquals(__METHOD__, $cookie('test'));
+    }
+    
+    public function testGet() 
+    {
+        $widget = $this->object;
+
+        $widget->set('test', __METHOD__);
+
+        $this->assertEquals(__METHOD__, $widget->get('test'));
+    }
+
+    public function testSet() 
+    {
+        $widget = $this->object;
+
+        $widget->set('test', __METHOD__, array('expire' => 1));
+
+        $this->assertEquals(__METHOD__, $widget->get('test'));
+
+        $widget->set('test', __METHOD__, array('expire' => -1));
+
+        $this->assertEquals(null, $widget->get('test'), 'test expired cookie');
+        
+        
+    }
+
+    public function testRemove() 
+    {
+        $widget = $this->object;
+
+        $widget->set('test', __METHOD__);
+
+        $this->assertEquals(__METHOD__, $widget->get('test'));
+
+        $widget->offsetUnset('test');
+
+        $this->assertEquals(null, $widget->get('test'));
+    }
+}
