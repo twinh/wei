52518b488fca9afc067bc10c6f7ec2d7d2d2bac7
twinh <twinhuang@qq.com>
2013-02-21 05:06:15 -0800

    added uuid validtor

---
+++ lib/Widget/Validator/Uuid.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Validator;
+
+/**
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class Uuid extends Regex
+{
+    protected $patternMessage = '%name% must be valid UUID';
+    
+    protected $notMessage = '%name% must not be valid UUID';
+    
+    protected $pattern = '/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/';
+}
