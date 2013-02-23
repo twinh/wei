673a0fbd346556ffdaaa4dc1b899e20e198ebe43
twinh <twinhuang@qq.com>
2013-02-22 22:58:24 -0800

    added more ipv6 ip test

--- tests/WidgetTest/Validator/IpTest.php
+++ tests/WidgetTest/Validator/IpTest.php
@@ -37,6 +37,21 @@ class IpTest extends TestCase
             array('2001:0db8:85a3:08d3:1319:8a2e:0370:7334', array(
                 'ipv6' => true,
             )),
+            array('0:0:0:0:0:0:0:0', array(
+                'ipv6' => true,
+            )),
+            array('::', array(
+                'ipv6' => true,
+            )),
+            array('0::', array(
+                'ipv6' => true,
+            )),
+            array('::0', array(
+                'ipv6' => true,
+            )),
+            array('0::0', array(
+                'ipv6' => true,
+            )),
         );
     }
 
