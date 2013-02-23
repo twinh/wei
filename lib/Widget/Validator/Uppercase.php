aee91ec8ae2cc6c884d12d63390a68399ca42d98
twinh <twinhuang@qq.com>
2013-02-22 23:13:38 -0800

    added uppercase validator

---
+++ lib/Widget/Validator/Uppercase.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Validator;
+
+/**
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class Uppercase extends AbstractValidator
+{
+    protected $invalidMessage = '%name% must be uppercase';
+    
+    protected $notMessage = '%name% must not be uppercase';
+    
+    protected function validate($input)
+    {
+        if (!$this->isString($input)) {
+            $this->addError('notString');
+            return false;
+        }
+        
+        if (mb_strtoupper($input, mb_detect_encoding($input)) != $input) {
+            $this->addError('invalid');
+            return false;
+        }
+        return true;
+    }
+}
