bf428f47cdb8b27f8491d66c74426baf8f27a66a
twinh <twinhuang@qq.com>
2013-02-23 02:51:22 -0800

    added divisibleBy validator

---
+++ lib/Widget/Validator/DivisibleBy.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Validator;
+
+/**
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class DivisibleBy extends AbstractValidator
+{
+    protected $notDivisibleMessage = '%name% must be divisible by %divisor%';
+    
+    protected $notMessage = '%name% must not be divisible by %divisor%';
+    
+    /**
+     * Set divisor to 1 to avoid "division by zero" error
+     * 
+     * @var int|float
+     */
+    protected $divisor = 1;
+    
+    public function __invoke($input, $divisor = null)
+    {
+        $divisor && $this->divisor = $divisor;
+        
+        return $this->isValid($input);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    protected function validate($input)
+    {
+        if (!$this->isString($input)) {
+            $this->addError('notDivisible');
+            return false;
+        }
+        
+        if (is_float($this->divisor)) {
+            $result = fmod($input, $this->divisor);
+        } else {
+            $result = $input % $this->divisor;
+        }
+        
+        if (0 != $result) {
+            $this->addError('notDivisible');
+            return false;
+        }
+        return true;
+    }    
+}
