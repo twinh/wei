1d0dec8eacd71ea4f64e8f94bc15819a082711bf
twinh <twinhuang@qq.com>
2013-02-24 08:14:36 -0800

    added AbstractWidgetAware class

---
+++ lib/Widget/AbstractWidgetAware.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget;
+
+/**
+ * The infterce for all widget
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+abstract class AbstractWidgetAware implements WidgetAwareInterface
+{
+    /**
+     * The widget manager
+     * 
+     * @var \Widget\Widget
+     */
+    protected $widget;
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function __call($name, $args)
+    {
+        return call_user_func_array($this->$name, $args);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __get($name)
+    {
+        return $this->$name = $this->widget->get($name);
+    }
+}
