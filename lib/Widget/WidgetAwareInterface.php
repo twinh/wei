fab8aa19977c90ec663e918e240df3d3842c73fd
twinh <twinhuang@qq.com>
2013-02-24 08:01:55 -0800

    added WidgetAwareInterface interface

---
+++ lib/Widget/WidgetAwareInterface.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget;
+
+/**
+ * The infterce for all widget
+ *
+ * @package     Widget
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+interface WidgetAwareInterface
+{
+    /**
+     * Get the widget object by the given name
+     *
+     * @param  string       $name The name of widget
+     * @return \Widget\WidgetInterface
+     */
+    public function __get($name);
+
+    /**
+     * Invoke the widget by the given name
+     *
+     * @param  string $name The name of widget
+     * @param  array  $args The arguments for widget's __invoke method
+     * @return mixed
+     */
+    public function __call($name, $args);
+}
