fa057676ef8fb6691de95eb44c249b083be226ee
twinh <twinhuang@qq.com>
2013-02-25 01:15:02 -0800

    added missing docblock for exception's __construct method

--- lib/Widget/Exception.php
+++ lib/Widget/Exception.php
@@ -15,6 +15,14 @@ namespace Widget;
  */
 class Exception extends \Exception
 {
+    /**
+     * Constructor
+     * 
+     * @param string    $message    The Exception message
+     * @param int       $code       The exception code
+     * @param string    $file       The filename in which the exception was created
+     * @param int       $line       The file line in which the exception occurred
+     */
     public function __construct($message, $code = 500, $file = null, $line = null)
     {
         $this->message = $message;
