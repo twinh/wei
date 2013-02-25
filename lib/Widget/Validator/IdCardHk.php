f4680d0fa50a580ca5a6c9bbcaae55dbf36537b5
twinh <twinhuang@qq.com>
2013-02-25 06:11:34 -0800

    added HoHong Kong id card validator

---
+++ lib/Widget/Validator/IdCardHk.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Validator;
+
+/**
+ * Check if the input value is valid Hong Kong identity card
+ * 
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class IdCardHk extends AbstractValidator
+{
+    protected $invalidMessage = '%name% must be valid Hong Kong identity card';
+    
+    protected $notMessage = '%name% must not be valid Hong Kong identity card';
+        
+    /**
+     * {@inheritdoc}
+     */
+    protected function validate($input) 
+    {
+        if (!$this->isString($input)) {
+            $this->addError('notString');
+            return false;
+        }
+        
+        if (8 != strlen($input)) {
+            $this->addError('invalid');
+            return false;
+        }
+        
+        $input = strtoupper($input);
+        
+        // The first char should be A-Z
+        $first = ord($input[0]);
+        if ($first <  65 || $first > 90) {
+            $this->addError('invalid');
+            return false;
+        }
+        
+        // c1 = ord(c1) - 64 => A=1, B=2, ... , Z=26
+        // sum = c1*8 + c2*7 + ... + c6*3 + c5*2
+        $sum = ($first - 64) * 8;
+        for ($i = 1, $j = 7; $i < 7; $i++, $j--) {
+            $sum += $input[$i] * $j;
+        }
+
+        $checksum = $sum % 11;
+        if ($checksum == 1) {
+            $checksum = 'A';
+        } elseif ($checksum > 1) {
+            $checksum = 11 - $checksum;
+        }
+        
+        if ($checksum != $input[7]) {
+            $this->addError('invalid');
+            return false;
+        }
+        
+        return true;
+    }
+}
