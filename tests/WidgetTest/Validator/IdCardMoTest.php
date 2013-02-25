8c2d900bbc94ce1f268ccc2817ea370b745988bc
twinh <twinhuang@qq.com>
2013-02-25 08:31:27 -0800

    added test for Macau id card validator

---
+++ tests/WidgetTest/Validator/IdCardMoTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace WidgetTest\Validator;
+
+class IdCardMoTest extends TestCase
+{
+    /**
+     * @dataProvider providerForIdCardMo
+     */
+    public function testIdCardMo($input)
+    {
+        $this->assertTrue($this->is('idCardMo', $input));
+        
+        $this->assertFalse($this->is('notIdCardMo', $input));
+    }
+
+    /**
+     * @dataProvider providerForNotIdCardMo
+     */
+    public function testNotIdCardMo($input)
+    {
+        $this->assertFalse($this->is('idCardMo', $input));
+        
+        $this->assertTrue($this->is('notIdCardMo', $input));
+    }
+
+    public function providerForIdCardMo()
+    {
+        return array(
+            array('11111111'),
+            array('55555555'),
+            array('77777777')
+        );
+    }
+
+    public function providerForNotIdCardMo()
+    {
+        return array(
+            array('00000000'), // first digit should be 1,5,7
+            array('22222222'), 
+            array('33333333'),
+            array('44444444'),
+            array('66666666'),
+            array('88888888'),
+            array('99999999'),
+            array('1234567'), // length
+        );
+    }
+}
