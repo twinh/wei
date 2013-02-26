3a6832e64e43b64f6ce6c8e5051754d4ad9a1447
twinh <twinhuang@qq.com>
2013-02-25 19:53:16 -0800

    added IOException

---
+++ lib/Widget/Exception/IoException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget\Exception;
+
+/**
+ * The exception thrown when file or stream operation fail
+ * 
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+class IOException extends \RuntimeException implements ExceptionInterface
+{
+}
