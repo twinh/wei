85d9f75fe5043105a1f12ed61cb0e3a86cb07d3f
twinh <twinhuang@qq.com>
2013-02-26 08:51:25 -0800

    added short description for invalidArgumentException

--- lib/Widget/Exception/InvalidArgumentException.php
+++ lib/Widget/Exception/InvalidArgumentException.php
@@ -9,6 +9,8 @@
 namespace Widget\Exception;
 
 /**
+ * Exception thrown if an argument does not match with the expected value
+ * 
  * @author      Twin Huang <twinh@yahoo.cn>
  */
 class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
