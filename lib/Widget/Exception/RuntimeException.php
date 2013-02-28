e595db45b07240ea1433eac2d309e94bc43d441e
twinh <twinhuang@qq.com>
2013-02-27 23:59:26 -0800

    added class description for RuntimeException class

--- lib/Widget/Exception/RuntimeException.php
+++ lib/Widget/Exception/RuntimeException.php
@@ -9,7 +9,10 @@
 namespace Widget\Exception;
 
 /**
+ * Exception thrown if an error which can only be found on runtime occurs.
+ * 
  * @author      Twin Huang <twinh@yahoo.cn>
+ * @link http://php.net/manual/en/class.runtimeexception.php
  */
 class RuntimeException extends \RuntimeException implements ExceptionInterface
 {
