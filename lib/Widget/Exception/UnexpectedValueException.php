5b5b7943dbea3731ede4902afa211cbaa721e864
twinh <twinhuang@qq.com>
2013-02-27 22:17:10 -0800

    added class description for unexpectedValueException

--- lib/Widget/Exception/UnexpectedValueException.php
+++ lib/Widget/Exception/UnexpectedValueException.php
@@ -9,6 +9,12 @@
 namespace Widget\Exception;
 
 /**
+ * Exception thrown if a value does not match with a set of values. Typically
+ * this happens when a function calls another function and expects the return
+ * value to be of a certain type or value not including arithmetic or buffer
+ * related errors.
+ * 
+ * @link http://php.net/manual/en/class.unexpectedvalueexception.php
  * @author      Twin Huang <twinh@yahoo.cn>
  */
 class UnexpectedValueException extends \UnexpectedValueException implements ExceptionInterface
