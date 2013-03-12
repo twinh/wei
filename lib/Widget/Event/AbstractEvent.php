4311c1132c567f89b075fe711b0b9b6f9f9c0e40
twinh <twinhuang@qq.com>
2013-03-12 08:11:34 -0700

    added event interface and abstract class

---
+++ lib/Widget/Event/AbstractEvent.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget;
+
+/**
+ * A simple implementation of \Widget\Event\EventInterface
+ *
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+abstract class AbstractEvent extends AbstractWidget implements EventInterface
+{
+    
+}
---
+++ lib/Widget/Event/EventInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget;
+
+/**
+ * The base interface for event class
+ *
+ * @author      Twin Huang <twinh@yahoo.cn>
+ */
+interface EventInterface
+{
+    
+}
