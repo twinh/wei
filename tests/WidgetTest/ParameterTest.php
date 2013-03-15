2a24e2f7dd655e5ed16d658528b896a3a597535b
twinh <twinhuang@qq.com>
2013-03-14 23:42:26 -0700

    added test for parameters widget

---
+++ tests/WidgetTest/ParameterTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace WidgetTest;
+
+class ParameterTest extends TestCase
+{
+    public function createParameterObject($type, $class)
+    {
+        // create request widget from custom parameter
+        $request = new \Widget\Request(array(
+            'widget' => $this->widget,
+            'fromGlobal' => false,
+            $type => array(
+                'key' => 'value',
+                'key2' => 'value2',
+                'int' => '5',
+                'array' => array(
+                    'item' => 'value'
+                )
+            )
+        ));
+
+        if ('request' === $class) {
+            return $request;
+        } else {
+            // inject request widget
+            $class = '\Widget\\' . ucfirst($class);
+            $parameter = new $class(array(
+                'widget' => $this->widget,
+                'request' => $request
+            ));
+            return $parameter;
+        }
+    }
+    
+    public function testGetter()
+    {
+        $parameters = array(
+            'get' => 'query',
+            'post' => 'post',
+            'cookie' => 'cookie',
+            'servers' => 'server',
+            'data' => 'request'
+        );
+
+        foreach ($parameters as $type => $class) {
+            $parameter = $this->createParameterObject($type, $class);
+
+            $this->assertEquals('value', $parameter->get('key'));
+
+            $this->assertEquals(5, $parameter->getInt('int'));
+
+            $this->assertEquals(array('5'), $parameter->getArray('int'));
+
+            // int => 5, not in specified array
+            $this->assertEquals('firstValue', $parameter->getInArray('int', array(
+                'firstKey' => 'firstValue',
+                'secondKey' => 'secondValue'
+            )));
+
+            $this->assertEquals(6, $parameter->getInt('int', 6));
+        }
+    }
+}
