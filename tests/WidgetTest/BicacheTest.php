48e0b2973fd41548484e936872849c79dd131de3
twinh <twinhuang@qq.com>
2013-03-17 09:17:29 -0700

    marked test skip when apc not loaded

--- tests/WidgetTest/BicacheTest.php
+++ tests/WidgetTest/BicacheTest.php
@@ -4,5 +4,12 @@ namespace WidgetTest;
 
 class BicacheTest extends CacheTestCase
 {
-    
+    protected function setUp()
+    {
+        // FIXME
+        if (!extension_loaded('apc') || !ini_get('apc.enable_cli')) {
+            $this->markTestSkipped('Extension "apc" is not loaded.');
+        }
+        parent::setUp();
+    }
 }
