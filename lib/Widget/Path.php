ad81598b1429e73f6ee7723eb68109b9a29847f9
twinh <twinhuang@qq.com>
2013-03-31 20:39:38 -0700

    added path widget

---
+++ lib/Widget/Path.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://www.opensource.org/licenses/apache2.0.php Apache License
+ */
+
+namespace Widget;
+
+/**
+ * Alias of $router->generatePath widget
+ *
+ * @author      Twin Huang <twinh@yahoo.cn>
+ * @property    \Widget\Router $router Router
+ */
+class Path extends AbstractWidget
+{
+    public function __invoke(array $parameters = array(), $name = null)
+    {
+        return $this->router->generatePath($parameters, $name);
+    }
+}
