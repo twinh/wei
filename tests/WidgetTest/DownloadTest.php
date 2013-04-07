f5e212f54a17a89d020313e9b22ed0382864a375
twinh <twinhuang@qq.com>
2013-04-06 18:56:20 -0700

    added test for download widget

---
+++ tests/WidgetTest/DownloadTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace WidgetTest;
+
+class DownloadTest extends TestCase
+{
+    public function testDownload()
+    {
+        ob_start();
+        $this->download(__DIR__ . '/Fixtures/view.php');
+        $content = ob_get_clean();
+
+        $this->assertContains('<?php $this->layout(\'layout.php\') ?>', $content);
+    }
+    
+    /**
+     * @expectedException Widget\Exception\NotFoundException
+     */
+    public function testFileNotFoundException()
+    {
+        $this->download('not found file');
+    }
+}
