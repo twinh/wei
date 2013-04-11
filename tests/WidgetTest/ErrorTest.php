6376c940aee6a8e49f8027041af3cd7bf393e8fc
twinh <twinhuang@qq.com>
2013-04-11 00:00:44 -0700

    updated priority to bigger int

--- tests/WidgetTest/ErrorTest.php
+++ tests/WidgetTest/ErrorTest.php
@@ -10,4 +10,15 @@ class ErrorTest extends TestCase
         
         $this->trigger('exception', array(new \Exception('testException')));
     }
+    
+    public function testAjaxException()
+    {
+        $this->server->set('HTTP_X_REQUESTED_WITH', 'xmlhttprequest');
+        
+        ob_start();
+        $this->trigger('exception', array(new \Exception('ajax error')));
+        $json = json_decode(ob_get_clean());
+        var_dump($json);
+        $this->assertSame(json_last_error(), JSON_ERROR_NONE);
+    }
 }
