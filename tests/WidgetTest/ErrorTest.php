1b80dad2349a71bce49607b37203b81aa1d8bb25
twinh <twinhuang@qq.com>
2013-04-11 06:01:03 -0700

    fixed error in error widget test

--- tests/WidgetTest/ErrorTest.php
+++ tests/WidgetTest/ErrorTest.php
@@ -4,6 +4,9 @@ namespace WidgetTest;
 
 class ErrorTest extends TestCase
 {
+    /**
+     * @runInSeparateProcess
+     */
     public function testException()
     {
         $this->expectOutputRegex('/testException/');
@@ -11,14 +14,30 @@ class ErrorTest extends TestCase
         $this->trigger('exception', array(new \Exception('testException')));
     }
     
+    /**
+     * @runInSeparateProcess
+     */
     public function testAjaxException()
     {
         $this->server->set('HTTP_X_REQUESTED_WITH', 'xmlhttprequest');
-        
+
         ob_start();
-        $this->trigger('exception', array(new \Exception('ajax error')));
-        $json = json_decode(ob_get_clean());
-        var_dump($json);
+        $this->trigger('exception', array(new \Exception('ajax error', -250)));
+        $json = json_decode(ob_get_clean(), true);
+        
         $this->assertSame(json_last_error(), JSON_ERROR_NONE);
+        $this->assertEquals($json['message'], 'ajax error');
+        $this->assertEquals($json['code'], -250);
+        $this->assertContains('Threw by Exception in', $json['detail']);
+    }
+    
+    public function testErrorHandler()
+    {
+        error_reporting(0);
+        
+        // Cover \Widget\Error::hanldeError
+        $array['key'];
+        
+        error_reporting(-1);
     }
 }
