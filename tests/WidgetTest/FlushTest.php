3e66bc3cb2d6f7a7b3d740d6151e017a09de1c8b
twinh <twinhuang@qq.com>
2013-04-12 23:49:14 -0700

    updated test for flush widget

--- tests/WidgetTest/FlushTest.php
+++ tests/WidgetTest/FlushTest.php
@@ -1,29 +1,38 @@
 <?php
 
-namespace WidgetTest;
-
-class FlushTest extends TestCase
+namespace WidgetTest
 {
-    public function setUp()
+    class FlushTest extends TestCase
     {
-        parent::setUp();
-    }
-    
-    public function tearDown()
-    {
-        parent::tearDown();
-        
-        /**
-         * @link https://github.com/symfony/symfony/issues/2531
-         * @link https://github.com/sebastianbergmann/phpunit/issues/390
-         */
-        if(ob_get_length() == 0) {
-            ob_start();
+        public function tearDown()
+        {
+            parent::tearDown();
+
+            /**
+             * @link https://github.com/symfony/symfony/issues/2531
+             * @link https://github.com/sebastianbergmann/phpunit/issues/390
+             */
+            if(ob_get_length() === 0) {
+                ob_start();
+            }
+        }
+
+        public function testInvoker()
+        {
+            $this->assertInstanceOf('\Widget\Flush', $this->flush());
+            
+            $this->assertEquals('0', ini_get('zlib.output_compression'));
+            
+            $this->assertEquals('1', ini_get('implicit_flush'));
+            
+            $this->assertEquals(0, ob_get_level());
         }
     }
-    
-    public function testInvoker()
-    {
-        $this->assertInstanceOf('\Widget\Flush', $this->flush());
+}
+
+namespace
+{
+    if (!function_exists('apache_setenv')) {
+        function apache_setenv(){}
     }
 }
