0299e6f8945e44d146e14c67d8b15e52c92cea88
twinh <twinhuang@qq.com>
2013-04-14 03:23:40 -0700

    Revert "using short tag in view file to echo varibale"
    This reverts commit 2dba2da145a7b074782341b61d61bffb9bba1478.

--- lib/Widget/Resource/views/error.php
+++ lib/Widget/Resource/views/error.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<title><?= $message ?></title>
+<title><?php echo $message ?></title>
 <style type="text/css">
 body { font-size: 12px; color: #333; padding: 15px 20px 20px 20px; }
 h1, h2, p, pre { margin: 0; padding: 0; }
@@ -15,15 +15,15 @@ strong, .error-text { color: #FF3000; }
 </style>
 </head>
 <body>
-<h1><?= $message ?></h1>
+<h1><?php echo $message ?></h1>
 <?php if ($debug) : ?>
     <h2>File</h2>
-    <p class="error-text"><?= $file ?> modified at <?= $mtime ?></p>
-    <p><pre><?= $fileInfo ?></pre></p>
+    <p class="error-text"><?php echo $file ?> modified at <?php echo $mtime ?></p>
+    <p><pre><?php echo $fileInfo ?></pre></p>
 
     <h2>Trace</h2>
-    <p class="error-text"><?= $detail ?></p>
-    <p><pre><?= $trace ?></pre></p>
+    <p class="error-text"><?php echo $detail ?></p>
+    <p><pre><?php echo $trace ?></pre></p>
 <?php else : ?>
     <p>Unfortunately, an error occurred. Please try again later.</p>
 <?php endif ?>
