7b2e7420b19f66883e3c75e613601fdb064a2605
twinh <twinhuang@qq.com>
2013-05-02 08:33:11 -0700

    fixed class not found error

--- tests/WidgetTest/DbCacheTest.php
+++ tests/WidgetTest/DbCacheTest.php
@@ -4,6 +4,16 @@ namespace WidgetTest;
 
 class DbCacheTest extends CacheTestCase
 {
+    public function setUp()
+    {
+        if (!class_exists(('\Doctrine\DBAL\DriverManager'))) {
+            $this->markTestSkipped('doctrine\dbal is required');
+            return;
+        }
+        
+        parent::setUp();
+    }
+    
     public static function tearDownAfterClass()
     {
         @unlink('cache.sqlite');
