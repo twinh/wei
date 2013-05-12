60593ba8c68011815fcc377b8f31ecdc3a253ae0
twinh <twinhuang@qq.com>
2013-05-12 03:48:32 -0700

    improved more functions

--- lib/Widget/Call.php
+++ lib/Widget/Call.php
@@ -29,6 +29,10 @@ class Call extends AbstractWidget
 
     protected $complete;
 
+    protected $success;
+
+    protected $error;
+
     protected $time;
 
     protected $timeout;
@@ -39,50 +43,72 @@ class Call extends AbstractWidget
 
     protected $headers = array();
 
+    protected $wsdl = true;
+
     public function __invoke($options)
     {
         $this->setOption($options);
 
         switch ($this->type) {
             case 'url':
-                $ch = curl_init();
-                $opts = array();
-
-                if ('POST' === strtoupper($this->method)) {
-                    $opts[CURLOPT_URL] = $this->url;
-                    $opts[CURLOPT_POST] = 1;
-                    $opts[CURLOPT_POSTFIELDS] = $this->data;
-                } else {
-                    $query = http_build_query($this->data);
-                    if (false === strpos('?', $this->url)) {
-                        $opts[CURLOPT_URL] = $this->url . '?' . $query;
-                    } else {
-                        $opts[CURLOPT_URL] = $this->url . '?' . $query;
-                    }
-                }
-
-                curl_setopt_array($opts);
-                $response = curl_exec($ch);
-                if (false === $response) {
-                    $this->trigger('error', curl_error($ch));
-                } else {
-                    $this->handleResponse($response);
-                }
+                $this->handleUrl();
                 break;
 
             case 'soap':
-                $soap = new \SoapClient($this->url);
-
-                try {
-                    $response = $soap->__soapCall($this->method, $this->data);
-                    $this->handleResponse($response);
-                } catch (\SoapFault $e) {
-                    $this->trigger('error', $e);
-                }
+                $this->handleSoap();
                 break;
         }
 
         return $this;
+    }
+
+    public function handleUrl()
+    {
+        $ch = curl_init();
+        $opts = array();
+
+        if ('POST' === strtoupper($this->method)) {
+            $opts[CURLOPT_URL] = $this->url;
+            $opts[CURLOPT_POST] = 1;
+            $opts[CURLOPT_POSTFIELDS] = $this->data;
+        } else {
+            $query = http_build_query($this->data);
+            if (false === strpos('?', $this->url)) {
+                $opts[CURLOPT_URL] = $this->url . '?' . $query;
+            } else {
+                $opts[CURLOPT_URL] = $this->url . '?' . $query;
+            }
+        }
+
+        curl_setopt_array($ch, $opts);
+        $this->trigger('beforeSend', $ch);
+        $response = curl_exec($ch);
+        if (false === $response) {
+            $this->trigger('error', curl_error($ch));
+        } else {
+            $this->handleResponse($response);
+        }
+    }
+
+    public function handleSoap()
+    {
+        if ($this->wsdl) {
+            $soap = new \SoapClient($this->url);
+        } else {
+            $soap = new \SoapClient(null, array(
+                'location' => $this->url,
+                'uri' => $this->url,
+                'trace' => 1
+            ));
+        }
+
+        try {
+            $this->trigger('beforeSend', $soap);
+            $response = $soap->__soapCall($this->method, $this->data);
+            $this->handleResponse($response);
+        } catch (\SoapFault $e) {
+            $this->trigger('error', $e);
+        }
     }
 
     public function get($url, $data, $callback)
@@ -113,7 +139,7 @@ class Call extends AbstractWidget
 
     protected function handleResponse($response)
     {
-        $data = $this->decode($data, $this->dataType);
+        $data = $this->decode($response, $this->dataType);
         $this->trigger('success', $data);
     }
 
@@ -121,7 +147,15 @@ class Call extends AbstractWidget
     {
         switch ($type) {
             case 'json' :
-                return json_decode($data);
+                $data = json_decode($data);
+
+                if (null === $data && json_last_error() != JSON_ERROR_NONE) {
+                    $this->trigger('error');
+                }
+
+                return $data;
+            case 'raw' :
+                return $data;
 
             case 'xml' :
                 // todo
@@ -142,8 +176,12 @@ class Call extends AbstractWidget
         }
     }
 
-    protected function trigger($name)
+    protected function trigger($name, $params = array())
     {
-        return $this->eventManager->trigger('call' . ucfirst($name));
+        if (is_callable($this->$name)) {
+            $params = is_array($params) ? $params : array($params);
+            call_user_func_array($this->$name, $params);
+        }
+        //return $this->eventManager->trigger('call' . ucfirst($name));
     }
 }
