87b93523dd573945347df41a043744c909d8c958
twinh <twinhuang@qq.com>
2013-05-30 07:39:46 -0700

    fixed dirver name not set when provided pdo object

--- lib/Widget/Db.php
+++ lib/Widget/Db.php
@@ -91,19 +91,16 @@ class Db extends AbstractWidget
             return false;
         }
 
-        if ($this->pdo) {
-            $this->isConnected = true;
-            return true;
+        if (!$this->pdo) {
+            $this->pdo = new PDO($this->dsn, $this->user, $this->password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, true);
+            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         }
 
-        $this->pdo = new PDO($this->dsn, $this->user, $this->password);
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, true);
-        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
         $this->driver = $this->pdo->getAttribute(PDO::ATTR_DRIVER_NAME);
-
         $this->isConnected = true;
+
         return true;
     }
 
