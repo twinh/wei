5a0952d32eddd2b7b8988b5d15f51d46ec052962
twinh <twinhuang@qq.com>
2013-05-31 23:31:27 -0700

    added basic gravatar widget, ref #91

---
+++ lib/Widget/Gravatar.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Widget Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Widget;
+
+/**
+ * A widget to get either a Gravatar URL or complete image tag for a specified email address
+ *
+ * @author      Twin Huang <twinhuang@qq.com>
+ */
+class Gravatar extends AbstractWidget
+{
+    /**
+     * Get either a Gravatar URL or complete image tag for a specified email address.
+     *
+     * @param string $email The email address
+     * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
+     * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+     * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+     * @param boolean $img True to return a complete IMG tag False for just the URL
+     * @param array $atts Optional, additional key/value attributes to include in the IMG tag
+     * @return String containing either just a URL or a complete image tag
+     * @source http://gravatar.com/site/implement/images/php/
+     */
+    public function __invoke( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() )
+    {
+        $url = 'http://www.gravatar.com/avatar/';
+        $url .= md5( strtolower( trim( $email ) ) );
+        $url .= "?s=$s&d=$d&r=$r";
+        if ( $img ) {
+            $url = '<img src="' . $url . '"';
+            foreach ( $atts as $key => $val )
+                $url .= ' ' . $key . '="' . $val . '"';
+            $url .= ' />';
+        }
+        return $url;
+    }
+}
