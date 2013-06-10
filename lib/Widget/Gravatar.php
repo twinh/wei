5110d0893f1068aee010d8dbf9e3f3fc48e9b320
twinh <twinhuang@qq.com>
2013-06-10 07:53:15 -0700

    added options for gravatar widget, ref #91

--- lib/Widget/Gravatar.php
+++ lib/Widget/Gravatar.php
@@ -9,35 +9,102 @@
 namespace Widget;
 
 /**
- * A widget to get either a Gravatar URL or complete image tag for a specified email address
+ * A widget to generate a Gravatar URL for a specified email address
  *
  * @author      Twin Huang <twinhuang@qq.com>
+ * @link        https://gravatar.com/site/implement/images/
  */
 class Gravatar extends AbstractWidget
 {
     /**
-     * Get either a Gravatar URL or complete image tag for a specified email address.
+     * The default image type or URL when email do not have a Gravatar image
+     *
+     * The image type could be 404, mm, identicon, monsterid or wavatar
+     *
+     * @var string
+     */
+    protected $default = 'mm';
+
+    /**
+     * Whether display Gravatar in HTTPS request
+     *
+     * @var bool
+     */
+    protected $secure = false;
+
+    /**
+     * The default size of image, from 1px up to 2048px
+     *
+     * @var int
+     */
+    protected $size = 80;
+
+    /**
+     * The image size for `small` method
+     *
+     * @var int
+     */
+    protected $smallSize = 48;
+
+    /**
+     * The image size for large
+     *
+     * @var int
+     */
+    protected $largeSize = 200;
+
+    /**
+     * Generate a Gravatar URL for a specified email address
      *
      * @param string $email The email address
-     * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
-     * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
-     * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
-     * @param boolean $img True to return a complete IMG tag False for just the URL
-     * @param array $atts Optional, additional key/value attributes to include in the IMG tag
-     * @return String containing either just a URL or a complete image tag
-     * @source http://gravatar.com/site/implement/images/php/
+     * @param string $size The image size in pixels
+     * @param string $default The image type or URL when email do not have a Gravatar image
+     * @param string $rating The image rating
+     * @return string A image URL
+     * @link http://gravatar.com/site/implement/images/php/
      */
-    public function __invoke( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() )
+    public function __invoke($email, $size = null, $default = null, $rating = null)
     {
-        $url = 'http://www.gravatar.com/avatar/';
-        $url .= md5( strtolower( trim( $email ) ) );
-        $url .= "?s=$s&d=$d&r=$r";
-        if ( $img ) {
-            $url = '<img src="' . $url . '"';
-            foreach ( $atts as $key => $val )
-                $url .= ' ' . $key . '="' . $val . '"';
-            $url .= ' />';
+        if ($this->secure) {
+            $url = 'https://secure.gravatar.com/';
+        } else {
+            $url = 'http://www.gravatar.com/avatar/';
         }
+
+        $url .= md5(strtolower(trim($email)));
+        $url .= '?=' . $size ?: $this->size;
+        $url .= '&d' . $default ?: $this->default;
+
+        if ($rating) {
+            $url .= '&r=' . $rating;
+        }
+
         return $url;
+    }
+
+    /**
+     * Generate a large size Gravatar URL for a specified email address
+     *
+     * @param string $email The email address
+     * @param string $default The image type or URL when email do not have a Gravatar image
+     * @param string $rating The image rating
+     * @return string
+     */
+    public function large($email, $default = null, $rating = null)
+    {
+        return $this->__invoke($email, $this->largeSize, $default, $rating);
+    }
+
+    /**
+     * Generate a small size Gravatar URL for a specified email address
+     *
+     * @param string $email The email address
+     * @param string $default The image type or URL when email do not have a Gravatar image
+     * @param string $rating The image rating
+     * @return string
+     */
+    public function small($email, $default = null, $rating = null)
+    {
+        return $this->__invoke($email, $this->smallSize, $default, $rating);
     }
 }
