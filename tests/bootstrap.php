c5937e6d26856814baf4dc73ed0338337652af94
twinh <twinhuang@qq.com>
2013-06-12 23:50:20 -0700

    used debug => true instead error_reporting ini configutation

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,11 +10,8 @@ require_once dirname(__DIR__) . '/lib/Widget/Widget.php';
 
 return Widget::create(array(
     'widget' => array(
+        // Display all error message
         'debug' => true,
-        'inis' => array(
-            // Display all error message
-            'error_reporting' => -1,
-        ),
         // Set up autoload for WidgetTest namespace
         'autoloadMap' => array(
             'WidgetTest' => __DIR__
