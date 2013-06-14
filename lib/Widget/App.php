e1869e807237ba99fb4c590585f4ba131a0cd808
Twin Huang <twinh@tencent.com>
2013-06-14 15:19:43 +0800

    changed line separator

--- lib/Widget/App.php
+++ lib/Widget/App.php
@@ -19,6 +19,7 @@ use Widget\Response;
  * @property    Logger $logger The logger widget
  * @property    Request $request The HTTP request widget
  */
+/** @noinspection PhpUndefinedClassInspection */
 class App extends AbstractWidget
 {
     const FORWARD_CODE = 1000;
