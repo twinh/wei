8aa268a8a65f0d6a93db06b77a38c91aa98485fe
twinh <twinhuang@qq.com>
2013-06-17 09:59:18 -0700

    fixed test error in circleci

--- tests/WidgetTest/Validator/ExistsTest.php
+++ tests/WidgetTest/Validator/ExistsTest.php
@@ -11,18 +11,5 @@ class ExistsTest extends TestCase
         $this->assertTrue($this->isExists(__FILE__), 'File found');
 
         $this->assertFalse($this->isExists('.file not found'), 'File not found');
-
-        $paths = explode(PATH_SEPARATOR, ini_get('include_path'));
-        $path = array_pop($paths);
-        $files = scandir($path);
-        foreach ($files as $file) {
-            if ('.' == $file || '..' == $file) {
-                continue;
-            }
-            if (file_exists($path . DIRECTORY_SEPARATOR . $file)) {
-                $this->assertNotEquals(false, $this->isExists($file), 'File in include path found');
-                break;
-            }
-        }
     }
 }
--- tests/WidgetTest/Validator/FileTest.php
+++ tests/WidgetTest/Validator/FileTest.php
@@ -18,19 +18,6 @@ class FileTest extends TestCase
         $this->assertTrue($this->is('file', __FILE__), 'File found');
 
         $this->assertFalse($this->is('file', '.file not found'), 'File not found');
-
-        $paths = explode(PATH_SEPARATOR, ini_get('include_path'));
-        $path = array_pop($paths);
-        $files = scandir($path);
-        foreach ($files as $file) {
-            if ('.' == $file || '..' == $file) {
-                continue;
-            }
-            if (is_file($path . DIRECTORY_SEPARATOR . $file)) {
-                $this->assertNotEquals(false, $this->is('file', $file), 'File in include path found');
-                break;
-            }
-        }
     }
 
     public function testNotFound()
