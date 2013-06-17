bc4d6e6f08cb0a2996faa7c89a3ee5f5bd0af4e4
twinh <twinhuang@qq.com>
2013-06-16 22:10:18 -0700

    fixed record exists test error

--- tests/WidgetTest/Validator/RecordExistsTest.php
+++ tests/WidgetTest/Validator/RecordExistsTest.php
@@ -8,15 +8,19 @@ class RecordExistsTest extends TestCase
         'table' => 'users'
     );
 
-    public static function setUpBeforeClass()
+    public function setUp()
     {
-        parent::setUpBeforeClass();
+        if (!class_exists(('\Doctrine\DBAL\DriverManager'))) {
+            $this->markTestSkipped('doctrine\dbal is required');
+        }
+
+        parent::setUp();
 
         if (!class_exists(('\Doctrine\DBAL\DriverManager'))) {
             return;
         }
 
-        $widget = \Widget\Widget::create();
+        $widget = $this->widget;
 
         /* @var $db \Doctrine\DBAL\Connection */
         $db = $widget->dbal();
@@ -32,31 +36,6 @@ class RecordExistsTest extends TestCase
             'name' => 'test',
             'address' => 'test'
         ));
-    }
-
-    public static function tearDownAfterClass()
-    {
-        if (!class_exists(('\Doctrine\DBAL\DriverManager'))) {
-            return;
-        }
-
-        $widget = \Widget\Widget::create();
-
-        try {
-            $widget->dbal()->query("DROP TABLE users");
-        } catch (\Doctrine\DBAL\DBALException $e) {
-            // ignore
-        }
-
-    }
-
-    public function setUp()
-    {
-        if (!class_exists(('\Doctrine\DBAL\DriverManager'))) {
-            $this->markTestSkipped('doctrine\dbal is required');
-        }
-
-        parent::setUp();
     }
 
     /**
