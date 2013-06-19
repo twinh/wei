f10eb85c3e6808b1064c5f05fe02284aec33dc3a
twinh <twinhuang@qq.com>
2013-06-18 22:45:04 -0700

    fixed typo

--- tests/WidgetTest/RedisTest.php
+++ tests/WidgetTest/RedisTest.php
@@ -7,38 +7,38 @@ class RedisTest extends CacheTestCase
     public function setUp()
     {
         if (!extension_loaded('redis') || !class_exists('\Redis')) {
-            $this->markTestSkipped('The memcache extension is not loaded');
+            $this->markTestSkipped('The "redis" extension is not loaded');
         }
-        
+
         parent::setUp();
-        
+
         try {
             $this->object->get('test');
         } catch (\RedisException $e) {
             $this->markTestSkipped('The redis server is not running');
         }
     }
-    
+
     public function testIncrementAndDecrement()
     {
         $redis = $this->object->getObject();
-        
+
         // FIXME
         // Avoid segmentation fault
         $redis->setOption(\Redis::OPT_SERIALIZER, \Redis::SERIALIZER_NONE);
-        
+
         parent::testIncrementAndDecrement();
     }
-    
+
     public function testGetAndSetObject()
     {
         $cache = $this->object;
         $redis = $cache->getObject();
-        
+
         $this->assertInstanceOf('\Redis', $redis);
-        
+
         $cache->setObject($redis);
-        
+
         $this->assertInstanceOf('\Redis', $cache->getObject());
     }
 }
