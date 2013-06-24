d652a9ca3edcccbbe9045172b57a1b6a035bb16c
twinh <twinhuang@qq.com>
2013-06-23 22:31:49 -0700

    added test for response toString

--- tests/WidgetTest/ResponseTest.php
+++ tests/WidgetTest/ResponseTest.php
@@ -33,8 +33,22 @@ class ResponseTest extends TestCase
     {
         $response = $this->object;
 
+        $this->assertFalse($response->isSent());
         $this->assertEquals('content', $this->getOutput($response, 'content', 304));
         $this->assertEquals(304, $response->getStatusCode());
+        $this->assertTrue($response->isSent());
+    }
+
+    public function testToString()
+    {
+        $response = $this->object;
+
+        $response->header->set(array(
+            'Key' => 'Value',
+            'Key1' => 'Value1'
+        ));
+
+        $this->assertEquals("HTTP/1.1 200 OK\r\nKey: Value\r\nKey1: Value1\r\n\r\n", (string)$response);
     }
 
     public function getOutput(\Widget\Response $response, $content = null, $statusCode = null)
