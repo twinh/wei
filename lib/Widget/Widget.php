c3e6b10d934328872bcfe5382d5bff7c138a2b16
twinh <twinhuang@qq.com>
2013-06-29 09:53:07 -0700

    try to debug 5.3.3 error 2

--- lib/Widget/Widget.php
+++ lib/Widget/Widget.php
@@ -478,7 +478,7 @@ class Widget extends AbstractWidget
 
         if ($bool) {
             error_reporting(-1);
-            ini_set('display_errors', 1);
+            //ini_set('display_errors', 1);
         }
 
         return $this;
