492fb3034e89b77b8f606fc2ff340ec71143ceb4
twinh <twinhuang@qq.com>
2013-07-02 07:06:27 -0700

    added demo: using json as widget configuration

---
+++ demos/using-json-as-widget-configuration/README.md
@@ -0,0 +1,1 @@
+# Using JSON as widget configuration
---
+++ demos/using-json-as-widget-configuration/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'vendor/autoload.php';
+require '../../lib/Widget/Widget.php';
+
+// Get widget manager
+$widget = widget();
+
+// Get current environment name
+$env = $widget->env();
+
+// Load configuration by environment name
+$file = 'config/config_' . $env . '.json';
+
+if (is_file($file)) {
+    // TODO cache me
+    $config = json_decode(file_get_contents($file), true);
+    widget($config);
+} else {
+    echo sprintf('JSON configuration file "%s" not found', $file);
+    return;
+}
+
+// Output configuration
+var_dump($widget->config('widget'));
