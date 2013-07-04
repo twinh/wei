5281a0691a3c631c92084414c3077305cd825d2a
twinh <twinhuang@qq.com>
2013-07-03 22:27:31 -0700

    added missing pgsqlCacheTest

---
+++ tests/WidgetTest/PgsqlCacheTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace WidgetTest;
+
+class PgsqlCacheTest extends CacheTestCase
+{
+    public function setUp()
+    {
+        if (!class_exists(('\Doctrine\DBAL\DriverManager'))) {
+            $this->markTestSkipped('doctrine\dbal is required');
+            return;
+        }
+
+        parent::setUp();
+
+        // TODO better way
+        try {
+            $this->object = $this->widget->pgsqlCache;
+        } catch (\PDOException $e) {
+            $this->markTestSkipped($e->getMessage());
+        }
+    }
+}
