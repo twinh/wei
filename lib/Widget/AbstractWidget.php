9ec84dd0904c88aa8e81568a08e61c18956ad1e5
twinh <twinhuang@qq.com>
2013-07-11 09:57:37 -0700

    updated api method order

--- lib/Widget/AbstractWidget.php
+++ lib/Widget/AbstractWidget.php
@@ -15,6 +15,40 @@ namespace Widget;
  *
  * @method   mixed      __invoke(mixed $mixed) The invoke method
  *
+ * Cache
+ * @property Cache      $cache A cache widget proxy
+ * @method   mixed      cache($key, $value = null, $expire = 0) Retrieve or store an item by cache
+ * @property ArrayCache $arrayCache  A cache widget stored data in PHP array
+ * @method   mixed      arrayCache($key, $value = null, $expire = 0) Retrieve or store an item by array cache
+ * @property Apc        $apc The PHP APC cache widget
+ * @method   mixed      apc($key, $value = null, $expire = 0) Retrieve or store an item
+ * @property DbCache    $dbCache A database cache widget
+ * @method   mixed      dbCache($key, $value = null, $expire = 0) Retrieve or store an item by database cache
+ * @property FileCache  $fileCache A file cache widget
+ * @method   mixed      fileCache($key, $value = null, $expire = 0) Retrieve or store an item by file
+ * @property Memcache   $memcache A cache widget base on Memcache
+ * @method   mixed      memcache($key, $value = null, $expire = 0) Retrieve or store an item by Memcache
+ * @property Memcached  $memcached A cache widget base on Memcached
+ * @method   mixed      memcached($key, $value = null, $expire = 0) Retrieve or store an item by Memcached
+ * @property Couchbase  $couchbase A cache widget base on Couchbase
+ * @method   mixed      couchbase($key, $value = null, $expire = 0) Retrieve or store an item by Couchbase
+ * @property redis      $redis A cache widget base on Redis
+ * @method   mixed      redis($key, $value = null, $expire = 0) Retrieve or store an item by Redis
+ * @property Bicache    $bicache A two-level cache widget
+ * @method   mixed      bicache($key, $value = null, $expire = 0) Retrieve or store an item by two-level cache
+ *
+ * Database
+ * @property Db                           $db A database widget
+ * @method   Db\QueryBuilder              db()
+ *
+ * @property Call       $call A widget handles HTTP request like jQuery Ajax
+ * @method   Call       call(array $options) Create a new call object and execute
+ *
+ * Validation
+ * @method   Validate   validate(array $option) Create a new validator and validate by specified options
+ * @property Is         $is The validator manager, use to validate input quickly, create validator
+ * @method   bool       is($rule = null, $input = null, $options = array()) Validate input by given rule
+ *
  * HTTP Request
  * @property Request    $request A widget that handles the HTTP request data
  * @method   mixed      request($name, $default = null) Returns a stringify request parameter value
@@ -47,84 +81,61 @@ namespace Widget;
  * @property Redirect   $redirect A widget that send a redirect response
  * @method   Redirect   redirect($url = null, $status = 302, array $options = array()) Send a redirect response
  *
- * Cache
- * @property Cache      $cache A cache widget proxy
- * @method   mixed      cache($key, $value = null, $expire = 0) Retrieve or store an item by cache
- * @property ArrayCache $arrayCache  A cache widget stored data in PHP array
- * @method   mixed      arrayCache($key, $value = null, $expire = 0) Retrieve or store an item by array cache
- * @property Apc        $apc The PHP APC cache widget
- * @method   mixed      apc($key, $value = null, $expire = 0) Retrieve or store an item
- * @property DbCache    $dbCache A database cache widget
- * @method   mixed      dbCache($key, $value = null, $expire = 0) Retrieve or store an item by database cache
- * @property FileCache  $fileCache A file cache widget
- * @method   mixed      fileCache($key, $value = null, $expire = 0) Retrieve or store an item by file
- * @property Memcache   $memcache A cache widget base on Memcache
- * @method   mixed      memcache($key, $value = null, $expire = 0) Retrieve or store an item by Memcache
- * @property Memcached  $memcached A cache widget base on Memcached
- * @method   mixed      memcached($key, $value = null, $expire = 0) Retrieve or store an item by Memcached
- * @property Couchbase  $couchbase A cache widget base on Couchbase
- * @method   mixed      couchbase($key, $value = null, $expire = 0) Retrieve or store an item by Couchbase
- * @property redis      $redis A cache widget base on Redis
- * @method   mixed      redis($key, $value = null, $expire = 0) Retrieve or store an item by Redis
- * @property Bicache    $bicache A two-level cache widget
- * @method   mixed      bicache($key, $value = null, $expire = 0) Retrieve or store an item by two-level cache
- *
  * View
- * @property View       $view A widget that use to render PHP template
- * @method   string     view($name = null, $vars = array()) Returns view widget or render a PHP template
  * @property Escape     $escape A widget to escape HTML, javascript, CSS, HTML Attribute and URL for secure output
  * @method   string     escape($string, $type = 'html') Escapes a string by specified type for secure output
- * @property Smarty     $smarty A wrapper widget for Smarty object
- * @method   mixed      smarty($name = null, $vars = array()) Returns the internal Smarty object or render a Smarty template
- * @property Twig       $twig A wrapper widget for Twig object
- * @method   mixed      twig($name = null, $vars = array()) Returns \Twig_Environment object or render a Twig template
+ * @property View       $view A widget that use to render PHP template
+ * @method   string     view($name = null, $vars = array()) Returns view widget or render a PHP template
  *
- * Validation
- * @method   Validate   validate(array $option) Create a new validator and validate by specified options
- * @property Is         $is The validator manager, use to validate input quickly, create validator
+ * Application
+ * @property WeChatApp  $weChatApp A widget handles WeChat(WeiXin) callback message
+ * @method   WeChatApp  weChatApp() Start up WeChat application and output the matched rule message
+ * @property App        $app The application widget
+ * @method   App        app(array $options = array()) Startup application
+ * @property Router     $router A widget that build a simple REST application
+ * @method   Router     router($pathInfo = null, $method = null) Run the application
+ * @property Url        $url A util widget to build URL
+ * @method   string     url($uri) Build URL by specified uri and parameters
  *
- * Database
- * @property Db                           $db A database widget
- * @method   Db\QueryBuilder              db()
- * @property Dbal                         $dbal A container widget for Doctrine DBAL connection object
- * @method   \Doctrine\DBAL\Connection    dbal() Retrieve the Doctrine DBAL connection object
- * @property EntityManager                $entityManager A container widget for Doctrine ORM entity manager object
- * @method   \Doctrine\ORM\EntityManager  entityManager() Returns the Doctrine ORM entity manager
- *
- * Event
- * @property Event      $event The event manager to add, remove and trigger events
- *
- * Util
+ * Other
  * @property Arr        $arr An util widget provides some useful method to manipulation array
  * @property Env        $env A widget to detect the environment name and load configuration by environment name
  * @method   string     env() Returns the environment name
- * @method   bool       is($rule = null, $input = null, $options = array()) Validate input by given rule
+ * @property Error      $error A widget that handles exception and display pretty exception message
+ * @method   Error      error($fn) Attach a handler to exception error
+ * @property Event      $event The event manager to add, remove and trigger events
+ * @property Gravatar   $gravatar A widget that generates a Gravatar URL for a specified email address
+ * @method   string     gravatar($email, $size = null, $default = null, $rating = null) Generates a Gravatar URL for a specified email address
  * @property Logger     $logger A simple logger widget, which is base on the Monolog
  * @method   bool       logger($level, $message) Logs with an arbitrary level
- * @property Monolog    $monolog A wrapper widget for Monolog
- * @method   bool       monolog($level = null, $message = null, array $context = array()) Get monolog logger object or add a log record
+ * @property Map        $map A widget that handles key-value map data
+ * @method   mixed      map($name, $key = null) Get map data by specified name
  * @property Pinyin     $pinyin An util widget that converts Chinese words to phonetic alphabets
  * @method   string     pinyin($word) Converts Chinese words to phonetic alphabets
  * @property Uuid       $uuid A util widget that generates a RANDOM UUID(universally unique identifier)
  * @method   string     uuid() generates a RANDOM UUID(universally unique identifier)
  * @property Website    $website A pure configuration widget for your website
  * @method   string     website($name) Returns the value of website configuration
- * @property Call       $call A widget handles HTTP request like jQuery Ajax
- * @method   Call       call(array $options) Create a new call object and execute
- * @property WeChatApp  $weChatApp A widget handles WeChat(WeiXin) callback message
- * @method   WeChatApp  weChatApp() Start up WeChat application and output the matched rule message
- * @property App        $app The application widget
- * @method   App        app(array $options = array()) Startup application
- * @property Gravatar   $gravatar A widget that generates a Gravatar URL for a specified email address
- * @method   string     gravatar($email, $size = null, $default = null, $rating = null) Generates a Gravatar URL for a specified email address
- * @property Router     $router A widget that build a simple REST application
- * @method   Router     router($pathInfo = null, $method = null) Run the application
  * @property T          $t A translator widget
  * @method   string     t($message, array $parameters = array()) Translate the message
- * @property Url        $url A util widget to build URL
- * @method   string     url($uri) Build URL by specified uri and parameters
- * @property Map        $map A widget that handles key-value map data
- * @method   mixed      map($name, $key = null) Get map data by specified name
+ *
+ * Third Party
+ *
+ *  Database
+ * @property Dbal                         $dbal A container widget for Doctrine DBAL connection object
+ * @method   \Doctrine\DBAL\Connection    dbal() Retrieve the Doctrine DBAL connection object
+ * @property EntityManager                $entityManager A container widget for Doctrine ORM entity manager object
+ * @method   \Doctrine\ORM\EntityManager  entityManager() Returns the Doctrine ORM entity manager
+ *
+ *  View
+ * @property Smarty     $smarty A wrapper widget for Smarty object
+ * @method   mixed      smarty($name = null, $vars = array()) Returns the internal Smarty object or render a Smarty template
+ * @property Twig       $twig A wrapper widget for Twig object
+ * @method   mixed      twig($name = null, $vars = array()) Returns \Twig_Environment object or render a Twig template
+ *
+ *  Others
+ * @property Monolog    $monolog A wrapper widget for Monolog
+ * @method   bool       monolog($level = null, $message = null, array $context = array()) Get monolog logger object or add a log record
  */
 abstract class AbstractWidget
 {
