31587f4b7646f56431574cb8823c6e1863af9938
twinh <twinhuang@qq.com>
2013-07-28 01:16:11 -0700

    fixed comment typo

--- tests/WidgetTest/WebsiteTest.php
+++ tests/WidgetTest/WebsiteTest.php
@@ -6,12 +6,12 @@ class WebsiteTest extends TestCase
 {
     public function testWebsite()
     {
-        // Set pre-defind configuration
+        // Set pre-defined configuration
         $this->website->set('title', 'My Blog');
-        
+
         $this->assertEquals('My Blog', $this->website->get('title'));
         $this->assertEquals('My Blog', $this->website('title'));
-        
+
         // Set configuration on demand
         $this->website->set('author', 'twin');
         $this->assertEquals('twin', $this->website->get('author'));
