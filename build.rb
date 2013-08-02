b92d7b89d43bc2d70123edecc8c9c7609b600ea3
twinh <twinhuang@qq.com>
2013-08-01 20:58:05 -0700

    automatic built documentation from 0cc27e2016c7cb89ff598e5d1f807ad4e1eb3c46

--- build.rb
+++ build.rb
@@ -54,7 +54,7 @@ widgets = [
   # view-section
     'escape', 'view',
   # others
-    'arr', 'env', 'error', 'event', 'gravatar', 'logger', 'pinyin', 'uuid', 'website',
+    'arr', 'env', 'error', 'event', 'gravatar', 'logger', 'map', 'pinyin', 'uuid', 'website',
   # third party
     # db
     'dbal', 'entityManager',
