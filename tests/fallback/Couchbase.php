b255ddda3ceadf619d02dd1f6c273aa11d79aaba
twinh <twinhuang@qq.com>
2013-08-08 23:14:46 -0700

    added return value for fallback class

--- tests/fallback/Couchbase.php
+++ tests/fallback/Couchbase.php
@@ -2,14 +2,53 @@
 
 class Couchbase
 {
-    public function get(){}
-    public function set(){}
-    public function add(){}
-    public function replace(){}
-    public function delete(){}
-    public function inc(){}
-    public function dec(){}
-    public function getMulti(){}
-    public function setMulti(){}
-    public function flush(){}
+    public function get()
+    {
+        return false;
+    }
+
+    public function set()
+    {
+        return false;
+    }
+
+    public function add()
+    {
+        return false;
+    }
+
+    public function replace()
+    {
+        return false;
+    }
+
+    public function delete()
+    {
+        return false;
+    }
+
+    public function inc()
+    {
+        return false;
+    }
+
+    public function dec()
+    {
+        return false;
+    }
+
+    public function getMulti()
+    {
+        return array();
+    }
+
+    public function setMulti()
+    {
+        return array();
+    }
+
+    public function flush()
+    {
+        return false;
+    }
 }
