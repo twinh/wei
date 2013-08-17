ba33ff3842da6d759367b403441b7d2c754b51e0
twinh <twinhuang@qq.com>
2013-08-16 19:26:08 -0700

    updated session widget, implements ArrayAccess and Countable interfaces instead of extends parameter class

--- lib/Widget/Session.php
+++ lib/Widget/Session.php
@@ -8,14 +8,12 @@
 
 namespace Widget;
 
-use Widget\Stdlib\Parameter;
-
 /**
- * A widget that handles session parameters ($_SESSION)
+ * A widget that handles session data ($_SESSION)
  *
  * @author      Twin Huang <twinhuang@qq.com>
  */
-class Session extends Parameter
+class Session extends Base implements \ArrayAccess, \Countable
 {
     /**
      * The namespace to store session data, disable on default
@@ -23,6 +21,13 @@ class Session extends Parameter
      * @var string
      */
     protected $namespace = false;
+
+    /**
+     * The session data
+     *
+     * @var array
+     */
+    protected $data = array();
 
     /**
      * The session configuration options
@@ -110,7 +115,49 @@ class Session extends Parameter
     }
 
     /**
-     * Destroy all session data
+     * Set session data
+     *
+     * @param string|array $name The session name or A key-value array
+     * @param mixed $value The session value
+     * @return Session
+     */
+    public function set($name, $value = null)
+    {
+        if (!is_array($name)) {
+            $this->data[$name] = $value;
+        } else {
+            foreach ($name as $key => $value) {
+                $this->data[$key] = $value;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Remove session data by specified name
+     *
+     * @param string $name The name of session
+     * @return Session
+     */
+    public function remove($name)
+    {
+        unset($this->data[$name]);
+        return $this;
+    }
+
+    /**
+     * Clear session data in current namespace
+     *
+     * @return Session
+     */
+    public function clear()
+    {
+        $this->data = array();
+        return $this;
+    }
+
+    /**
+     * Destroy all session data, including session in other namespaces
      *
      * @return Session
      */
@@ -135,5 +182,58 @@ class Session extends Parameter
         }
         $this->inis = $inis + $this->inis;
         return $this;
+    }
+
+    /**
+     * Check if the offset exists
+     *
+     * @param  string $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->data);
+    }
+
+    /**
+     * Get the offset value
+     *
+     * @param  string $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->data[$offset]) ? $this->data[$offset] : null;
+    }
+
+    /**
+     * Set the offset value
+     *
+     * @param string $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->data[$offset] = $value;
+    }
+
+    /**
+     * Unset the offset
+     *
+     * @param string $offset
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
+    }
+
+    /**
+     * Return the length of data
+     *
+     * @return int the length of data
+     */
+    public function count()
+    {
+        return count($this->data);
     }
 }
