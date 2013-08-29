420a73ab59f57074b8cb64767a0f81dbaf287f02
twinh <twinhuang@qq.com>
2013-08-28 23:21:32 -0700

    automatic built documentation from 6a43ae467b6755fff66129ff17021eb0337816f6

--- build.rb
+++ build.rb
@@ -54,7 +54,7 @@ widgets = [
   # view
     'escape', 'view',
   # others
-    'arr', 'config', 'env', 'error', 'event', 'gravatar', 'logger', 'pinyin', 'uuid',
+    'arr', 'config', 'env', 'error', 'gravatar', 'logger', 'pinyin', 'uuid',
   # third party
     'monolog', 
     'phpError'
