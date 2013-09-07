7dad23f522fd0c1992d0c85d6e67edfd44fdd949
twinh <twinhuang@qq.com>
2013-09-06 19:48:24 -0700

    removed bicache setup method

--- tests/WidgetTest/BicacheTest.php
+++ tests/WidgetTest/BicacheTest.php
@@ -4,12 +4,4 @@ namespace WidgetTest;
 
 class BicacheTest extends CacheTestCase
 {
-    protected function setUp()
-    {
-        // FIXME
-        if (!extension_loaded('apc') || !ini_get('apc.enable_cli')) {
-            $this->markTestSkipped('Extension "apc" is not loaded.');
-        }
-        parent::setUp();
-    }
 }
