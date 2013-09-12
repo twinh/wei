f5fe0bee54a66be1724ec8e2acce3c6f0688a676
twinh <twinhuang@qq.com>
2013-09-11 23:53:37 -0700

    added missing file

---
+++ tests/WidgetTest/AppTest/DispatchBreak.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace WidgetTest\AppTest;
+
+class DispatchBreak extends \Widget\Base
+{
+    public function __construct($options = array())
+    {
+        parent::__construct($options);
+
+        // Stop the app to call action method
+        $this->app->preventPreviousDispatch();
+    }
+
+    public function index()
+    {
+        return 'can not see me';
+    }
+}
