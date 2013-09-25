d78150873f256c819a9c47ce15111206a862eff9
twinh <twinhuang@qq.com>
2013-09-25 07:20:58 -0700

    fixed uuid test error

--- tests/WidgetTest/UuidTest.php
+++ tests/WidgetTest/UuidTest.php
@@ -6,9 +6,6 @@ class UuidTest extends TestCase
 {
     public function testUuid()
     {
-        // Instance validator manager widget
-        $this->is;
-        
         for ($i = 0; $i < 100; $i++) {
             $this->isUuid($this->uuid());
         }
