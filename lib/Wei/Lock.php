4d9e382e610879c9e44866ad81e52c0516e3e47e
twinh <twinhuang@qq.com>
2013-11-01 20:15:27 -0700

    added initial version of lock service

---
+++ lib/Wei/Lock.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Wei Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Wei;
+
+/**
+ * A service that generates a Gravatar URL for a specified email address
+ *
+ * @author      Twin Huang <twinhuang@qq.com>
+ * @property    Cache $cache A cache service
+ */
+class Lock extends Base
+{
+    /**
+     * @var array
+     */
+    protected $keys = array();
+
+    /**
+     * Constructor
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = array())
+    {
+        parent::__construct($options);
+
+        $self = $this;
+        register_shutdown_function(function() use($self) {
+            $self->releaseAll();
+        });
+    }
+
+    /**
+     * Acquire a lock key
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function __invoke($key)
+    {
+        if ($this->cache->add($key, true)) {
+            $this->keys[] = $key;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Release a lock key
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function release($key)
+    {
+        return $this->cache->remove($key);
+    }
+
+    /**
+     * Release all lock keys
+     */
+    public function releaseAll()
+    {
+        foreach ($this->keys as $key) {
+            $this->release($key);
+        }
+    }
+}
