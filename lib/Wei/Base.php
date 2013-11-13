ef2ea1754c0abc9db8901761bbfb9b632aceb415
twinh <twinhuang@qq.com>
2013-11-12 16:30:55 -0800

    fixed typo

--- lib/Wei/Base.php
+++ lib/Wei/Base.php
@@ -86,10 +86,10 @@ abstract class Base
     }
 
     /**
-     * Invoke the wei by the given name
+     * Invoke a service by the given name
      *
-     * @param string $name The name of wei
-     * @param array $args The arguments for wei's __invoke method
+     * @param string $name The name of service
+     * @param array $args The arguments for the service's __invoke method
      * @return mixed
      */
     public function __call($name, $args)
@@ -98,9 +98,9 @@ abstract class Base
     }
 
     /**
-     * Get the wei object by the given name
+     * Get a service object by the given name
      *
-     * @param  string $name The name of wei
+     * @param  string $name The name of service
      * @return $this
      */
     public function __get($name)
