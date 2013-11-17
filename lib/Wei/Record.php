c674850e36d37659d80465505764833c2883937c
twinh <twinhuang@qq.com>
2013-11-17 03:44:25 -0800

    removed extra header

--- lib/Wei/Record.php
+++ lib/Wei/Record.php
@@ -5,12 +5,6 @@
  * @copyright   Copyright (c) 2008-2013 Twin Huang
  * @license     http://opensource.org/licenses/mit-license.php MIT License
  */
-/**
- * Wei Framework
- *
- * @copyright   Copyright (c) 2008-2013 Twin Huang
- * @license     http://opensource.org/licenses/mit-license.php MIT License
- */
 
 namespace Wei;
 
