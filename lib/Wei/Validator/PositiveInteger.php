a93329f52999ab0410f373a59d476b766f28bfee
twinh <twinhuang@qq.com>
2013-11-20 21:52:47 -0800

    added isPositiveInteger validator

---
+++ lib/Wei/Validator/PositiveInteger.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Wei Framework
+ *
+ * @copyright   Copyright (c) 2008-2013 Twin Huang
+ * @license     http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Wei\Validator;
+
+/**
+ * Check if the input is a positive integer
+ *
+ * @author      Twin Huang <twinhuang@qq.com>
+ */
+class PositiveInteger extends BaseValidator
+{
+    protected $invalidMessage = '%name% must be positive integer';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function doValidate($input)
+    {
+        if (false !== filter_var($input, FILTER_VALIDATE_INT)) {
+            return true;
+        } else {
+            $this->addError('invalid');
+            return false;
+        }
+    }
+}
