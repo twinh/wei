0495db64b7f8e30d6a5775e6e7f60d1677336828
twinh <twinhuang@qq.com>
2013-12-11 21:56:14 -0800

    updated error view 2

--- lib/Error.php
+++ lib/Error.php
@@ -278,28 +278,27 @@ class Error extends Base
             $detail = "<p>$detail</p>";
         }
 
-        $html = <<<HTML
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<title>{$message}</title>
-<style type="text/css">
-body { font-size: 12px; color: #333; padding: 15px 20px 20px 20px; }
-h1, h2, p, pre { margin: 0; padding: 0; }
-body, pre { font-family: "Helvetica Neue", Helvetica, Arial, sans-serif, "\5fae\8f6f\96c5\9ed1", "\5b8b\4f53"; }
-h1 { font-size: 36px; }
-h2 { font-size: 20px; margin: 20px 0 0; }
-pre { line-height: 18px; }
-strong, .error-text { color: #FF3000; }
-</style>
-</head>
-<body>
-<h1>{$message}</h1>
-{$detail}
-</body>
-</html>
-HTML;
+        $html = '<!DOCTYPE html>'
+            . '<html>'
+            . '<head>'
+            . '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>'
+            . "<title>$message</title>"
+            . '<style type="text/css">'
+            . 'body { font-size: 12px; color: #333; padding: 15px 20px 20px 20px; }'
+            . 'h1, h2, p, pre { margin: 0; padding: 0; }'
+            . 'body, pre { font-family: "Helvetica Neue", Helvetica, Arial, sans-serif, "\5fae\8f6f\96c5\9ed1", "\5b8b\4f53"; }'
+            . 'h1 { font-size: 36px; }'
+            . 'h2 { font-size: 20px; margin: 20px 0 0; }'
+            . 'pre { line-height: 18px; }'
+            . 'strong, .error-text { color: #FF3000; }'
+            . '</style>'
+            . '</head>'
+            . '<body>'
+            . "<h1>$message</h1>"
+            . $detail
+            . '</body>'
+            . '</html>';
+
         echo $html;
     }
 
