349b1c96a36f086d0de2903853237637540c692f
twinh <twinhuang@qq.com>
2013-12-17 22:47:35 -0800

    try to fixed password test error when php < 5.5

--- tests/unit/PasswordTest.php
+++ tests/unit/PasswordTest.php
@@ -8,25 +8,6 @@ namespace WeiTest;
  */
 class PasswordTest extends TestCase
 {
-    /**
-     * @dataProvider provideInfo
-     */
-    public function testInfo($hash, $info)
-    {
-        $this->assertEquals($info, $this->password->getInfo($hash));
-    }
-
-    public static function provideInfo()
-    {
-        return array(
-            array('foo', array('algo' => 0, 'algoName' => 'unknown', 'options' => array())),
-            array('$2y$', array('algo' => 0, 'algoName' => 'unknown', 'options' => array())),
-            array('$2y$07$', array('algo' => 0, 'algoName' => 'unknown', 'options' => array())),
-            array('$2y$07$usesomesillystringfore2uDLvp1Ii2e./U9C8sBjqp8I90dH6hi', array('algo' => PASSWORD_BCRYPT, 'algoName' => 'bcrypt', 'options' => array('cost' => 7))),
-            array('$2y$10$usesomesillystringfore2uDLvp1Ii2e./U9C8sBjqp8I90dH6hi', array('algo' => PASSWORD_BCRYPT, 'algoName' => 'bcrypt', 'options' => array('cost' => 10))),
-        );
-    }
-
     public function testStringLength()
     {
         $this->assertEquals(60, strlen($this->password->hash('foo')));
@@ -67,5 +48,24 @@ class PasswordTest extends TestCase
     {
         $this->setExpectedException('InvalidArgumentException', 'Provided salt is too short: 3 expecting 22');
         $this->password->hash('foo', 'abc');
+    }
+
+    /**
+     * @dataProvider provideInfo
+     */
+    public function testInfo($hash, $info)
+    {
+        $this->assertEquals($info, $this->password->getInfo($hash));
+    }
+
+    public static function provideInfo()
+    {
+        return array(
+            array('foo', array('algo' => 0, 'algoName' => 'unknown', 'options' => array())),
+            array('$2y$', array('algo' => 0, 'algoName' => 'unknown', 'options' => array())),
+            array('$2y$07$', array('algo' => 0, 'algoName' => 'unknown', 'options' => array())),
+            array('$2y$07$usesomesillystringfore2uDLvp1Ii2e./U9C8sBjqp8I90dH6hi', array('algo' => PASSWORD_BCRYPT, 'algoName' => 'bcrypt', 'options' => array('cost' => 7))),
+            array('$2y$10$usesomesillystringfore2uDLvp1Ii2e./U9C8sBjqp8I90dH6hi', array('algo' => PASSWORD_BCRYPT, 'algoName' => 'bcrypt', 'options' => array('cost' => 10))),
+        );
     }
 }
