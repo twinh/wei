86646a29a5443f83e204e8b2080b2a633681cea4
twinh <twinhuang@qq.com>
2013-12-21 05:43:20 -0800

    get primary key value when primary key not set or empty

--- lib/Record.php
+++ lib/Record.php
@@ -349,7 +349,7 @@ class Record extends Base implements \ArrayAccess, \IteratorAggregate, \Countabl
                 $result = (bool)$this->db->insert($this->table, $this->data);
                 if ($result) {
                     $this->isNew = false;
-                    if (!isset($this->data[$this->primaryKey]) || $this->data[$this->primaryKey] === null) {
+                    if (!isset($this->data[$this->primaryKey]) || !$this->data[$this->primaryKey]) {
                         $name = sprintf('%s_%s_seq', $this->fullTable, $this->primaryKey);
                         $this->data[$this->primaryKey] = $this->db->lastInsertId($name);
                     }
