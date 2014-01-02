00ea82dd46bbee3d05a96346cfc02ecdb6d0c475
twinh <twinhuang@qq.com>
2014-01-01 21:25:01 -0800

    try to fix hhvm runInSeparateProcess test error, refs #153

--- tests/unit/SessionTest.php
+++ tests/unit/SessionTest.php
@@ -109,7 +109,6 @@ class SessionTest extends TestCase
 
     /**
      * @dataProvider providerForGetterAndSetter
-     * @runInSeparateProcess
      */
     public function testValues($value, $key)
     {
