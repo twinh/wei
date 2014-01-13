5233d940b9211e3d7122c9430ef91b3e66dea13f
twinh <twinhuang@qq.com>
2014-01-12 21:48:17 -0800

    recognized null as primary key value

--- lib/Record.php
+++ lib/Record.php
@@ -1492,7 +1492,7 @@ class Record extends Base implements \ArrayAccess, \IteratorAggregate, \Countabl
     protected function processCondition($conditions, $params, $types)
     {
         // Regard numeric conditions as primary key value
-        if (is_numeric($conditions)) {
+        if (is_numeric($conditions) || is_null($conditions)) {
             $conditions = array($this->primaryKey => $conditions);
         }
 
