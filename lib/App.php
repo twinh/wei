62827ed05b5e75d2598fd72b332599fa79604716
twin <twinhuang@qq.com>
2015-03-27 18:04:47 +0800

    完善注释

--- lib/App.php
+++ lib/App.php
@@ -343,6 +343,12 @@ class App extends Base
         return $this;
     }
 
+    /**
+     * Returns the method name of specified acion
+     *
+     * @param string $action
+     * @return string
+     */
     public function getActionMethod($action)
     {
         return $action . 'Action';
