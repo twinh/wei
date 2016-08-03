90e6cd27c99e381216676f4ec6f20541fcab63b6
twin <twinhuang@qq.com>
2016-08-03 10:07:09 +0800

    调试5.5,5.6 redis测试失败原因 6, 简化测试逻辑

--- tests/unit/RedisTest.php
+++ tests/unit/RedisTest.php
@@ -2,8 +2,13 @@
 
 namespace WeiTest;
 
-class RedisTest extends CacheTestCase
+class RedisTest extends TestCase
 {
+    /**
+     * @var \Wei\BaseCache
+     */
+    protected $object;
+
     public function setUp()
     {
         if (!extension_loaded('redis') || !class_exists('\Redis')) {
@@ -11,6 +16,8 @@ class RedisTest extends CacheTestCase
         }
 
         parent::setUp();
+
+        $this->object->setNamespace('test-');
 
         try {
             $this->object->get('test');
@@ -36,6 +43,46 @@ class RedisTest extends CacheTestCase
         }
     }
 
+    /**
+     * @dataProvider providerForGetterAndSetter
+     */
+    public function testGetterAndSetter($value, $key)
+    {
+        $cache = $this->object;
+
+        $cache->remove($key);
+        $this->assertFalse($cache->get($key));
+
+        $this->assertFalse($cache->replace($key, $value));
+        $this->assertTrue($cache->add($key, $value));
+
+        $cache->set($key, $value);
+        $this->assertEquals($value, $cache->get($key));
+
+        $this->assertFalse($cache->add($key, $value));
+
+        // Replace with the same value for testing MySQL cache
+        $this->assertTrue($cache->replace($key, $value));
+
+        $this->assertTrue($cache->replace($key, uniqid()));
+    }
+
+    public function providerForGetterAndSetter()
+    {
+        $obj = new \stdClass;
+
+        return array(
+            array(array(),  'array'),
+            array(true,     'bool'),
+            array(1.2,      'float'),
+            array(1,        'int'),
+            array(1,        'integer'),
+            array(null,     'null'),
+            array('1',      'numeric'),
+            array($obj,     'object'),
+        );
+    }
+
     public function tearDown()
     {
         /** @var \Redis $redis */
@@ -44,43 +91,5 @@ class RedisTest extends CacheTestCase
         var_dump('Redis last error', $redis->getLastError());
 
         return parent::tearDown();
-    }
-
-    public function testIncrAndDecr()
-    {
-        $redis = $this->object->getObject();
-        $redis->setOption(\Redis::OPT_SERIALIZER, \Redis::SERIALIZER_NONE);
-
-        parent::testIncrAndDecr();
-    }
-
-    public function testPrefix()
-    {
-        $redis = $this->object->getObject();
-        $redis->setOption(\Redis::OPT_SERIALIZER, \Redis::SERIALIZER_NONE);
-
-        parent::testPrefix();
-    }
-
-    public function testGetAndSetObject()
-    {
-        $cache = $this->object;
-        $redis = $cache->getObject();
-
-        $this->assertInstanceOf('\Redis', $redis);
-
-        $cache->setObject($redis);
-
-        $this->assertInstanceOf('\Redis', $cache->getObject());
-    }
-
-    public function testGetRedisObject()
-    {
-        $this->assertInstanceOf('\Redis', $this->redis());
-    }
-
-    public function testConnectWithExistsObject()
-    {
-        $this->assertTrue($this->object->connect());
     }
 }
