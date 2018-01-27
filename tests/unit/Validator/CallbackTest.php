a4f3d297a781f1ebb30d836d405269f96950b353
twinh <twinhuang@qq.com>
2018-01-27 18:23:30 +0800

    isCallback: 回调函数除了闭包,还允许传入字符串,数字

--- tests/unit/Validator/CallbackTest.php
+++ tests/unit/Validator/CallbackTest.php
@@ -8,33 +8,46 @@ class CallbackTest extends TestCase
     {
         parent::setUp();
 
-        $this->inputTestOptions['fn'] = function(){};
+        $this->inputTestOptions['fn'] = function () {
+        };
     }
 
     public function testCallback()
     {
-        $this->assertTrue($this->isCallback('data', function(){
+        $this->assertTrue($this->isCallback('data', function () {
             return true;
         }));
     }
 
     public function testNotCallback()
     {
-        $this->assertFalse($this->isCallback('data', function(){
+        $this->assertFalse($this->isCallback('data', function () {
             return false;
         }));
 
-        $this->assertFalse($this->isCallback('data', function(){
+        $this->assertFalse($this->isCallback('data', function () {
             // convert to boolen(false)
-            return  null;
+            return null;
         }));
     }
 
     public function testCallbackMessage()
     {
-        $this->isCallback('data', function(){
+        $this->isCallback('data', function () {
             return false;
         }, 'invalid message');
         $this->assertEquals('invalid message', $this->isCallback->getFirstMessage());
+    }
+
+    public function testArrayCallback()
+    {
+        $result = $this->isCallback('data', [$this, 'arrayCallback']);
+
+        $this->assertFalse($result);
+    }
+
+    public function arrayCallback()
+    {
+        return false;
     }
 }
