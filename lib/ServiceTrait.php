6106fd98b520572c65f3aeedd7da57ff1a0f22ec
twinh <twinhuang@qq.com>
2020-06-08 21:25:18 +0800

    feat: ServiceTrait 加入 wei 中

---
+++ lib/ServiceTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wei;
+
+/**
+ * Add the ability to get and call other services for the class
+ */
+trait ServiceTrait
+{
+    /**
+     * The service provider map
+     *
+     * @var array
+     */
+    protected $providers = [];
+
+    /**
+     * Invoke a service by the given name
+     *
+     * @param string $name The name of service
+     * @param array $args The arguments for the service's __invoke method
+     * @return mixed
+     */
+    public function __call($name, $args)
+    {
+        return call_user_func_array($this->$name, $args);
+    }
+
+    /**
+     * Get a service object by the given name
+     *
+     * @param string $name The name of service
+     * @return Base
+     */
+    public function __get($name)
+    {
+        return $this->$name = $this->wei->get($name, [], $this->providers);
+    }
+}
