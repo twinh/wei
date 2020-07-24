de2f732f8d61ce89df62d17236d9994ba713dc99
twinh <twinhuang@qq.com>
2020-07-24 17:25:18 +0800

    test: 设置 driver 后还原

--- tests/unit/CacheTest.php
+++ tests/unit/CacheTest.php
@@ -28,8 +28,12 @@ class CacheTest extends CacheTestCase
 
     public function testCacheDriver()
     {
+        $driver = $this->cache->getDriver();
+
         $this->cache->setDriver('phpFileCache');
 
         $this->assertEquals('phpFileCache', $this->cache->getDriver());
+
+        $this->cache->setDriver($driver);
     }
 }
