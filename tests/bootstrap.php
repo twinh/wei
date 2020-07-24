877383e7dc54008ea78fb8ae36932678e36348cc
twinh <twinhuang@qq.com>
2020-07-24 16:42:42 +0800

    ci: 文件路径错误

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -81,7 +81,7 @@ wei(array(
 // Load configuration for CI
 foreach (array('TRAVIS', 'CODESHIP', 'GITHUB_ACTIONS') as $ci) {
     if (getenv($ci)) {
-        wei(__DIR__ . '/config/' . strtolower($ci) . '.php');
+        wei(__DIR__ . '/config/' . strtolower(strtr($ci, '_', '-')) . '.php');
     }
 }
 
