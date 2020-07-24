b01933aeafc4a5dd4ab5548f55a2c2900d452cea
twinh <twinhuang@qq.com>
2020-07-24 17:39:06 +0800

    test: 不替换默认缓存

--- tests/config/github-actions.php
+++ tests/config/github-actions.php
@@ -1,11 +1,6 @@
 <?php
 
 return [
-    'wei' => [
-        'providers' => [
-            'cache' => 'redis',
-        ],
-    ],
     'mysql.db' => [
         'host' => '127.0.0.1',
         'port' => getenv('DB_PORT'),
