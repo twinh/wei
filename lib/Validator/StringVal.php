8c6a1205deb9a2cc90c15a7d5d67c75d2c5be199
twinh <twinhuang@qq.com>
2020-10-28 17:36:51 +0800

    feat(Validator): 增加 StringVal 规则

---
+++ lib/Validator/StringVal.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Wei Framework
+ *
+ * @copyright   Copyright (c) 2008-2020 Twin Huang
+ * @license     http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Wei\Validator;
+
+/**
+ * Check if the input could be convert to string
+ *
+ * @author      Twin Huang <twinhuang@qq.com>
+ */
+class StringVal extends BaseValidator
+{
+    protected function doValidate($input)
+    {
+        if (!$this->isString($input)) {
+            $this->addError('notString');
+            return false;
+        }
+        return true;
+    }
+}
