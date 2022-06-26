6b33d4e91e153fa52c388f6bff378ebcb2bb3d01
twinh <twinhuang@qq.com>
2022-06-26 22:05:42 +0800

    test(NearCache): 增加 get，set 测试

--- tests/unit/NearCacheTest.php
+++ tests/unit/NearCacheTest.php
@@ -2,9 +2,45 @@
 
 namespace WeiTest;
 
+use Wei\NearCache;
+
 /**
  * @internal
+ * @property NearCache $object
  */
 final class NearCacheTest extends CacheTestCase
 {
+    public function testGetFromFront()
+    {
+        $time = time();
+
+        $this->object->getFront()->set(__FUNCTION__, $time);
+        $this->object->getBack()->delete(__FUNCTION__);
+
+        $this->assertSame($time, $this->object->get(__FUNCTION__));
+    }
+
+    public function testGetFromBack()
+    {
+        $time = time();
+
+        $this->object->getFront()->delete(__FUNCTION__);
+        $this->object->getBack()->set(__FUNCTION__, $time);
+
+        $this->assertSame($time, $this->object->get(__FUNCTION__));
+
+        $this->assertSame($time, $this->object->getFront()->get(__FUNCTION__));
+    }
+
+    public function testSetBoth()
+    {
+        $time = time();
+        $this->object->set(__FUNCTION__, $time);
+
+        $front = $this->object->getFront();
+        $back = $this->object->getFront();
+
+        $this->assertSame($time, $front->get(__FUNCTION__));
+        $this->assertSame($time, $back->get(__FUNCTION__));
+    }
 }
